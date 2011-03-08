2443a9787e04f1e5201686622c75053ce443da49
aga <user@ubuntu.(none)>
2011-03-08 17:58:56 +0000

    Modified tests to inherit from CamelIntegrationSpec

--- test/integration/frontlinesms2/EmailTranslationRouteSpec.groovy
+++ test/integration/frontlinesms2/EmailTranslationRouteSpec.groovy
@@ -1,37 +1,13 @@
 package frontlinesms2
 
-import spock.lang.*
-import grails.plugin.spock.*
+import routing.CamelIntegrationSpec
 
-import org.apache.camel.*
-import org.apache.camel.builder.*
-import org.apache.camel.impl.DefaultProducerTemplate
-
-class EmailTranslationRouteSpec extends IntegrationSpec {
-	def camelContext
-
-	def resultEndpoint
-	def template
-
-	def setup() {
-		camelContext.addRoutes(createRouteBuilder())
-		resultEndpoint = camelContext.getEndpoint('mock:result')
-		template = new DefaultProducerTemplate(
-			camelContext,
-			camelContext.getEndpoint('direct:start'))
-		template.start()
+class EmailTranslationRouteSpec extends CamelIntegrationSpec {
+	String getFrom() {
+		'seda:raw-email'
 	}
-
-	def cleanup() {
-		template?.stop()
-		println "Current routes:"
-		for(r in camelContext.getRoutes()) {
-			println "\t${r?.id} :: ${r}"
-		}
-		def testRoutes = [camelContext.getRouteDefinition('test-1'),
-						camelContext.getRouteDefinition('test-2')]
-		println "Removing routes: ${testRoutes}"
-		camelContext.removeRouteDefinitions(testRoutes)
+	String getTo() {
+		'seda:fmessages-to-store'
 	}
 
 	def "test translation route"() {
@@ -45,15 +21,6 @@ class EmailTranslationRouteSpec extends IntegrationSpec {
 		then:
        			resultEndpoint.assertIsSatisfied()
 			assert Fmessage.count() == 0		
-	}
-
-	private RouteBuilder createRouteBuilder() {
-		return new RouteBuilder() {
-			public void configure() {
-				from('direct:start').to('seda:raw-email').routeId('test-1')
-				from('seda:fmessages-to-store').to('mock:result').routeId('test-2')
-			}
-		}
 	}
 }
 
--- test/integration/routing/CamelIntegrationSpec.groovy
+++ test/integration/routing/CamelIntegrationSpec.groovy
@@ -8,9 +8,12 @@ import org.apache.camel.builder.*
 import org.apache.camel.impl.DefaultProducerTemplate
 
 abstract class CamelIntegrationSpec extends IntegrationSpec {
+	// This is the CamelContext used by Grails.  It will be injected for you.
 	def camelContext
 
+	// This is the endpoint of the route to be tested
 	def resultEndpoint
+	// This is the start of the route to be tested
 	def template
 
 	def setup() {
@@ -24,13 +27,8 @@ abstract class CamelIntegrationSpec extends IntegrationSpec {
 
 	def cleanup() {
 		template?.stop()
-		println "Current routes:"
-		for(r in camelContext.getRoutes()) {
-			println "\t${r?.id} :: ${r}"
-		}
 		def testRoutes = [camelContext.getRouteDefinition('test-1'),
 						camelContext.getRouteDefinition('test-2')]
-		println "Removing routes: ${testRoutes}"
 		camelContext.removeRouteDefinitions(testRoutes)
 	}
 
