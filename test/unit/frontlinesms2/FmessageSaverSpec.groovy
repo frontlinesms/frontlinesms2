e80e15b2b8f136316135658715353dcaeba13051
aga <user@ubuntu.(none)>
2011-03-08 18:50:08 +0000

    Fixed one more stupid compilation error!

--- test/unit/frontlinesms2/FmessageSaverSpec.groovy
+++ test/unit/frontlinesms2/FmessageSaverSpec.groovy
@@ -12,7 +12,7 @@ class FmessageSaverSpec extends UnitSpec {
 
 	def "it's a processor"() {
 		then:
-			t instanceof org.apache.camel.processor.Processor
+			t instanceof org.apache.camel.Processor
 	}
 
 	def "it saves the incoming Fmessage"() {
