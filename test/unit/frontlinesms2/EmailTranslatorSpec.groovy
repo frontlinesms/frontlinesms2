c1192aeb321ce86c750dd3d4a5d04589c05d8cf3
aga <user@ubuntu.(none)>
2011-03-08 18:36:32 +0000

    Added unit spec for EmailTranslator.

---
+++ test/unit/frontlinesms2/EmailTranslatorSpec.groovy
@@ -0,0 +1,59 @@
+package frontlinesms2
+
+import spock.lang.*
+import grails.plugin.spock.*
+
+class EmailTranslatorSpec extends UnitSpec {
+	def t
+
+	setupSpec() {
+		t = new EmailTranslator()
+	}
+
+	def "it's a processor"() {
+		then:
+			t instanceof org.apache.camel.processor.TransformProcessor
+	}
+
+	def "email from field is converted to a suitable FMessage from field"() {
+		given:
+			def testExchange = createTestExchange()
+		when:
+			t.process(testExchange)
+		then:
+			FMessage out = testExchange.getOut()
+			assert out.src == "email:test@example.com"
+	}
+
+	def "email to field is converted to a suitable FMessage to desitination field"() {
+		given:
+			def testExchange = createTestExchange()
+		when:
+			t.process(testExchange)
+		then:
+			FMessage out = testExchange.getOut()
+			assert out.dst == "email:frontlinesms1@example.com"
+	}
+
+	def "email body is converted to a suitable FMessage body"() {
+		given:
+			def testExchange = createTestExchange()
+		when:
+			t.process(testExchange)
+		then:
+			FMessage out = testExchange.getOut()
+			assert out.body == "Hello there, here's the test email body converted into a textual message."
+	}
+
+	def "original email is available from the FMessage"() {
+		given:
+			def testEmail = createTestEmail()
+			def createTestExchange = createTestExchange(testEmail)
+		when:
+			t.process(testExchange)
+		then:
+			FMessage out = testExchange.getOut()
+			assert out.original == testEmail
+	}
+}
+
--- test/unit/frontlinesms2/FmessageSpec.groovy
+++ test/unit/frontlinesms2/FmessageSpec.groovy
@@ -4,8 +4,5 @@ import spock.lang.*
 import grails.plugin.spock.*
 
 class FmessageSpec extends UnitSpec {
-	def "feature method"() {
-		
-	}
 }
 
