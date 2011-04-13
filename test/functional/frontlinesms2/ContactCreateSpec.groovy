f1cb8780f817eb81ae91f54d6a4f547ca2444c57
david <david@kutalek-laptop.(none)>
2011-04-13 11:25:26 +0300

    David: minor changes to neaten things

--- test/functional/frontlinesms2/ContactCreateSpec.groovy
+++ test/functional/frontlinesms2/ContactCreateSpec.groovy
@@ -5,17 +5,14 @@ import org.openqa.selenium.firefox.FirefoxDriver
 import grails.plugin.geb.GebSpec
 
 class ContactCreateSpec extends grails.plugin.geb.GebSpec {
-	def 'link to create new contact is displayed and can be clicked'() {
+	def 'link to create new contact is displayed in the menu'() {
 		given:
 		when:
 			go 'contact'
-			println $('body').text()
 			def createContact = $('#create-menu').find('a').first()
 			def btn = $("#create-menu .create")
 		then:
-			assert createContact != null
 			assert createContact.text() == "Contact"
-			println createContact
 			assert createContact.getAttribute('href') == "/frontlinesms2/contact/create"
 
 	}
