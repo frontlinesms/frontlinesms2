cf3a2ac69bb5b427e726aa463834df713c7a263d
Geoffrey Muchai <geoffreymuchai@gmail.com>
2011-05-04 16:20:30 +0300

    Geoff|Dave|Al: Corrected test for duplicate names.

--- test/unit/frontlinesms2/ContactSpec.groovy
+++ test/unit/frontlinesms2/ContactSpec.groovy
@@ -17,53 +17,33 @@ class ContactSpec extends UnitSpec {
 			c.name == 'Alice'
 	}
 
-	def "blank names are allowed"() {
+	def "blank names are allowed, there is no minimum length for name"() {
 		when:
 			def noNameContact = new Contact(name:'')
-			def namedContact = new Contact(name:'Alice')
+			def namedContact = new Contact(name:'a')
 		then:
 			noNameContact.validate()
 			namedContact.validate()
 	}
 	
 	def "duplicate names are allowed"(){
+		setup:
+			mockDomain(Contact)
 		when:
-			def Contact contact1=new Contact(name:'John')
-			def Contact contact2=new Contact(name:'John')
+			def Contact contact1 = new Contact(name:'John')
+			def Contact contact2 = new Contact(name:'John')
 		then:
-			contact1.validate()
-			contact2.validate()
+			contact1.save()
+			contact2.save()
 	}
 	
 	def "max name length 255"(){
 		when:
-			def Contact contact=new Contact(name:'0123456789'+
-												'0123456789'+
-												'0123456789'+
-												'0123456789'+
-												'0123456789'+
-												'0123456789'+
-												'0123456789'+
-												'0123456789'+
-												'0123456789'+
-												'0123456789'+
-												'0123456789'+
-												'0123456789'+
-												'0123456789'+
-												'0123456789'+
-												'0123456789'+
-												'0123456789'+
-												'0123456789'+
-												'0123456789'+
-												'0123456789'+
-												'0123456789'+
-												'0123456789'+
-												'0123456789'+
-												'0123456789'+
-												'0123456789'+
-												'0123456789'+
-												'0123456789'+
-												'012345')
+			def Contact contact = new Contact(name:
+					'0123456789abcdef'+'0123456789abcdef'+'0123456789abcdef'+'0123456789abcdef'+
+					'0123456789abcdef'+'0123456789abcdef'+'0123456789abcdef'+'0123456789abcdef'+
+					'0123456789abcdef'+'0123456789abcdef'+'0123456789abcdef'+'0123456789abcdef'+
+					'0123456789abcdef'+'0123456789abcdef'+'0123456789abcdef'+'0123456789abcdef')
 	then:
 		!contact.validate()
 	}
