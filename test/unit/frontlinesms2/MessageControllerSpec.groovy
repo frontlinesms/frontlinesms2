6f0c30fd697741f5aeb1a89c956a47faa543ce37
Geoffrey Muchai <geoffreymuchai@gmail.com>
2011-05-10 17:03:32 +0300

    David|Geoffrey|Alex: Added inbound vs outbound controls to Fmessage

--- test/unit/frontlinesms2/MessageControllerSpec.groovy
+++ test/unit/frontlinesms2/MessageControllerSpec.groovy
@@ -4,8 +4,22 @@ import spock.lang.*
 import grails.plugin.spock.*
 
 class MessageControllerSpec extends ControllerSpec {
-	def "feature method"() {
-		
+	def 'inbox closure requests correct messages'() {
+		setup:
+			mockDomain(Fmessage)
+		when:
+			controller.inbox()
+		then:
+			mockParams.inbound
+	}
+
+	def "sent closure requests correct messages"() {
+		setup:
+			mockDomain(Fmessage)
+		when:
+			controller.sent()
+		then:
+			!mockParams.inbound
 	}
 }
 
