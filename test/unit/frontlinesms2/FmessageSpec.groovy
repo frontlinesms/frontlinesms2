66b3d0f86aba6d4630fe5df87e805fe2b90993c4
Geoffrey Muchai <geoffreymuchai@gmail.com>
2011-05-11 14:17:45 +0300

    Geoffrey|Alex: added 'read' flag to Fmessage class.

--- test/unit/frontlinesms2/FmessageSpec.groovy
+++ test/unit/frontlinesms2/FmessageSpec.groovy
@@ -4,5 +4,21 @@ import spock.lang.*
 import grails.plugin.spock.*
 
 class FmessageSpec extends UnitSpec {
+	def 'check that "read" flag cannot be null'() {
+		setup:
+			mockForConstraintsTests(Fmessage)
+		when:
+			Fmessage message = new Fmessage(read: null)
+		then:
+			message.read != null || !message.validate()
+	}
+	
+	def 'check that messages are unread by default'() {
+		when:
+			Fmessage message = new Fmessage()
+		then:
+			message.read == false
+	}
+	
 }
 
