55fe5fbefadd7165c87bcd9f6a07363d15ead7c3
Geoffrey Muchai <geoffreymuchai@gmail.com>
2011-05-12 20:08:05 +0300

    Trying to get compass to work...

---
+++ grails-app/conf/GrassConfig.groovy
@@ -0,0 +1,15 @@
+grass {
+	sass_dir = "./src/stylesheets"
+
+	css_dir = "./web-app/css"
+	
+	images_dir = "web-app/images"
+	
+	// default is true
+	relative_assets = false
+	
+	// other options: nested, expanded, compact, compressed
+	output_style = "nested"
+	
+	line_comments = false
+}
