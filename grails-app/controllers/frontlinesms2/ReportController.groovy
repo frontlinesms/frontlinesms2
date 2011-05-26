9b21bd276335714f1b33c338eed83f1a96bfb9d0
David Kutalek <davkutalek@gmail.com>
2011-05-26 17:37:57 +0300

    David|Geoffrey: Changes to report csv filename

--- grails-app/controllers/frontlinesms2/ReportController.groovy
+++ grails-app/controllers/frontlinesms2/ReportController.groovy
@@ -1,6 +1,9 @@
 package frontlinesms2
 
 import org.grails.plugins.csv.CSVWriter
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.Date
 
 class ReportController {
 
@@ -17,10 +20,21 @@ class ReportController {
 			Date {it.dateCreated}
 		})
 
+		def formatedTime = dateToString(currentTime)
 		Fmessage.findAll().each {
 			csv << [id: it.id, src: it.src, dst: it.dst, text: it.text, dateCreated: it.dateCreated]
 		}
-		response.setHeader("Content-disposition", "attachment; filename=frontlineSMS-messageDump-${currentTime}.csv")
+		response.setHeader("Content-disposition", "attachment; filename=frontlineSMS-messageDump-${formatedTime}.csv")
 		render(contentType:"text/csv", text: csv.writer.toString(), encoding:"UTF-8")
 	}
+
+	String dateToString(Date date) {
+		DateFormat formatedDate = createDateFormat()
+		return formatedDate.format(date)
+	}
+
+	DateFormat createDateFormat() {
+		return new SimpleDateFormat("dd-MMM-yyyy")
+	}
+	
 }
--- grails-app/views/report/_menu.gsp
+++ grails-app/views/report/_menu.gsp
@@ -1,7 +1,7 @@
 <%@ page contentType="text/html;charset=UTF-8" %>
 <ul class="content-menu" id="reports-menu">
 	<li>
-		<h2>Create new...</h2>
+		<h2>Report Generation</h2>
 		<ol id="create-submenu">
 				<li id="create-CSVreport">
 					<g:link class="create" action="create">Generate CSV file of all messages</g:link>
