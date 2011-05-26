f9412e9b3f054d10c6e57445fc1f74c7ec81c047
David Kutalek <davkutalek@gmail.com>
2011-05-26 14:15:59 +0300

    David: Minor fixes- a test was breaking when run with others

--- test/unit/frontlinesms2/ReportControllerSpec.groovy
+++ test/unit/frontlinesms2/ReportControllerSpec.groovy
@@ -6,14 +6,16 @@ import grails.plugin.spock.*
 class ReportControllerSpec extends ControllerSpec {
 	def "generated csv looks right innit"() {
 		given:
-			mockDomain(Fmessage, [new Fmessage(src:'geraldine', text:'hi david u r a hunk')])
+			def gMessage = new Fmessage(src:'geraldine', text:'hi david u r a hunk')
+			mockDomain(Fmessage, [gMessage])
 		when:
 			controller.create()
 			println "renderArgs: ${controller.renderArgs}"
 			println "response: ${controller.response}"
+			def csv = '''"DatabaseID","Source","Destination","Text","Date"
+"''' + gMessage.id + '''","geraldine","null","hi david u r a hunk","null"'''
 		then:
-			controller.renderArgs == [contentType:"text/csv", text:'''"DatabaseID","Source","Destination","Text","Date"
-"1","geraldine","null","hi david u r a hunk","null"''', encoding:"UTF-8"]
+			controller.renderArgs == [contentType:"text/csv", text:csv, encoding:"UTF-8"]
 	}
 }
 
