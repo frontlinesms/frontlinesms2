9cc7016539288e8a0a855fdf2a2bca43d7f49b78
Alex Anderson <alex@frontlinesms.com>
2011-05-27 10:14:40 +0300

    Fixed indentation in PollController.

--- grails-app/controllers/frontlinesms2/PollController.groovy
+++ grails-app/controllers/frontlinesms2/PollController.groovy
@@ -1,30 +1,29 @@
 package frontlinesms2
 
 class PollController {
-
-    def index = {
+	def index = {
 		 redirect(action: "create", params: params)
 	}
 
 	def create = {
 		def pollInstance = new Poll()
-        pollInstance.properties = params
-        [pollInstance: pollInstance]
+		pollInstance.properties = params
+		[pollInstance: pollInstance]
 	}
 
 	def save = {
-        def pollInstance = new Poll(title: params.title)
+		def pollInstance = new Poll(title: params.title)
 		def responseList = params.responses.tokenize().collect() {
 			new PollResponse(value:it)
 		}
 		pollInstance.responses = responseList
 		
 		if (pollInstance.save(flush: true)) {
-            flash.message = "${message(code: 'default.created.poll', args: [message(code: 'poll.label', default: 'Poll'), pollInstance.id])}"
+		flash.message = "${message(code: 'default.created.poll', args: [message(code: 'poll.label', default: 'Poll'), pollInstance.id])}"
 			redirect(controller: "message")
 		} else {
 			println "Something went wrong while saving the poll instance."
-            render(view: "create", model: [pollInstance: pollInstance])
-        }
-    }
+			render(view: "create", model: [pollInstance: pollInstance])
+		}
+	}
 }
