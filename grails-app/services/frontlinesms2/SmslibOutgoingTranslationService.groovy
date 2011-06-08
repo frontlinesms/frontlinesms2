d8d2e9fd1577bdf37fc3b34d46b611292fd986e2
David Kutalek <davkutalek@gmail.com>
2011-06-08 14:56:17 +0300

    David|Alex: Sending of smslibmessages. There is no automatic testing for this, only visual

---
+++ grails-app/services/frontlinesms2/SmslibOutgoingTranslationService.groovy
@@ -0,0 +1,18 @@
+package frontlinesms2
+
+import org.apache.camel.Processor
+import org.apache.camel.Exchange
+
+class SmslibOutgoingTranslationService implements Processor {
+	static transactional = false
+
+	void process(Exchange ex) {
+		def i = ex.in
+		def message = i.body
+		ex.out.body = message
+
+//		def endpoint = i.header
+//		ex.out.setHeader(endpoint, Endpoint)
+	}
+}
+
