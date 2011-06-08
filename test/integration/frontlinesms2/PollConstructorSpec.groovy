7f4c0a01c0ecc84ef673ee66a63e7fc233bb92a6
Alex Anderson <alex@frontlinesms.com>
2011-06-08 19:00:52 +0300

    Cleaned up code and twiddled tests a little.

--- test/integration/frontlinesms2/PollConstructorSpec.groovy
+++ test/integration/frontlinesms2/PollConstructorSpec.groovy
@@ -1,12 +1,12 @@
 package frontlinesms2
 
 class PollConstructorSpec extends grails.plugin.spock.IntegrationSpec {
-	def "creating a new poll also creates a poll response with value 'Unknown'"() {
+	def "creating a new poll also creates a poll response with value UNKNOWN"() {
 		when:
-			def responseList = [new PollResponse(value:'one'), new PollResponse(value:'other')]
+			def responseList = [new PollResponse(value:'One'), new PollResponse(value:'Other')]
 			def p = Poll.createPoll('This is a poll', responseList)
 		then:
 			p.responses.size() == 3
-
+			p.responses*.value.sort() == ['One', 'Other', 'Unknown']
 	}
 }
