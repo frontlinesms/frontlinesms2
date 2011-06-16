48877eab7487c6f070ff9d2a41dac631d7d00797
vinodkumaar <vinodkumaar@gmail.com>
2011-06-16 17:29:25 +0530

    [#TechTask] Vinod: Renamed the specification to enable it to work in windows

--- test/unit/frontlinesms2/SearchControllerSpec.groovy
+++ test/unit/frontlinesms2/SearchControllerSpec.groovy
@@ -2,16 +2,14 @@ package frontlinesms2
 
 import grails.plugin.spock.*
 
-class SearchControllerSpec extends ControllerSpec{
+class SearchControllerSpec extends ControllerSpec {
 	
-	def "default action is 'show'"() {
+	def "default action is LIST"() {
 		when:
 			controller.index()
 		then:
 			controller.redirectArgs.controller == 'search' || !controller.redirectArgs.controller
 			controller.redirectArgs.action == 'list'
 	}
-	
-	
 }
 
