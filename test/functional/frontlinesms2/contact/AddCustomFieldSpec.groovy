94077159627d74570e56d15ace9a992169e67ad4
David Kutalek <davkutalek@gmail.com>
2011-06-24 18:28:19 +0300

    David: Added another functional test for Custom Fields

---
+++ test/functional/frontlinesms2/contact/AddCustomFieldSpec.groovy
@@ -0,0 +1,39 @@
+package frontlinesms2.contact
+
+import frontlinesms2.*
+import java.util.regex.*
+
+class AddCustomFieldSpec extends grails.plugin.geb.GebSpec {
+	def "selecting add custom field from dropdown opens the popup"() {
+		when:
+			Contact bob = new Contact(name:'Bob').save(failOnError: true, flush: true)
+			go "contact/show/${bob.id}"
+			$("#new-field-dropdown").value('add-new')
+			waitFor {$('div#custom-field-popup').displayed}
+		then:
+			$('div#custom-field-popup').displayed
+	}
+
+	def "should add the manually entered custom fields to the list "() {
+		when:
+			Contact bob = new Contact(name:'Bob').save(failOnError: true, flush: true)
+			go "contact/show/${bob.id}"
+			$("#new-field-dropdown").value('add-new')
+			waitFor {$('div#custom-field-popup').displayed}
+			$("#custom-field-name").value("planet")
+			$("#custom-field-value").value("mars")
+			def btnCreate = $('.buttons .create')
+			btnCreate.click()
+		then:
+			$('#custom-field-list li').find('label').text() == "planet"
+			$('#custom-field-list li').find('input').value() == "mars"
+	}
+}
+
+class CustomFieldPage extends geb.Page {
+	static url = 'contact/show'
+	static content = {
+		selectedMenuItem { $('#messages-menu .selected') }
+		messagesList { $('#messages-submenu') }
+	}
+}
