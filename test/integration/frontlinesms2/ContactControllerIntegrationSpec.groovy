b823c420850191f823a467c2f64bb06a41e12227
Vinod Kumaar R <vinodkumaar@gmail.com>
2011-06-27 11:51:55 +0530

    [Tech-task] Vinod: Cleaning up MessageOwner table after tests

--- test/integration/frontlinesms2/ContactControllerIntegrationSpec.groovy
+++ test/integration/frontlinesms2/ContactControllerIntegrationSpec.groovy
@@ -12,7 +12,7 @@ class ContactControllerIntegrationSpec extends grails.plugin.spock.IntegrationSp
 	}
 
 	def cleanup() {
-		MessageOwner.findAll()*.delete(flush:true, failOnError:true)
+		MessageOwner.list()*.delete(flush:true, failOnError:true)
 		Contact.findAll()*.delete(flush:true, failOnError:true)
 		Group.findAll()*.delete(flush:true, failOnError:true)
 	}
