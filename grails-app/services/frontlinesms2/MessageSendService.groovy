b8b19849e34e83070f01b8c4ee5089810b27795f
David Kutalek <davkutalek@gmail.com>
2011-06-28 17:39:41 +0300

    David: changes to message sending. Still not working, but closer

--- grails-app/services/frontlinesms2/MessageSendService.groovy
+++ grails-app/services/frontlinesms2/MessageSendService.groovy
@@ -1,12 +1,10 @@
 package frontlinesms2
 
-import org.apache.camel.Exchange
-
 class MessageSendService {
     static transactional = true
 
     def dispatch(Fmessage m, SmslibFconnection c) {
 		m.save(failOnError:true, flush:true)
-		sendMessage('seda:smslib-outgoing-fmessages', [body:m, endpoint:c.camelProducerAddress()])
-    }
+		sendMessage('seda:smslib-outgoing-fmessages', m, [fconnection:c.id])
+	}
 }
--- grails-app/services/frontlinesms2/SmslibOutgoingTranslationService.groovy
+++ grails-app/services/frontlinesms2/SmslibOutgoingTranslationService.groovy
@@ -2,14 +2,22 @@ package frontlinesms2
 
 import org.apache.camel.Processor
 import org.apache.camel.Exchange
+import org.smslib.CMessage
+import net.frontlinesms.camel.smslib.OutgoingSmslibCamelMessage
 
 class SmslibOutgoingTranslationService implements Processor {
 	static transactional = false
 
 	void process(Exchange ex) {
-		def i = ex.in
-		def message = i.body
-		ex.out.body = message
+		def messageIn = ex.in.body
+		println "message is: $messageIn"
+//		def msg = new CMessage(2, '0702231301', '0703535226', messageIn.text)
+//		OutgoingSmslibCamelMessage msg = null
+
+
+//		ex.out.body = c
+		ex.out = new OutgoingSmslibCamelMessage() {}
+//		println "message text is: ${ex.out}"
 	}
 }
 
