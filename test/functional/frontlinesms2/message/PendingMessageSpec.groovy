3987f060f030a623275468209a09ddde4e8a4d53
Prabha <prabhap@thoughtworks.com>
2011-06-28 15:35:40 +0530

    [CORE-187] Prabha: Did Minor refactoring

--- test/functional/frontlinesms2/message/PendingMessageSpec.groovy
+++ test/functional/frontlinesms2/message/PendingMessageSpec.groovy
@@ -9,24 +9,29 @@ import frontlinesms2.*
 import frontlinesms2.enums.MessageStatus
 
 class PendingMessageSpec extends grails.plugin.geb.GebSpec {
+	def setup() {
+		new Fmessage(src: "src1", status: MessageStatus.SEND_FAILED).save(flush: true)
+		new Fmessage(src: "src2", status: MessageStatus.SEND_PENDING).save(flush: true)
+		new Fmessage(src: "src", status: MessageStatus.SENT).save(flush: true)
+		new Fmessage(src: "src", status: MessageStatus.INBOUND).save(flush: true)
+	}
+
+	def cleanup() {
+		Fmessage.findAll().each() {
+			it.refresh()
+			it.delete(failOnError:true, flush:true)
+		}
+	}
 
 	def 'should list all the pending messages'() {
-		given:
-			new Fmessage(src: "src1", status: MessageStatus.SEND_FAILED).save(flush: true)
-			new Fmessage(src: "src2", status: MessageStatus.SEND_PENDING).save(flush: true)
-			new Fmessage(src: "src", status: MessageStatus.SENT).save(flush: true)
-			new Fmessage(src: "src", status: MessageStatus.INBOUND).save(flush: true)
 		when:
 			to MessagesPage
 			$(".pending").click()
-			waitFor { title == "Pending" } 
+			waitFor { title == "Pending" }
 			def messages = $('#messages tbody tr')
 		then:
 			messages.size() == 2
 			messages*.getAttribute('class').each {it.contains("SEND_FAILED") || it.contains("SEND_PENDING")}
 		    messages.collect { it.find("td:nth-child(1) a")[0].text()}.containsAll(["src1", "src2"])
-		cleanup:
-			Fmessage.list()*.delete()
 	}
-    
 }
