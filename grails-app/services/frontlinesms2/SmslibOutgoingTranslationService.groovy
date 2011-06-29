6ff1ee8587bde37fbffbccef4629ce8d66a6454d
David Kutalek <davkutalek@gmail.com>
2011-06-29 17:22:38 +0300

    Geoffrey|David: Sending a test message to a particular connection now seems to be working

--- grails-app/services/frontlinesms2/SmslibOutgoingTranslationService.groovy
+++ grails-app/services/frontlinesms2/SmslibOutgoingTranslationService.groovy
@@ -2,29 +2,17 @@ package frontlinesms2
 
 import org.apache.camel.Processor
 import org.apache.camel.Exchange
-import org.smslib.CMessage
-import net.frontlinesms.camel.smslib.OutgoingSmslibCamelMessage
-import org.apache.camel.impl.DefaultMessage
 import org.smslib.COutgoingMessage
 
 class SmslibOutgoingTranslationService implements Processor {
 	static transactional = false
 
 	void process(Exchange ex) {
-		def messageIn = ex.in.body
-		println "exchange in: ${ex.in}"
-		println "exchange in body: ${ex.in.body}"
-		println "message is: $messageIn"
-//		def msg = new CMessage(2, '0702231301', '0703535226', messageIn.text)
-//		OutgoingSmslibCamelMessage msg = null
-		def cmsg = new COutgoingMessage("123","wewewe")
-		println "outgoing cmessage: $cmsg"
-		println "outgoing message text: ${cmsg.text}"
-//		ex.out.body = c
+		def msgIn = ex.in.body
+		def msgTxt = msgIn.text
+		def msgDst = msgIn.dst
+		def cmsg = new COutgoingMessage("$msgDst","$msgTxt")
 		ex.out.body = cmsg
-
-		println "outgoing exchange is: ${ex.out}"
-		println "outgoing exchange body is: ${ex.out.body}"
 	}
 }
 
