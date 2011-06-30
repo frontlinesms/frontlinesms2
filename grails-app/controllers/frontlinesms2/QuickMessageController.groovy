6ac3025817a770256397cccf042fe952da6a4255
Prabha <prabhap@thoughtworks.com>
2011-06-30 09:44:24 +0530

    [CORE-249] Prabha: Removed unnecessary printlns in the code

--- grails-app/controllers/frontlinesms2/QuickMessageController.groovy
+++ grails-app/controllers/frontlinesms2/QuickMessageController.groovy
@@ -4,8 +4,6 @@ class QuickMessageController {
 	def create = {
 		def recipients = [params['recipient']]
 		def contacts = Contact.list()
-		println contacts
-		println recipients
 
 		[contactList: contacts,
 			groupList:GroupMembership.getGroupDetails(),
