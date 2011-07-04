b4a91ad423d281107e0ae280aa81a8ec3a8e4091
Geoffrey Muchai <geoffreymuchai@gmail.com>
2011-07-04 11:33:23 +0300

    Added tests for exporting search results to PDF and CSS formats

--- test/unit/frontlinesms2/ReportControllerSpec.groovy
+++ test/unit/frontlinesms2/ReportControllerSpec.groovy
@@ -15,30 +15,5 @@ class ReportControllerSpec extends ControllerSpec {
 		then:
 			controller.renderArgs == [contentType:"text/csv", text:csv, encoding:"UTF-8"]
 	}
-	
-	def "csv file is generated from provided list of messages"() {
-		given:
-			def gMessage = new Fmessage(src:'gerad', text:'i love pie')
-			def gMessage2 = new Fmessage(src:'steve', text:'i hate beef')
-			mockDomain(Fmessage, [gMessage, gMessage2])
-		when:
-			controller.generateCSVReport([gMessage, gMessage2])
-			def csv = '''"DatabaseID","Source","Destination","Text","Date"
-"''' + gMessage.id + '''","gerad","null","i love pie","null"
-"''' + gMessage2.id + '''","steve","null","i hate beef","null"'''
-		then:
-			controller.renderArgs == [contentType:"text/csv", text:csv, encoding:"UTF-8"]
-	}
-	
-	def "pdf file is generated from provided list of messages"() {
-		given:
-			def gMessage = new Fmessage(src:'gerad', text:'i love pie')
-			def gMessage2 = new Fmessage(src:'steve', text:'i hate beef')
-			mockDomain(Fmessage, [gMessage, gMessage2])
-		when:
-			def model = controller.generatePDFReport([gMessage, gMessage2])
-		then:
-			model == [gMessage, gMessage2]
-	}
 }
 
