bff0ec6ae5aeb4781df2b2d6ae1c0795cb3cabf7
Prabha <prabhap@thoughtworks.com>
2011-07-06 15:31:24 +0530

    [CORE-271] Prabha/Lokesh: Added Processor and route for processing subscription messages

---
+++ grails-app/services/frontlinesms2/SubscriptionService.groovy
@@ -0,0 +1,28 @@
+package frontlinesms2
+
+import org.apache.camel.Exchange
+import org.apache.camel.Processor
+
+class SubscriptionService implements Processor {
+	static transactional = false
+
+	void process(Exchange exchange) {
+		Fmessage message = getMessage(exchange)
+		def address = message.src
+		def firstWord = message.text.split(" ")[0]
+		def groupToAdd = Group.findBySubscriptionKey(firstWord)
+		def groupToRemove = Group.findByUnsubscriptionKey(firstWord)
+		groupToAdd?.addToMembers(findOrCreateContact(address))
+		groupToRemove?.removeFromMembers(findOrCreateContact(address))
+		groupToAdd?.save()
+		groupToRemove?.save()
+	}
+
+	private Contact findOrCreateContact(String src) {
+		return Contact.findByAddress(src) ?: new Contact(address: src)
+	}
+	
+	 Fmessage getMessage(Exchange exchange) {
+		return new Fmessage()
+	}
+}
