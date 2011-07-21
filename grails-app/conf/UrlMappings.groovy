097e0b7e9da55eee4c6036f8b4571f11d8c4da15
Prabha <prabhap@thoughtworks.com>
2011-07-21 18:17:13 +0530

    [CORE-352] Prabha: Changed url mappings for pending sent and trash messages

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -8,6 +8,9 @@ class UrlMappings {
 		}
 		
 		"/message/inbox/show/$messageId"(controller:'message', action:'inbox') {}
+		"/message/sent/show/$messageId"(controller:'message', action:'sent') {}
+		"/message/pending/show/$messageId"(controller:'message', action: 'pending') {}
+		"/message/trash/show/$messageId"(controller:'message', action: 'trash') {}
 
 		"/message/poll/$ownerId"(controller:'message', action:'poll') {}
 		"/message/poll/$ownerId/show/$messageId"(controller:'message', action:'poll') {}
