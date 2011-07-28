21abd0b16c8ff4579f97348290000a6cbf278365
Geoffrey <geoffreymuchai@gmail.com>
2011-07-28 14:52:27 +0300

    Fixed message buttons to work when a user unchecks all messages

--- web-app/js/message/actions.js
+++ web-app/js/message/actions.js
@@ -15,7 +15,7 @@ function setStarStatus(object,data){
 	}	
 }
 
-var messageDetails;
+var selectedMessageId;
 
 function checkAllMessages(){
 	if($(':checkbox')[0].checked){
@@ -45,25 +45,30 @@ function updateMessageDetails(id){
 	
 	var count = countCheckedMessages();
 	if(count == 1 && $(':checkbox').size() != 2) {
-		loadCheckedMessage();
+		if(selectedMessageId != $('tr :checkbox[checked="true"]').val())
+			loadMessage($('tr :checkbox[checked="true"]').val(), true);
 	}
 	if(count > 1){
 		changeMessageCount(count);
 	}
 }
 
-function loadCheckedMessage() {
+function loadMessage(id, checked) {
 	var location = window.location.pathname
 	var messageSection = $('input:hidden[name=messageSection]').val();
 	var ownerId = $('input:hidden[name=ownerId]').val();
-	var messageId = $('tr :checkbox[checked="true"]').val();
+	var messageId = id;
+	if(checked){
+		messageId = "?messageId="+messageId+"&checkedId="+messageId;
+	}
+	
 	
 	if(messageSection == 'sent' || messageSection == 'pending' || messageSection == 'trash') {
-		window.location = "/frontlinesms2/message/"+messageSection+"?messageId="+messageId+"&checkedId="+messageId;
+		window.location = "/frontlinesms2/message/"+messageSection+messageId;
 	}else if(messageSection == 'poll' || messageSection == 'folder'){
-		window.location = "/frontlinesms2/message/"+messageSection+"/"+ownerId+"/show/"+messageId+"?checkedId="+messageId;
+		window.location = "/frontlinesms2/message/"+messageSection+"/"+ownerId+"/show/"+messageId;
 	} else{
-		window.location = "/frontlinesms2/message/"+messageSection+"/show/"+messageId+"?checkedId="+messageId;
+		window.location = "/frontlinesms2/message/"+messageSection+"/show/"+messageId;
 	}
 }
 
@@ -93,9 +98,7 @@ function validateCheckedMessageCount(count) {
 }
 
 function changeMessageCount(count){
-	if(messageDetails == null){
-		messageDetails = $('#message-details').html();
-	}
+	setSelectedMessage();
 	$('#message-details').empty();
 	$('#message-details').append("<p> "+count+" messages selected</p>");
 	setMessageActions();
@@ -142,12 +145,17 @@ function deleteAllClickAction() {
 	});
 }
 
+function setSelectedMessage() {
+	if(selectedMessageId == null){
+		selectedMessageId = $('tr.selected').attr('id').substring('message-'.length);
+	}
+}
+
 function showMessageDetails(){
-	if(messageDetails == null){
+	if(selectedMessageId == null){
 		return;
 	}
-	$('#message-details').empty();
-	$('#message-details').html(messageDetails);
+	loadMessage(selectedMessageId,false);
 }
 
 function highlightRow(id){
