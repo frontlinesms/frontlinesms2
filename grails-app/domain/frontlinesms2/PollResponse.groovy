da6b5f3afbf72f016582b5827b6c203028a52615
Loki <lokeshwer@gmail.com>
2011-08-05 16:55:59 +0530

    [CORE-336] Lokesh, Added poll shortcuts for responses

--- grails-app/domain/frontlinesms2/PollResponse.groovy
+++ grails-app/domain/frontlinesms2/PollResponse.groovy
@@ -3,8 +3,10 @@ package frontlinesms2
 class PollResponse extends MessageOwner {
 	static transients = ['liveMessageCount']
 	String value
+	String key
 	static constraints = {
 		value(blank: false, nullable: false, maxSize: 255)
+		key(nullable: true)
 	} // having a unique value here makes all reponses across all polls unique - not what we want right now
 
 	def getLiveMessageCount() {
