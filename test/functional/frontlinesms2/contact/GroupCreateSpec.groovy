c5215c895c14d1f7a8666588ec7f6630e7f6b14a
Geoffrey <geoffreymuchai@gmail.com>
2011-08-16 10:25:42 +0300

    Fixed failing tests in GroupCreateSpec

--- test/functional/frontlinesms2/contact/GroupCreateSpec.groovy
+++ test/functional/frontlinesms2/contact/GroupCreateSpec.groovy
@@ -10,25 +10,25 @@ class GroupCreateSpec extends grails.plugin.geb.GebSpec {
 		}
 	}
 
+	def 'Errors are displayed when group fails to save'() {
+		when:
+			to CreateGroupPage
+			$("li#create-group a").click()
+			$("#done").click()
+		then:
+			errorMessages.present
+	}
+	
 	def 'button to save new group is displayed and works'() {
 		when:
 			to CreateGroupPage
 			def initNumGroups = Group.count()
 			$("li#create-group a").click()
 			$('input', name: "name").value('People')
-			btnSave.click()
+			$("#done").click()
 		then:
 			at ContactListPage
 			assert Group.count() == (initNumGroups + 1)
-	}
-
-	def 'Errors are displayed when group fails to save'() {
-		when:
-			to CreateGroupPage
-			$("li#create-group a").click()
-			btnSave.click()
-		then:
-			errorMessages.present
 	}
 }
 
@@ -37,9 +37,8 @@ class CreateGroupPage extends geb.Page {
 	static at = {
 		title.endsWith('Create Group')
 	}
-
+	
 	static content = {
-		btnSave { $("#done") }
 		errorMessages { $('.flash.message') }
 	}
 }
