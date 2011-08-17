4f82c620ab7bd5acb56c0311c6e64f050e5c1386
Prabha <prabhap@thoughtworks.com>
2011-08-17 15:22:17 +0530

    [CORE-175] Prabha : getRecipientDisplayName should display the number if the contact does not exist.

--- test/unit/frontlinesms2/FmessageSpec.groovy
+++ test/unit/frontlinesms2/FmessageSpec.groovy
@@ -90,11 +90,13 @@ class FmessageSpec extends UnitSpec {
 	def "should fetch the display name of the recipient"() {
 		setup:
 			mockDomain Contact, [new Contact(name: "John", primaryMobile: "dst")]
-			mockDomain Fmessage, [new Fmessage(src: "src", dst: "dst")]
+			mockDomain Fmessage, [new Fmessage(src: "src", dst: "dst"), new Fmessage(src: "src1", dst: "dst1")]
 		when:
 			def message = Fmessage.findBySrc("src")
+			def message1 = Fmessage.findBySrc("src1")
 		then:
-			message.getRecipientDisplayName() == "john"
+			message.getRecipientDisplayName() == "John"
+			message1.getRecipientDisplayName() == "dst1"
 
 	}
 }
