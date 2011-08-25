83577c6c723e2c84cd73844d585577710c807820
Geoffrey <geoffreymuchai@gmail.com>
2011-08-25 23:34:46 +0300

    Added unit test for verifying model returned by multipleContactGroupList

--- test/unit/frontlinesms2/ContactControllerSpec.groovy
+++ test/unit/frontlinesms2/ContactControllerSpec.groovy
@@ -23,5 +23,23 @@ class ContactControllerSpec extends ControllerSpec {
 			result.contactInstanceList == [contact2, contact1, contact3]
 			result.contactInstanceList != [contact1, contact2, contact3]
 	}
+	
+	def "should render model containing shared and unshared groups"() {
+		setup:
+			def alice = new Contact(name: "Alice", primaryMobile: "12345")
+			def bob = new Contact(name: "Bob", primaryMobile: "54321")
+			mockDomain(Contact, [alice, bob])
+			mockDomain(Group, [new Group(name: "group1"), new Group(name: "group2")])
+			mockDomain GroupMembership, [new GroupMembership(group: Group.findByName('group1'), contact: alice),
+				new GroupMembership(group: Group.findByName('group1'), contact: bob) ,
+				new GroupMembership(group: Group.findByName('group2'), contact: bob)]
+			mockParams.contactIds = "$alice.id, $bob.id"
+		when:
+			controller.multipleContactGroupList()
+			def model = controller.modelAndView.model
+		then:
+			model.sharedGroupInstanceList == [Group.findByName('group1')]
+			model.nonSharedGroupInstanceList == [Group.findByName('group2')]
+	}
 }
 
