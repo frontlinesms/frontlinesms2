8fd0650dfe9e413381f1cdcd10d983ac87e03893
David Kutalek <davkutalek@gmail.com>
2011-09-01 19:05:14 +0300

    David: Simplifying Message Controller. not quite done

--- grails-app/controllers/frontlinesms2/MessageController.groovy
+++ grails-app/controllers/frontlinesms2/MessageController.groovy
@@ -119,22 +119,8 @@ class MessageController {
 		flash.message = "Message has been queued to send to " + addresses.unique().join(", ")
 		redirect (action: 'sent')
 	}
-	
+
 	def delete = {
-		withFmessage {messageInstance ->
-			messageInstance.toDelete()
-			messageInstance.save(failOnError: true, flush: true)
-		}
-		flash.message = "${message(code: 'default.deleted.message', args: [message(code: 'message.label', default: ''), 'message'])}"
-		if (isAjaxRequest()) {
-			render ""
-		}else {
-			if(params.messageSection == 'search') redirect(controller: params.messageSection)
-			else redirect(action: params.messageSection, params: [ownerId: params.ownerId])
-		}
-	}
-	
-	def deleteAll = {
 		def messageIdList = params.checkedMessageList ? params.checkedMessageList.tokenize(',') : [params.messageId]
 		messageIdList.each { id ->
 			withFmessage id, {messageInstance ->
@@ -152,21 +138,7 @@ class MessageController {
 	}
 
 	def archive = {
-		withFmessage { messageInstance ->
-			messageInstance.archive()
-			messageInstance.save(failOnError: true, flush: true)
-		}
-		flash.message = "${message(code: 'default.archived.message', args: [message(code: 'message.label', default: ''), 'messages'])}"
-		if (request.xhr) {
-			render ""
-		}else {
-			if(params.messageSection == 'search') redirect(controller: params.messageSection)
-			else redirect(action: params.messageSection, params: [ownerId: params.ownerId])
-		}
-	}
-	
-	def archiveAll = {
-		def messageIdList = params.checkedMessageList?.tokenize(',')
+		def messageIdList = params.checkedMessageList ? params.checkedMessageList.tokenize(',') : [params.messageId]
 		messageIdList.each { id ->
 			withFmessage id, {messageInstance ->
 				messageInstance.archive()
@@ -182,28 +154,8 @@ class MessageController {
 		}
 	}
 
-
 	def move = {
-		withFmessage {messageInstance ->
-			def messageOwner
-			if (params.messageSection == 'poll') {
-				messageOwner = Poll.get(params.ownerId)
-			} else if (params.messageSection == 'folder') {
-				messageOwner = Folder.get(params.ownerId)
-			}
-			if (messageOwner instanceof Poll) {
-				def unknownResponse = messageOwner.getResponses().find { it.value == 'Unknown'}
-				unknownResponse.addToMessages(Fmessage.get(params.messageId) ?: messageInstance).save(failOnError: true, flush: true)
-			} else if (messageOwner instanceof Folder) {
-				messageOwner.addToMessages(Fmessage.get(params.messageId) ?: messageInstance).save(failOnError: true, flush: true)
-			}
-		}
-		flash.message = "${message(code: 'default.updated.message', args: [message(code: 'message.label', default: ''), ' messages'])}"
-		render ""
-	}
-	
-	def moveAll = {
-		def messageIdList = params.messageId?.tokenize(',')
+		def messageIdList = params.checkedMessageList ? params.checkedMessageList.tokenize(',') : [params.messageId]
 		messageIdList.each { id ->
 			withFmessage id, {messageInstance ->
 				def messageOwner
@@ -225,16 +177,7 @@ class MessageController {
 	}
 
 	def changeResponse = {
-		withFmessage { messageInstance ->
-			def responseInstance = PollResponse.get(params.responseId)
-			responseInstance.addToMessages(messageInstance).save(failOnError: true, flush: true)
-		}
-		flash.message = "${message(code: 'default.updated.message', args: [message(code: 'message.label', default: 'Fmessage'), 'message'])}"
-		render ""
-	}
-	
-	def changeAllResponses = {
-		def messageIdList = params.messageId?.tokenize(',')
+		def messageIdList = params.checkedMessageList ? params.checkedMessageList.tokenize(',') : [params.messageId]
 		messageIdList.each { id ->
 			withFmessage id, { messageInstance ->
 				println messageInstance
