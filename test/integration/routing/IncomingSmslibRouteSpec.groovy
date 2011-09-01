4a50c58200a17d92b76f2a08f163f6be4a273f04
Alex Anderson <alex@frontlinesms.com>
2011-09-01 20:26:13 +0300

    Fleshed out implementation of IncomingSmslibRouteSpec.

--- test/integration/routing/IncomingSmslibRouteSpec.groovy
+++ test/integration/routing/IncomingSmslibRouteSpec.groovy
@@ -6,25 +6,32 @@ import frontlinesms2.dev.MockModemUtils
 import serial.mock.*
 
 class IncomingSmslibRouteSpec extends CamelIntegrationSpec {
+	def fconnectionService
+	
 	String getFrom() { '' }
 	String getTo() { '' }
 	
 	def "should translate a CIncomingMessage into a Fmessage then save it then deliver it to KeywordProcessor"() {
 		given:
+			def mockPortHandler = MockModemUtils.createMockPortHandler([1:'0123456789abcdef']) /* TODO insert proper PDU payload */
 			// initialise mock serial device with message available
  			MockModemUtils.initialiseMockSerial(['/def/test-modem': new CommPortIdentifier("COM99",
-					MockModemUtils.createMockPortHandler([1:'0123456789abcdef'])) /* TODO insert proper PDU payload */ ])
-			def c = 0
+					mockPortHandler)])
+			// start route
+			def connection = new SmslibFconnection('/def/test-modem').save(failOnError:true)
+			fconnectionService.createRoutes(connection)
 		when:
-			// TODO wait for message to be read from mock serial device
-			++c
+			// wait for message to be read from mock serial device
+			while(mockPortHandler.hasMessages()) { /* wait */ }
 		then:
-			// TODO assert Fmessage is saved
+			// assert Fmessage is saved
+			Fmessage.findAll().size() == 1
+			// TODO check Fmessage content?
 			// TODO assert KeywordProcessor is called
-			false
 		cleanup:
-			// TODO stop mock serial connection
-			// TODO remove mock serial port
-			c = false
+			// stop route
+			fconnectionService.destroyRoutes(connection)
+			// remove mock serial port
+			MockSerial.reset()
 	}
 }
