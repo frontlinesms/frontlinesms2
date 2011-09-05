28167ca24459c37fc8b9d235a2e7bb134b586642
Prabha <prabhap@thoughtworks.com>
2011-09-05 11:37:18 +0530

    Prabha: Fixed couple of failing tests

--- test/unit/frontlinesms2/SearchControllerUnitSpec.groovy
+++ test/unit/frontlinesms2/SearchControllerUnitSpec.groovy
@@ -3,20 +3,27 @@ package frontlinesms2
 import grails.plugin.spock.ControllerSpec
 
 class SearchControllerUnitSpec extends ControllerSpec {
+
+	def setup() {
+		registerMetaClass(Fmessage)
+		Fmessage.metaClass.static.countAllSearchMessages = {params ->
+		}
+		mockDomain(Fmessage)
+		mockDomain(Contact)
+		mockDomain(Group)
+		mockDomain(Poll)
+		mockDomain(Folder)
+
+	}
+
 	def "should set max and offset parameters if not sent"() {
 		setup:
-			registerMetaClass(Fmessage)
 			def searchResults = [new Fmessage()]
 			Fmessage.metaClass.static.search = {params ->
 				assert params.max == 10
 				assert params.offset == 0
 				searchResults
 			}
-			mockDomain(Fmessage)
-			mockDomain(Contact)
-			mockDomain(Group)
-			mockDomain(Poll)
-			mockDomain(Folder)
 			mockParams.max = 10
 			controller.beforeInterceptor()
 		when:
@@ -27,18 +34,12 @@ class SearchControllerUnitSpec extends ControllerSpec {
 
 	def "should send the max and offset parameters to db if sent"() {
 		setup:
-			registerMetaClass(Fmessage)
 			def searchResults = [new Fmessage()]
 			Fmessage.metaClass.'static'.search = {params ->
 				assert params.max == 5
 				assert params.offset == 7
 				searchResults
 			}
-			mockDomain(Fmessage)
-			mockDomain(Contact)
-			mockDomain(Group)
-			mockDomain(Poll)
-			mockDomain(Folder)
 			mockParams.max = 5
 		    mockParams.offset = 7
 			controller.beforeInterceptor()
