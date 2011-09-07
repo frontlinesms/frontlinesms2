d0dd9be757b2f50736db7760c4bda9abb55732ce
Prabha <prabhap@thoughtworks.com>
2011-09-07 13:11:03 +0530

    Prabha: uncommenting SimpleRouteSpec

--- test/integration/frontlinesms2/SimpleRouteSpec.groovy
+++ test/integration/frontlinesms2/SimpleRouteSpec.groovy
@@ -3,16 +3,23 @@ package frontlinesms2
 import routing.CamelIntegrationSpec
 
 class SimpleRouteSpec extends CamelIntegrationSpec {
-//	def "test simple route"() {
-//		given:
-//			resultEndpoint.expectedBodiesReceived("hello")
-//		when:
-//			template.sendBodyAndHeaders("hello", [:])
-//		then:
-//			resultEndpoint.assertIsSatisfied()
-//	}
-//
-	String getTestRouteFrom() { '' }
-	String getTestRouteTo() { '' }
+	def "test simple route"() {
+		given:
+			resultEndpoint.expectedBodiesReceived("hello")
+		when:
+			template.sendBodyAndHeaders("hello", [:])
+		then:
+			resultEndpoint.assertIsSatisfied()
+	}
+
+	@Override
+	String getFrom() {
+		'seda:simple'
+	}
+
+	@Override
+	String getTo() {
+		'seda:simple'
+	}
 }
 
