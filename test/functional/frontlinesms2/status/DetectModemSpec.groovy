b4ad9221551f22ac98ff813107492e6afa230f8d
Alex Anderson <alex@frontlinesms.com>
2011-09-12 12:57:38 +0300

    Added tests for displaying details of detected devices on status tab.

---
+++ test/functional/frontlinesms2/status/DetectModemSpec.groovy
@@ -0,0 +1,60 @@
+package frontlinesms2.status
+
+import frontlinesms2.*
+
+class DetectModemSpec extends grails.plugin.geb.GebSpec {
+	def deviceDetectionService
+
+	def 'DETECT MODEMS button should be visible on STATUS tab'() {
+		when:
+			go StatusPage
+		then:
+			detectModems.displayed
+	}
+	
+	def 'DETECTED DEVICES section should be visible on STATUS tab'() {
+		when:
+			go StatusPage
+		then:
+			detectedDevicesSection.displayed
+			detectedDevicesSection.find('h2').text() == 'Detected devices'
+			noDevicesDetectedNotification.visible
+			noDevicesDetectedNotification.text() == 'No devices have been detected yet.'
+			!detectedDevicesList.visible
+	}
+
+	def 'DETECTED DEVICES list should appear when a device has been detected'() {
+		when:
+			deviceDetectionService.publishDetection new DetectedDevice(port:'COM1', description:'Kiwanja T1 Test Modem')
+			go StatusPage
+		then:
+			!noDevicesDetectedNotification.visible
+			detectedDevicesList.visible
+		cleanup:
+			deviceDetectionService.reset()
+	}
+
+	def 'DETECTED DEVICES list should update when a device detection is published'() {
+		when:
+			go StatusPage
+		then:
+			noDevicesDetectedNotification.visible
+			!detectedDevicesList.visible
+			detectedDevicesList.find('li').size() == 0
+		when:
+			deviceDetectionService.publishDetection new DetectedDevice(port:'COM2', description:'Kiwanja T2 Test Modem')
+		when:
+			waitFor { detectedDevicesList.find('li').size() == 1 }
+		cleanup:
+			deviceDetectionService.reset()
+	}
+}
+
+class StatusPage extends geb.Page {
+	static url = 'status'
+	static content = {
+		detectModems { $('.button', href:'status/detectModems') }
+		detectedDevicesSection { $('div#detectedDevices') }
+		noDevicesDetectedNotification { detectedDevicesSection.find('p') }
+	}
+}
