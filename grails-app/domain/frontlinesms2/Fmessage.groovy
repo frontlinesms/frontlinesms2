b112bb6de8a18b60ce7b1566945bc0aaddc83f59
David Kutalek <davkutalek@gmail.com>
2011-09-15 18:54:13 +0300

    David: bug fixes

--- grails-app/domain/frontlinesms2/Fmessage.groovy
+++ grails-app/domain/frontlinesms2/Fmessage.groovy
@@ -25,9 +25,8 @@ class Fmessage {
 
 	def beforeInsert = {
 		dateCreated = dateCreated ? dateCreated : new Date()
-		if(status==MessageStatus.INBOUND? src: dst) updateContactName()
+		if(status == MessageStatus.INBOUND ? src : dst) updateContactName()
 	}
-	
 	
 	private String findContact(String number) {
 		return Contact.findByPrimaryMobile(number)?.name ?: (Contact.findBySecondaryMobile(number)?.name ?: number)
@@ -39,9 +38,8 @@ class Fmessage {
 				return Contact.findByPrimaryMobile(number)?.name ?: (Contact.findBySecondaryMobile(number)?.name ?: number)
 			}
 		}
-		contactName = fetchContactName(status==MessageStatus.INBOUND? src: dst)
-		if(contactName != src && contactName != dst) contactExists = true
-		else contactExists = false
+		contactName = fetchContactName(status == MessageStatus.INBOUND ? src : dst)
+		contactExists = contactName && contactName != src && contactName != dst
 	}
 	
 	static constraints = {
