9ea8806a416f46591c5a7146f611a12eb91776c2
Olivier Vernin <overnin@gmail.com>
2011-09-19 16:19:17 +0300

    Olivier create optimized query for customfield

--- grails-app/domain/frontlinesms2/CustomField.groovy
+++ grails-app/domain/frontlinesms2/CustomField.groovy
@@ -18,4 +18,18 @@ class CustomField {
 		}
 		uniqueNameList
 	}
+	
+	static def getAllContactNameMatchingCustomField(customFields){
+		def matchingString = ""
+		def conditionString 
+		customFields.each{ name, value -> 
+//			println "starting namestring:"+matchingString+" name:"+name+" value:"+value
+			conditionString = " WHERE  name='"+name+"' AND value like '%"+value+"%'"
+			matchingString = matchingString? (matchingString+" AND cf.contact.name in (SELECT DISTINCT cf2.contact.name FROM CustomField cf2 join cf2.contact"+conditionString+")") : conditionString
+//			matchingString +=  
+			
+		}
+		println "final namestring"+matchingString
+		CustomField.executeQuery("SELECT DISTINCT cf.contact.name FROM CustomField cf join cf.contact"+matchingString)
+	}
 }
