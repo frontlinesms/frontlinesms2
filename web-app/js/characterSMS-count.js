a18bbbf2588dd59809b6868d6d49ea0778e95a77
Geoffrey <geoffreymuchai@gmail.com>
2011-11-01 09:34:59 +0300

    Fixed the message count freezing problem in popups

--- web-app/js/characterSMS-count.js
+++ web-app/js/characterSMS-count.js
@@ -1,14 +1,13 @@
 function updateCount() {
 	var value = $(this).val();
-	
 	if(value.length > 3000) {
 		//prevent addition of new content to message
 		$(this).val(value.substring(0, 3000));
-	} else if(value.length > 140) {
+	} else if(value.length > 59) {
 		$.get(url_root + 'message/getSendMessageCount', {message: value}, function(data) {
-			$(this).siblings("#message-stats").html(value.length + " characters " + data);
+			$("#message-stats").html(value.length + " characters " + data);
 		});
 	} else {
-		$(this).siblings("#message-stats").html(value.length + " characters (1 SMS message)");
+		$("#message-stats").html(value.length + " characters (1 SMS message)");
 	}
 }
