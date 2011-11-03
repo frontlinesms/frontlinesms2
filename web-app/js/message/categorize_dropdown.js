3c4af9f2b58f4eaf773214dba53378194878c267
David Kutalek <davkutalek@gmail.com>
2011-11-03 17:50:35 +0300

    David

--- web-app/js/message/categorize_dropdown.js
+++ web-app/js/message/categorize_dropdown.js
@@ -5,13 +5,15 @@ function categorizeClickAction() {
 	var messageSection = $('input:hidden[name=messageSection]').val();
 	if(countCheckedMessages() > 1) {
 		var messagesToChange = $('input:hidden[name=checkedMessageList]').val();
+		var successUrl = "message/" + messageSection + "/" + ownerId
 	} else {
 		var messagesToChange = $("#message-id").val();
+		var successUrl = "message/" + messageSection + "/" + ownerId + "/show/" + messagesToChange;
 	}
 	$.ajax({
 		type:'POST',
 		url: url_root + 'message/changeResponse',
 		data: {responseId: responseId, messageId: messagesToChange, ownerId: ownerId},
-		success: function(data) { window.location = url_root + "message/" + messageSection + "/" + ownerId; }
+		success: function(data) { window.location = url_root + successUrl; }
 	});
 }
