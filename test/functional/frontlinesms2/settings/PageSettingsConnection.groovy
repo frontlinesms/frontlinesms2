96ddd1456e933f489f552e0dd6d8150111769a3c
Geoffrey <geoffreymuchai@gmail.com>
2011-12-02 17:08:48 +0300

    Fixed failing settings tests

--- test/functional/frontlinesms2/settings/PageSettingsConnection.groovy
+++ test/functional/frontlinesms2/settings/PageSettingsConnection.groovy
@@ -2,10 +2,14 @@ package frontlinesms2.settings
 
 import frontlinesms2.*
 
-class ConnectionPage extends geb.Page {
+class PageSettingsConnection extends geb.Page {
 	static url = 'settings/connections'
+	static at = {
+		title.startsWith('Settings')
+	}
+	
 	static content = {
 		btnNewConnection { $('#create-connection-btn a') }
-		connectionNames { $('connection-header h2') }
+		connectionNames { $('.connection-header').find('h2') }
 	}
 }
