0dc97b2c28eefb12837b69d8a830452b27341e5f
David Kutalek <davkutalek@gmail.com>
2011-12-07 16:12:54 +0300

    changes to plugin setup

--- radio/RadioGrailsPlugin.groovy
+++ radio/RadioGrailsPlugin.groovy
@@ -4,7 +4,7 @@ class RadioGrailsPlugin {
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
-    def dependsOn = [:]
+    def dependsOn = [frontlinesms2:"*"]
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
             "grails-app/views/error.gsp"
