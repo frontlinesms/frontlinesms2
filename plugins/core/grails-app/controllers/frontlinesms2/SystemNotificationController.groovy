7b8d0a752728032f35f7ceb200b8d9122009e4ab
Geoffrey <geoffreymuchai@gmail.com>
2012-02-21 14:44:14 +0300

    Implemented asynchronous notifications when creating fconnection routes; fixed failing tests

--- plugins/core/grails-app/controllers/frontlinesms2/SystemNotificationController.groovy
+++ plugins/core/grails-app/controllers/frontlinesms2/SystemNotificationController.groovy
@@ -1,12 +1,26 @@
 package frontlinesms2
 
+import grails.converters.*
+
 class SystemNotificationController {
+	
 	def markRead = {
 		withNotification {
 			it.read = true
 			it.save(failOnError:true)
 			render text:'OK'
 		}
+	}
+	
+	def list = {
+		def systemNotificationInstanceList = SystemNotification.findAllByRead(false)
+		def notifications = systemNotificationInstanceList.collect {
+			[
+				"markRead":" ${remoteLink(controller:'systemNotification', action:'markRead', id:it.id){'mark read'}}",
+				"text":it.text
+			]
+		}
+		render notifications as JSON
 	}
 	
 	private def withNotification(Closure c) {
