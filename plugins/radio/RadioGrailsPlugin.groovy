753af34d7cc574d37bbd6d242900582a25b83abe
Joy <joy@frontlinesms-acer-TravelMate-5742G.(none)>
2012-03-12 11:46:03 +0300

    Updating plugin dependancy for core; Updating plugin descriptors for radio

--- plugins/radio/RadioGrailsPlugin.groovy
+++ plugins/radio/RadioGrailsPlugin.groovy
@@ -1,11 +1,11 @@
 import frontlinesms2.radio.*
 class RadioGrailsPlugin {
     // the plugin version
-    def version = "0.1-SNAPSHOT"
+    def version = "0.a3-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
-    def dependsOn = ["core":"0.1-SNAPSHOT"]
+    def dependsOn = ["core":"0.a3-SNAPSHOT"]
     // resources that are excluded from plugin packaging
     // def pluginExcludes = ["grails-app/views/error.gsp"]
 
