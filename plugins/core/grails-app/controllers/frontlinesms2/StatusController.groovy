8e997091b1b36348d9d82f3274a5f0edc2f3556d
Geoffrey <geoffreymuchai@gmail.com>
2012-03-14 08:32:08 +0300

    Filtering of sent messages by message owners now works

--- plugins/core/grails-app/controllers/frontlinesms2/StatusController.groovy
+++ plugins/core/grails-app/controllers/frontlinesms2/StatusController.groovy
@@ -37,10 +37,10 @@ class StatusController {
 	}
 
 	private def getMessageStats() {
-		def activityInstance = getActivityInstance()
+		def activityInstance = MessageOwner.get(params.activityId)
 		params.startDate = params.rangeOption == "between-dates" ? params.startDate : new Date() - 14
 		params.endDate = params.rangeOption == "between-dates" ? params.endDate : new Date()
-		params.messageOwner = (activityInstance && activityInstance instanceof Poll) ? activityInstance.responses : activityInstance
+		params.messageOwner = activityInstance
 		params.groupInstance = params.groupId ? Group.get(params.groupId) : null
 		params.messageStatus = params.messageStatus?.tokenize(",")*.trim()
 		def messageStats = Fmessage.getMessageStats(params) // TODO consider changing the output of this method to match the data we actually want
@@ -55,17 +55,9 @@ class StatusController {
 			[groupInstance: groupInstance,
 					activityId: params.activityId,
 					groupInstanceList : Group.findAll(),
+					activityInstanceList: Activity.findAll(),
 					folderInstanceList: Folder.findAll(),
-					pollInstanceList: Poll.findAll(),
 					search: new Search(group:groupInstance,	activityId: params.activityId)]
 	}
 	
-	private def getActivityInstance() {
-		if(params.activityId) {
-			def stringParts = params.activityId.tokenize('-')
-			def activityType = stringParts[0] == 'poll' ? Poll : Folder
-			def activityId = stringParts[1]
-			activityType.findById(activityId)
-		} else return null
-	}
 }
