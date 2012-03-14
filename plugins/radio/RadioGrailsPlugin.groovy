75f694e4604d92e88da61fb3fbe74c31c318327e
Geoffrey <geoffreymuchai@gmail.com>
2012-03-14 17:46:23 +0300

    [ Radio 0.a4.1 release ]

--- plugins/radio/RadioGrailsPlugin.groovy
+++ plugins/radio/RadioGrailsPlugin.groovy
@@ -5,7 +5,7 @@ class RadioGrailsPlugin {
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
-    def dependsOn = ["core":"0.a4.1"]
+    def dependsOn = ["core":"0.a5-SNAPSHOT"]
     // resources that are excluded from plugin packaging
     // def pluginExcludes = ["grails-app/views/error.gsp"]
 
