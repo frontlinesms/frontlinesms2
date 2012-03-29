c80cefaf1942dacb1cdf7135324a0ef32c4bae4e
Roy Owino <roy@credit.frontlinesms.com>
2012-03-29 15:53:10 +0300

    Roy|Alex: prepared i18n report first draft

---
+++ plugins/core/scripts/I18nReport.groovy
@@ -0,0 +1,13 @@
+final def I18N_VIOLATION_REGEX = /[>]\s*[[\s*.:!+@;'?()$,{}]*\s*0-9a-zA-Z\s]*+\s*[<]*/
+final def DIR_ROOT = "grails-app/views"
+new File(DIR_ROOT).eachFileRecurse { file ->
+    if (file.isFile()) {
+        def lineNumber = 0
+        file.eachLine { line ->
+            lineNumber++
+            def result = line.findAll(I18N_VIOLATION_REGEX)
+			if(result.any { it && it != '>' && it != '><' })
+				println "$DIR_ROOT/$file.name:$lineNumber:$line"
+        }
+    }   
+} 
