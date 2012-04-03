6e828990d11aab26c4bc22226fee34d5e7056963
Joy <joy@frontlinesms-acer-TravelMate-5742G.(none)>
2012-04-03 18:19:20 +0300

    Added keyword can be blank and not null tests

--- plugins/core/grails-app/domain/frontlinesms2/Keyword.groovy
+++ plugins/core/grails-app/domain/frontlinesms2/Keyword.groovy
@@ -5,14 +5,18 @@ class Keyword {
 	static belongsTo = [activity: Activity]
 	
 	static constraints = {
-		value(blank: false, nullable: false, maxSize: 255, validator: { val, me ->
-			if(val.find(/\s/)) return false
+		value(blank:true, nullable:false, maxSize:255, validator: { val, me ->
+			if(!(val ==~ /[A-Z]*/)) return false
 			else {
 				if(me.activity?.archived) return true
 				def found = Keyword.findAllByValue(val)
+				println "Found: $found"
 				if(!found || found==[me]) return true
-				else if (found.any { it != me && !it.activity?.archived }) return false
-				return true
+				else if (found.any { it != me && !it.activity?.archived }) {
+					println "Returning false because ?"
+					return false
+				}
+				else return true
 			}
 		})
 		activity(nullable: false)
