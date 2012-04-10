41114075789936b963812b5fa2f70c22160432ab
Alex Anderson <alex@frontlinesms.com>
2012-04-10 23:32:10 +0300

    Updated camel-smslib.

--- plugins/core/grails-app/conf/BuildConfig.groovy
+++ plugins/core/grails-app/conf/BuildConfig.groovy
@@ -52,7 +52,7 @@ grails.project.dependency.resolution = {
 		compile 'net.frontlinesms.test:hayescommandset-test:0.0.4'
 
 		// COMPILE
-		compile 'net.frontlinesms.core:camel-smslib:0.0.3'
+		compile 'net.frontlinesms.core:camel-smslib:0.0.4-SNAPSHOT'
 		['mail', 'http'].each { compile camel(it) }
 
 		compile 'net.frontlinesms.core:serial:1.0.1'
