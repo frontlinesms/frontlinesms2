def68c935cc5a5c30a4048e571cbffda2288260d
Geoffrey <geoffreymuchai@gmail.com>
2012-04-13 12:52:16 +0300

    Radio a6 release

--- plugins/radio/RadioGrailsPlugin.groovy
+++ plugins/radio/RadioGrailsPlugin.groovy
@@ -1,11 +1,11 @@
 import frontlinesms2.radio.*
 class RadioGrailsPlugin {
     // the plugin version
-    def version = "0.a6-SNAPSHOT"
+    def version = "0.a6"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
-    def dependsOn = ["core":"0.a6-SNAPSHOT"]
+    def dependsOn = ["core":"0.a6"]
     // resources that are excluded from plugin packaging
     // def pluginExcludes = ["grails-app/views/error.gsp"]
 
