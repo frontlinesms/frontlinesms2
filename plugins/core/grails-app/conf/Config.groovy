86a8bef4d84f5ace51e073bd04c58af836b9cf93
Joy Kendi <joy@frontlinesms.com>
2012-04-13 11:20:45 +0300

    Added missing Config.groovy file

---
+++ plugins/core/grails-app/conf/Config.groovy
@@ -0,0 +1,120 @@
+import org.apache.log4j.ConsoleAppender
+import org.apache.log4j.RollingFileAppender
+
+// locations to search for config files that get merged into the main config
+// config files can either be Java properties files or ConfigSlurper scripts
+
+// grails.config.locations = [ "classpath:${appName}-config.properties",
+//                             "classpath:${appName}-config.groovy",
+//                             "file:${userHome}/.grails/${appName}-config.properties",
+//                             "file:${userHome}/.grails/${appName}-config.groovy"]
+
+// if(System.properties["${appName}.config.location"]) {
+//    grails.config.locations << "file:" + System.properties["${appName}.config.location"]
+// }
+
+grails.project.groupId = "frontlinesms2" // change this to alter the default package name and Maven publishing destination
+grails.mime.file.extensions = true // enables the parsing of file extensions from URLs into the request format
+grails.mime.use.accept.header = false
+grails.mime.types = [ html: ['text/html','application/xhtml+xml'],
+                      xml: ['text/xml', 'application/xml'],
+                      text: 'text/plain',
+                      js: 'text/javascript',
+                      rss: 'application/rss+xml',
+                      atom: 'application/atom+xml',
+                      css: 'text/css',
+                      csv: 'text/csv',
+					  pdf: 'application/pdf',
+                      all: '*/*',
+                      json: ['application/json','text/json'],
+                      form: 'application/x-www-form-urlencoded',
+                      multipartForm: 'multipart/form-data'
+                    ]
+
+// URL Mapping Cache Max Size, defaults to 5000
+//grails.urlmapping.cache.maxsize = 1000
+
+// The default codec used to encode data with ${}
+grails.views.default.codec = "none" // none, html, base64
+grails.views.gsp.encoding = "UTF-8"
+grails.converters.encoding = "UTF-8"
+// enable Sitemesh preprocessing of GSP pages
+grails.views.gsp.sitemesh.preprocess = true
+// scaffolding templates configuration
+grails.scaffolding.templates.domainSuffix = 'Instance'
+
+// jquery plugin
+grails.views.javascript.library = "jquery"
+
+// pagination
+grails.views.pagination.max = 50
+
+// request parameters to mask when logging exceptions
+grails.exceptionresolver.params.exclude = ['password']
+
+// Set to false to use the new Grails 1.2 JSONBuilder in the render method
+grails.json.legacy.builder = false
+// enabled native2ascii conversion of i18n properties files
+grails.enable.native2ascii = true
+// whether to install the java.util.logging bridge for sl4j. Disable for AppEngine!
+grails.logging.jul.usebridge = true
+// packages to include in Spring bean scanning
+grails.spring.bean.packages = []
+
+// request parameters to mask when logging exceptions
+grails.exceptionresolver.params.exclude = ['password']
+
+//fronlinesms plugin
+frontlinesms2.plugin = "core"
+
+// set per-environment serverURL stem for creating absolute links
+environments {
+    production {
+        grails.serverURL = "http://www.changeme.com"
+    }
+    development {
+        grails.serverURL = "http://localhost:8080/${appName}"
+    }
+    test {
+        grails.serverURL = "http://localhost:8080/${appName}"
+    }
+}
+
+// log4j configuration
+log4j = {
+	environments {
+		production {
+			def conf = "${System.properties.'user.home'}/.frontlinesms2"
+			def datePattern = "'.'yyyy-MM-dd"
+			def layout = pattern(conversionPattern:'%d %-5p [%c{2}] %m%n')
+			appender new RollingFileAppender(name:"prod",
+					datePattern:datePattern, layout:layout, file:"$conf/standard.log",
+					threshold:org.apache.log4j.Level.INFO);
+			appender new RollingFileAppender(name:"prod-stacktrace",
+					datePattern:datePattern, layout:layout, file:"$conf/stacktrace.log",
+					threshold:org.apache.log4j.Level.ERROR);
+		}
+		development {
+			appender new ConsoleAppender(name:'console-logger')
+			info 'console-logger'
+		}
+	}
+
+    error  'org.codehaus.groovy.grails.web.servlet',  //  controllers
+           'org.codehaus.groovy.grails.web.pages', //  GSP
+           'org.codehaus.groovy.grails.web.sitemesh', //  layouts
+           'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
+           'org.codehaus.groovy.grails.web.mapping', // URL mapping
+           'org.codehaus.groovy.grails.commons', // core / classloading
+           'org.codehaus.groovy.grails.plugins', // plugins
+           'org.codehaus.groovy.grails.orm.hibernate', // hibernate integration
+           'org.springframework',
+           'org.hibernate',
+           'net.sf.ehcache.hibernate'
+
+    warn   'org.mortbay.log'
+
+    info  'serial', 'org.smslib'
+
+	debug 'org.apache.camel'
+}
