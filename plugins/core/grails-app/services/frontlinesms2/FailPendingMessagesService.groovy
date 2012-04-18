444ff9c1b11096edf8450aaf14fcf994b618bf64
Alex Anderson <alex@frontlinesms.com>
2012-04-18 12:28:10 +0300

    Added note to fail pending messages service re: not being a service.

--- plugins/core/grails-app/services/frontlinesms2/FailPendingMessagesService.groovy
+++ plugins/core/grails-app/services/frontlinesms2/FailPendingMessagesService.groovy
@@ -2,6 +2,7 @@ package frontlinesms2
 
 class FailPendingMessagesService {
 	def init() {
+		// N.B. This should ONLY EVER be called in the bootstrap, and therefore probably shouldn't be a service
 		def pendingDispatchList = Dispatch.findAllByStatus(DispatchStatus.PENDING)
 		if (pendingDispatchList) {
 			pendingDispatchList.each() {
