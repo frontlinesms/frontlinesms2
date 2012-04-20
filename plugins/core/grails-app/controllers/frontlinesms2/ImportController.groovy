053ad306c72d3c67ab9c4fbbd3ff6cc9fddf62a5
Joy Kendi <joy@frontlinesms.com>
2012-04-20 13:04:16 +0300

    Changed the method that deletes the temp file for downloading the failedcontacts.csv

--- plugins/core/grails-app/controllers/frontlinesms2/ImportController.groovy
+++ plugins/core/grails-app/controllers/frontlinesms2/ImportController.groovy
@@ -70,7 +70,7 @@ class ImportController {
 		response.setHeader("Content-disposition", "attachment; filename=failedContacts.csv")
 		failedContactsFile.eachLine {response.outputStream  << "$it\n"}
 		response.outputStream.flush()
-		failedContactsFile.deleteOnExit()
+		failedContactsFile.delete()
 	}
 	
 	def importMessages = {
