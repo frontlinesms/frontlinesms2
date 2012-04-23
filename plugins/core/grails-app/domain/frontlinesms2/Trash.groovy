367e59707df04edc161dfc59ab577d50d596d054
Alex Anderson <alex@frontlinesms.com>
2012-04-23 11:33:09 +0300

    Added TODOs to Trash.

--- plugins/core/grails-app/domain/frontlinesms2/Trash.groovy
+++ plugins/core/grails-app/domain/frontlinesms2/Trash.groovy
@@ -2,15 +2,17 @@ package frontlinesms2
 
 class Trash {
 	Date dateCreated
-	Long linkId
-	String objectType
-	String message
-	String identifier
+	Long linkId // TODO rename this objectId
+	String objectType // TODO this could be a Class instead of String.  If keeping as class name, call it that
+	String message // TODO rename this something like displayDetail
+	String identifier // TODO rename this displayName as that is what it is
 	
-	def getLink() { getClass().classLoader.loadClass(objectType).get(linkId) }
+	def getLink() { getClass().classLoader.loadClass(objectType).get(linkId) } // if this has to be done this way, use Class.forName()
 	
 	static constraints = {
 		message(nullable: true)
 		identifier(nullable: true)
 	}
+
+	// TODO would be nice to have a factory method here where database objects can be passed in
 }
