00b3c1d11cdb1aa3a6d2b368493939193fc09120
Alex Anderson <alex@frontlinesms.com>
2012-04-24 09:24:24 +0300

    Rolled version numbers to 0.a7.

--- plugins/radio/RadioGrailsPlugin.groovy
+++ plugins/radio/RadioGrailsPlugin.groovy
@@ -5,7 +5,7 @@ class RadioGrailsPlugin {
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
-    def dependsOn = ["core":"0.a7-SNAPSHOT"]
+    def dependsOn = [core:"0.a7"]
     // resources that are excluded from plugin packaging
     // def pluginExcludes = ["grails-app/views/error.gsp"]
 
