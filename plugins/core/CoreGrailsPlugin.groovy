17bd22a89f5d1b8be55f2eb2c9cbe4d3cce7c534
Alex Anderson <alex@frontlinesms.com>
2012-04-24 19:18:17 +0300

    Updated core-plugin details

--- plugins/core/CoreGrailsPlugin.groovy
+++ plugins/core/CoreGrailsPlugin.groovy
@@ -1,7 +1,7 @@
 class CoreGrailsPlugin {
     def version = "0.a8-SNAPSHOT"
-    def grailsVersion = "1.3.7 > *"
-    def dependsOn = [csv:"0.3.1", jquery:"1.6.1.1", routing:"1.1.2-frontlinesms"]
+    def grailsVersion = "2.0.3 > *"
+    def dependsOn = [csv:"0.3.1", jquery:"1.7.1", routing:"1.2.0"]
     def pluginExcludes = ["grails-app/views/error.gsp"]
     def author = "FrontlineSMS team"
     def authorEmail = "dev@frontlinesms.com"
