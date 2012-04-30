1412948223c3f9b824378a26ff785d20ab58478c
Alex Anderson <alex@frontlinesms.com>
2012-04-30 12:34:39 +0300

    Fixed HelpController and spec for grails2.

--- plugins/core/test/unit/frontlinesms2/HelpControllerSpec.groovy
+++ plugins/core/test/unit/frontlinesms2/HelpControllerSpec.groovy
@@ -1,36 +1,33 @@
 package frontlinesms2
 
 import spock.lang.*
-import grails.plugin.spock.*
 
 @TestFor(HelpController)
-class HelpControllerSpec extends ControllerSpec {
+class HelpControllerSpec extends Specification {
+	def setup() {
+		String.metaClass.markdownToHtml = { "markdown:$delegate" }
+	}
 	
 	def 'If a help file with the given name exists its text will be rendered'() {
 		given:
-			registerMetaClass String
-			String.metaClass.markdownToHtml = { delegate }
 			def helpFile = new File('testHelp.txt')
 			def helpFileContent = "This is test content for the help"
 			helpFile.text = helpFileContent
 			params.helpSection = 'testHelp'
 		when:
-			controller.getSection()
+			controller.section()
 		then:
-			renderArgs.text == helpFileContent
+			response.text == 'markdown:' + helpFileContent
 		cleanup:
 			helpFile.delete()
 	}
 	
 	def 'If a help file with the given name does not exist error text will be rendered'() {
 		given:
-			registerMetaClass String
-			String.metaClass.markdownToHtml = { delegate }
 			def helpFileContent = "This is test content for the help"
 		when:
-			controller.getSection()
+			controller.section()
 		then:
-			renderArgs.text != helpFileContent
-			renderArgs.text == "This help file is not yet available, sorry."
+			response.text == 'markdown:This help file is not yet available, sorry.'
 	}
 }
