fa7cbb0fabcdd308aad4e3b61287ec58f86e3e96
Sitati Kituyi <sitati.kituyi@googlemail.com>
2012-05-07 18:08:35 +0300

    selenium/firefox functional testing is selected by -DfunctionalTestMode=[local/remote]

--- plugins/core/test/functional/GebConfig.groovy
+++ plugins/core/test/functional/GebConfig.groovy
@@ -1,19 +1,23 @@
 driver = {
-	//def driver = new org.openqa.selenium.firefox.FirefoxDriver()
-//	def driver = new org.openqa.selenium.htmlunit.HtmlUnitDriver()
-
-	org.openqa.selenium.remote.DesiredCapabilities capabillities = org.openqa.selenium.remote.DesiredCapabilities.firefox();
-        capabillities.setCapability("version", "5");
-        capabillities.setCapability("platform", Platform.XP);
-        capabillities.setCapability("name", "FrontlineSMS2 Functional test cases");
-	def driver = new org.openqa.selenium.remote.RemoteWebDriver(
-           new URL("http://sitati:6561dd21-0371-4e43-b1c9-4d86dc824061@ondemand.saucelabs.com:80/wd/hub"),
-           capabillities);
-        driver.manage().timeouts().implicitlyWait(30, java.util.concurrent.TimeUnit.SECONDS);
+	def mode=System.properties['functionalTestMode']
+	if (mode=="remote") {
+		org.openqa.selenium.remote.DesiredCapabilities capabillities = org.openqa.selenium.remote.DesiredCapabilities.firefox();
+	        capabillities.setCapability("version", "5");
+	        capabillities.setCapability("platform", Platform.XP);
+	        capabillities.setCapability("name", "FrontlineSMS2 Functional test cases");
+		def driver = new org.openqa.selenium.remote.RemoteWebDriver(
+	           new URL("http://sitati:6561dd21-0371-4e43-b1c9-4d86dc824061@ondemand.saucelabs.com:80/wd/hub"),
+	           capabillities);
+	        driver.manage().timeouts().implicitlyWait(30, java.util.concurrent.TimeUnit.SECONDS);
+	}
+	else {
+		// defaults to locally-run FirefoxDriver
+	    def driver = new org.openqa.selenium.firefox.FirefoxDriver()	
+	}
 	if(driver.class.simpleName == 'FirefoxDriver') {
 		def width = 1366
 		def height = 768
-		driver.navigate().to('http://myapp.test')
+		driver.navigate().to('http://localhost')
 		driver.manage().window().setSize(new org.openqa.selenium.Dimension(width, height))
 	} else if(driver.class.simpleName == 'HtmlUnitDriver') {
 		driver.javascriptEnabled = true
