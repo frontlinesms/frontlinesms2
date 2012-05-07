62c6948d8adcd64c66bfd164ce14817fc8756c35
Sitati Kituyi <sitati.kituyi@googlemail.com>
2012-05-07 11:54:07 +0300

    alternate selenium config for OnDemand

--- plugins/core/test/functional/GebConfig.groovy
+++ plugins/core/test/functional/GebConfig.groovy
@@ -1,6 +1,14 @@
 driver = {
-	def driver = new org.openqa.selenium.firefox.FirefoxDriver()
+	//def driver = new org.openqa.selenium.firefox.FirefoxDriver()
 //	def driver = new org.openqa.selenium.htmlunit.HtmlUnitDriver()
+	DesiredCapabilities capabillities = DesiredCapabilities.firefox();
+        capabillities.setCapability("version", "5");
+        capabillities.setCapability("platform", Platform.XP);
+        capabillities.setCapability("name", "Testing Selenium 2 with Java on Sauce");
+	def driver = new RemoteWebDriver(
+           new URL("http://username-string:access-key-string@ondemand.saucelabs.com:80/wd/hub"),
+           capabillities);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	if(driver.class.simpleName == 'FirefoxDriver') {
 		def width = 1366
 		def height = 768
