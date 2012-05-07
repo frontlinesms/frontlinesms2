1d1b7000c1419ff5a7f20171e188af4b5c41859d
Alex Anderson <alex@frontlinesms.com>
2012-05-07 14:52:42 +0300

    Minor cleanup of LogEntry tests.

--- plugins/core/grails-app/conf/CoreBootStrap.groovy
+++ plugins/core/grails-app/conf/CoreBootStrap.groovy
@@ -266,14 +266,12 @@ class CoreBootStrap {
 	private def dev_initLogEntries() {
 		if(!dev) return
 		def now = new Date()
-		[new LogEntry(date: now, content: "entry1"),
-				new LogEntry(date: now-2, content: "entry2"),
-				new LogEntry(date: now-6, content: "entry3"),
-				new LogEntry(date: now-13, content: "entry4"),
-				new LogEntry(date: now-27, content: "entry5"),
-				new LogEntry(date: now-100, content: "entry6")].each() {
-			it.save(failOnError:true, flush:true)
-		}
+		[new LogEntry(date:now, content: "entry1"),
+				new LogEntry(date:now-2, content: "entry2"),
+				new LogEntry(date:now-6, content: "entry3"),
+				new LogEntry(date:now-13, content: "entry4"),
+				new LogEntry(date:now-27, content: "entry5"),
+				new LogEntry(date:now-100, content: "entry6")]*.save(failOnError:true, flush:true)
 	}
 
 	private def createGroup(String n) {
