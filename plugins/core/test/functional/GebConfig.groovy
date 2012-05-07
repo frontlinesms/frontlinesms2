f9f365e274e654178b5d7edbf8b2a2b059b19e59
Sitati Kituyi <sitati.kituyi@googlemail.com>
2012-05-07 12:22:32 +0300

    added Sitati's OnDemand account info

--- plugins/core/test/functional/GebConfig.groovy
+++ plugins/core/test/functional/GebConfig.groovy
@@ -1,14 +1,15 @@
 driver = {
 	//def driver = new org.openqa.selenium.firefox.FirefoxDriver()
 //	def driver = new org.openqa.selenium.htmlunit.HtmlUnitDriver()
-	DesiredCapabilities capabillities = DesiredCapabilities.firefox();
+
+	org.openqa.selenium.remote.DesiredCapabilities capabillities = org.openqa.selenium.remote.DesiredCapabilities.firefox();
         capabillities.setCapability("version", "5");
         capabillities.setCapability("platform", Platform.XP);
         capabillities.setCapability("name", "Testing Selenium 2 with Java on Sauce");
-	def driver = new RemoteWebDriver(
-           new URL("http://username-string:access-key-string@ondemand.saucelabs.com:80/wd/hub"),
+	def driver = new org.openqa.selenium.remote.RemoteWebDriver(
+           new URL("http://sitati:6561dd21-0371-4e43-b1c9-4d86dc824061@ondemand.saucelabs.com:80/wd/hub"),
            capabillities);
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(30, java.util.concurrent.TimeUnit.SECONDS);
 	if(driver.class.simpleName == 'FirefoxDriver') {
 		def width = 1366
 		def height = 768
