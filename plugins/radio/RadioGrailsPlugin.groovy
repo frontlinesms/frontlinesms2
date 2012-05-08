960f7d2d95008f1facff758807f4feed4daf717c
Alex Anderson <alex@frontlinesms.com>
2012-05-08 14:53:11 +0300

    :x

--- plugins/radio/RadioGrailsPlugin.groovy
+++ plugins/radio/RadioGrailsPlugin.groovy
@@ -5,7 +5,7 @@ class RadioGrailsPlugin {
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
-    def dependsOn = [core:"0.a7"]
+    def dependsOn = [core:"0.b1-SNAPSHOT"]
     // resources that are excluded from plugin packaging
     // def pluginExcludes = ["grails-app/views/error.gsp"]
 
