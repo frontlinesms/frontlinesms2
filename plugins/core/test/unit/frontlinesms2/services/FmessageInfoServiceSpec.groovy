280f68f12c2c6f051ae2b3b8a0aeda59696e1ae5
Alex Anderson <alex@frontlinesms.com>
2012-05-14 08:19:30 +0300

    Updated FmessageInfoService spec to recongnise empty messages properly

--- plugins/core/test/unit/frontlinesms2/services/FmessageInfoServiceSpec.groovy
+++ plugins/core/test/unit/frontlinesms2/services/FmessageInfoServiceSpec.groovy
@@ -2,38 +2,25 @@ package frontlinesms2.services
 
 import frontlinesms2.*
 import grails.plugin.spock.*
+import spock.lang.*
 
+@TestFor(FmessageInfoService)
 class FmessageInfoServiceSpec extends UnitSpec {
-	def fmessageInfoService
-	
-	def setup() {
-		fmessageInfoService = new FmessageInfoService()
-	}
-	
+	@Unroll
 	def "should calculate the total number of characters and messages being sent"() {
+		
 		when:
-			def messageInfo = fmessageInfoService.getMessageInfos('')
+			def messageInfo = service.getMessageInfos(text)
 		then:
-			messageInfo.charCount == 0
-			messageInfo.remaining == 160
-			messageInfo.partCount == 0
-		when:
-			messageInfo = fmessageInfoService.getMessageInfos("a" * 160)
-		then:
-			messageInfo.charCount == 160
-			messageInfo.remaining == 0
-			messageInfo.partCount == 1
-		when:
-			messageInfo = fmessageInfoService.getMessageInfos("a" * 161)
-		then:
-			messageInfo.charCount == 161
-			messageInfo.remaining == 109
-			messageInfo.partCount == 2
-		when:
-			messageInfo = fmessageInfoService.getMessageInfos("a" * 270)
-		then:
-			messageInfo.charCount == 270
-			messageInfo.remaining == 0
-			messageInfo.partCount == 2
+			messageInfo.charCount == chars
+			messageInfo.remaining == remaining
+			messageInfo.partCount == parts
+		where:
+			text      | chars | remaining | parts
+			''        | 0     | 160       | 1
+			'a' * 160 | 160   | 0         | 1
+			'a' * 161 | 161   | 109       | 2
+			'a' * 270 | 270   | 0         | 2
 	}
 }
+
