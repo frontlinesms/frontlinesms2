6b4a760980d302fe04c7bc7bbf7f2f52a0fbc905
vaneyck <vaneycksemeotoki@gmail.com>
2012-05-23 14:58:17 +0300

    Source code for sending registration data

---
+++ plugins/core/install/Testsend.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.net.*;
+import java.util.Map;
+import java.util.Set;
+import java.util.Iterator;
+
+public class Testsend {
+	public boolean submitData(Map<String, String> data){
+		try{
+			URL siteUrl = new URL("http://register.frontlinesms.com/process.php");
+			HttpURLConnection conn = (HttpURLConnection) siteUrl.openConnection();
+			conn.setRequestMethod("POST");
+			conn.setDoOutput(true);
+			conn.setDoInput(true);
+			
+			DataOutputStream out = new DataOutputStream(conn.getOutputStream());
+			
+			Set keys = data.keySet();
+			Iterator keyIter = keys.iterator();
+			String content = "";
+			for(int i=0; keyIter.hasNext(); i++) {
+				Object key = keyIter.next();
+				if(i!=0) {
+					content += "&";
+				}
+				content += key + "=" + URLEncoder.encode(data.get(key), "UTF-8");
+			}
+			System.out.println(content);
+			out.writeBytes(content);
+			out.flush();
+			out.close();
+			BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String line = "";
+			while((line=in.readLine())!=null) {
+				System.out.println(line);
+			}
+			in.close();
+			return true;
+		}catch(Exception e){
+			return false;
+		}
+	}
+}
+
