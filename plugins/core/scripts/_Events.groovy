e1e2478a2d0decae26cc50746e0ce549b7078df9
Alex Anderson <alex@frontlinesms.com>
2012-05-23 09:02:30 +0300

    Fixed func test screenshot wall so it doesn't throw error with integration tests.

--- plugins/core/scripts/_Events.groovy
+++ plugins/core/scripts/_Events.groovy
@@ -44,7 +44,7 @@ eventTestStart = { testName ->
 }
 
 eventTestPhaseEnd = { phaseName ->
-	if (phaseName == 'functional') {
+	if (phaseName == 'functional' && new File('target/test-reports/geb').exists()) {
 		def report = new File('target/test-reports/html/screenshots.html')
 		new File('target/test-reports/geb').eachFileRecurse { f ->
 			if(!f.name.endsWith('.png')) return
