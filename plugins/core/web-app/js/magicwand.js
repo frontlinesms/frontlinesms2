ac35e03f86f9672454dd129518034d4c486723dd
Alex Anderson <alex@frontlinesms.com>
2012-05-24 13:38:10 +0300

    CORE-933 make magic wand display as icon instead of text and make list reset on click.x

--- plugins/core/web-app/js/magicwand.js
+++ plugins/core/web-app/js/magicwand.js
@@ -1,50 +1,30 @@
-function insertDynamicField(inputArea, magicWand) {
-	var list = $("#" + magicWand);
-	var varName = list.val();
-	insertAtCaret(inputArea, "${" + varName + "}");
-	resetSelectMenu(list);
-}
+var magicwand = {
+	wave: function() {
+		var list = $("#magicwand-select");
+		var varName = list.val();
+		insertAtCaret('messageText', "${" + varName + "}");
+		magicwand.reset(list);
+	},
 
-/**
- * Reset a jquery "selectmenu" to display the original selected item.
- * @arg list jquery selecter for the <option/> element
- */
-function resetSelectMenu(list) {
-	list.selectmenu("destroy");
-	list[0].selectedIndex = 0; //.attr("selectedIndex", 1); //selectmenu("destroy");
-	list.selectmenu();
-}
+	/**
+	 * Reset a jquery "selectmenu" to display the original selected item.
+	 * @arg list jquery selecter for the <option/> element
+	 */
+	reset: function(list) {
+		list.selectmenu("destroy");
+		list[0].selectedIndex = 0;
+		magicwand.init(list);
+	},
 
-function insertAtCaret(areaId,text) {
-    var txtarea = document.getElementById(areaId);
-    var scrollPos = txtarea.scrollTop;
-    var strPos = 0;
-    var browser = ((txtarea.selectionStart || txtarea.selectionStart == '0') ? 
-        "ff" : (document.selection ? "ie" : false ) );
-    if (browser == "ie") { 
-        txtarea.focus();
-        var range = document.selection.createRange();
-        range.moveStart ('character', -txtarea.value.length);
-        strPos = range.text.length;
-    }
-    else if (browser == "ff") strPos = txtarea.selectionStart;
+	init: function(list) {
+		list.selectmenu();
+		var status = list.parent().find(".ui-selectmenu-status");
+		status.addClass("magicwand-button");
+		status.removeClass("ui-selectmenu-status");
+		status.text('');
+		status.siblings(".ui-selectmenu-icon").remove();
+		var statusParent = status.parent();
+		statusParent.removeClass("ui-selectmenu");
+	}
+};
 
-    var front = (txtarea.value).substring(0,strPos);  
-    var back = (txtarea.value).substring(strPos,txtarea.value.length); 
-    txtarea.value=front+text+back;
-    strPos = strPos + text.length;
-    if (browser == "ie") { 
-        txtarea.focus();
-        var range = document.selection.createRange();
-        range.moveStart ('character', -txtarea.value.length);
-        range.moveStart ('character', strPos);
-        range.moveEnd ('character', 0);
-        range.select();
-    }
-    else if (browser == "ff") {
-        txtarea.selectionStart = strPos;
-        txtarea.selectionEnd = strPos;
-        txtarea.focus();
-    }
-    txtarea.scrollTop = scrollPos;
-}
