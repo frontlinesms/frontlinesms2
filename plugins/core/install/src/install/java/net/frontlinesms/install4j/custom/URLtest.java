543097b6c4a69f8ea890d844e586f02526b594b8
vaneyck <vaneycksemeotoki@gmail.com>
2012-05-24 12:47:05 +0300

    validation fixes to registration form

--- plugins/core/install/src/install/java/net/frontlinesms/install4j/custom/URLtest.java
+++ plugins/core/install/src/install/java/net/frontlinesms/install4j/custom/URLtest.java
@@ -6,7 +6,14 @@ public class URLtest {
 	    Pattern p2 = Pattern.compile("(((file|gopher|news|nntp|telnet|http|ftp|https|ftps|sftp)://)|(www\\.))+(([a-zA-Z0-9\\._-]+\\.[a-zA-Z]{2,6})|([0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}))(/[a-zA-Z0-9\\&amp;%_\\./-~-]*)?");
 	    Matcher m = p2.matcher(text);
 	    boolean matchFound = m.matches();
-	    if(matchFound == false){ javax.swing.JOptionPane.showMessageDialog(null, "Invalid Webaddress \nTry e.g. www.google.com"); }
+	    if(matchFound == false){ javax.swing.JOptionPane.showMessageDialog(null, "Invalid Web Address \nTry e.g. www.google.com"); }
+	    return matchFound;
+	}
+	public boolean testEmail(String text){
+		Pattern p2 = Pattern.compile("^([0-9a-zA-Z]([-.\\w]*[0-9a-zA-Z])*@(([0-9a-zA-Z])+([-\\w]*[0-9a-zA-Z])*\\.)+[a-zA-Z]{2,9})$");
+	    Matcher m = p2.matcher(text);
+	    boolean matchFound = m.matches();
+	    if(matchFound == false){ javax.swing.JOptionPane.showMessageDialog(null, "Invalid Email \nTry e.g. jim@home.com"); }
 	    return matchFound;
 	}
 }
