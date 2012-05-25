3222e180149ad84ffd9d36ef6487017c59603435
Joy Kendi <joy@frontlinesms.com>
2012-05-25 12:33:38 +0300

    Added check for registration file

--- plugins/core/install/src/install/java/net/frontlinesms/install4j/custom/CompleteRegistration.java
+++ plugins/core/install/src/install/java/net/frontlinesms/install4j/custom/CompleteRegistration.java
@@ -5,17 +5,15 @@ public class CompleteRegistration {
 	public void createRegistrationPropertiesFile(String uuid, boolean success){
 		File userHome = new File(System.getProperty("user.home"));
 		File regProp = new File(userHome.getAbsolutePath()+"/.fronlinesms2/registration.properties");
-		
 		BufferedWriter wt = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(regProp)));
-		
+
 		if(success){
 			wt.write("registered=true");
 			wt.write("uuid=" + uuid);
 		} else {
 			wt.write("registered=false");
 		}
-		wt.close();		
-		
+		wt.close();
 	}
 
 	public String generateUUID(){
