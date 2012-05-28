036e919410e3cddda5cf4814af46f8d103e1dcbf
Joy Kendi <joy@frontlinesms.com>
2012-05-28 09:42:36 +0300

    Paging arrows enabled when search word is deleted

--- plugins/core/web-app/js/contact/search_within_list.js
+++ plugins/core/web-app/js/contact/search_within_list.js
@@ -7,6 +7,9 @@ function updateContacts(data) {
 	var replacement = $(data);
 	$("#contact-list").html(replacement.filter('#contact-list').html());
 	$(".footer #paging").html(replacement.filter('.footer #paging').html());
+	if($("#contact-search").val()=="") {
+		$("#paging").append('<a href="#" class="nextLink enabled"></a>');
+	}
 	disablePaginationControls();
 }
 
