76205a386bfb44c461b17d4a6589cfe05621370a
Alex Anderson <alex@frontlinesms.com>
2012-06-02 10:39:57 +0300

    In demo layout: Added reset.css; localized jquery; added option to disable JS; added tabs and sys menu; split table to thead and tbody; made layout work if JS is disabled.

--- plugins/core/layout/resizer.js
+++ plugins/core/layout/resizer.js
@@ -1,14 +1,28 @@
 var Resizer = function(listId, listHeaderId, listFooterId) {
-	var _main_header_height = $("#head").height(),
-		_list_header = $("#" + listHeaderId),
-		_list_footer = $("#" + listFooterId),
-		_list = $("#" + listId),
-		_do_resize = function() {
-			var headerHeight = _list_header.height(),
-				footerHeight = _list_footer.height();
-			_list.css('top', headerHeight + _main_header_height);
-			_list.css('bottom', footerHeight);
+	var main_header_height = $("#head").height(),
+		list_header = $("#" + listHeaderId),
+		list_footer = $("#" + listFooterId),
+		list = $("#" + listId),
+		list_left = list.css("left"),
+		list_right = list.css("right"),
+		do_resize = function() {
+			list.css('top', list_header.height() + main_header_height);
+			list.css('bottom', list_footer.height());
 		};
-	return { go: _do_resize };
+
+	// initialise
+	list_header.css("position", "fixed");
+	list_header.css("top", main_header_height);
+	list_header.css("left", list_left);
+	list_header.css("right", list_right);
+
+	list_footer.css("position", "fixed");
+	list_footer.css("bottom", 0);
+	list_footer.css("left", list_left);
+	list_footer.css("right", list_right);
+
+	do_resize();
+
+	return do_resize;
 };
 
