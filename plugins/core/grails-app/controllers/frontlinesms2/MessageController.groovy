f6b22717d93f464c8c136b68e65cc906cf667857
geoffreymuchai <geoffreymuchai@gmail.com>
2012-06-06 22:09:57 +0300

    Categorize response dropdown is now displayed when a message is checked in a poll

--- plugins/core/grails-app/controllers/frontlinesms2/MessageController.groovy
+++ plugins/core/grails-app/controllers/frontlinesms2/MessageController.groovy
@@ -47,9 +47,11 @@ class MessageController {
 
 	def show() {
 		def messageInstance = Fmessage.get(params.messageId)
+		def activityInstance = Activity.get(params?.ownerId)
 		messageInstance.read = true
 		messageInstance.save()
 		def model = [messageInstance: messageInstance,
+				ownerInstance:activityInstance,
 				folderInstanceList: Folder.findAllByArchivedAndDeleted(viewingArchive, false),
 				activityInstanceList: Activity.findAllByArchivedAndDeleted(viewingArchive, false),
 				messageSection: params.messageSection]
