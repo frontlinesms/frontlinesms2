134c2eaa6e3636244c85b5b1863721c91e787306
Alex Anderson <alex@frontlinesms.com>
2012-06-07 10:35:36 +0300

    Fixed compile error in func. tests and added option to use remote webdriver.

--- plugins/core/test/functional/GebConfig.groovy
+++ plugins/core/test/functional/GebConfig.groovy
@@ -1,3 +1,7 @@
+import org.openqa.selenium.*
+import org.openqa.selenium.firefox.*
+import org.openqa.selenium.remote.*
+
 def jenkins = Boolean.parseBoolean(System.properties.jenkins)
 
 waiting {
@@ -16,15 +20,29 @@ waiting {
 }
 
 driver = {
-	def driver = new org.openqa.selenium.firefox.FirefoxDriver()
-	if(driver.class.simpleName == 'FirefoxDriver') {
+	def driver = new FirefoxDriver()
+	if(driver?.class?.simpleName == 'FirefoxDriver') {
 		def width = 1366
 		def height = 768
 		driver.navigate().to('http://localhost') // make sure that the browser has opened so we can resize it
-		driver.manage().window().setSize(new org.openqa.selenium.Dimension(width, height))
-	} else if(driver.class.simpleName == 'HtmlUnitDriver') {
+		driver.manage().window().setSize(new Dimension(width, height))
+	} else if(driver?.class?.simpleName == 'HtmlUnitDriver') {
 		driver.javascriptEnabled = true
 		driver.@webClient.throwExceptionOnScriptError = false
+	} else {
+		DesiredCapabilities capabilities = new DesiredCapabilities()
+		capabilities.javascriptEnabled = true
+		driver = new RemoteWebDriver(new URL("http://localhost:8083"), capabilities)
+		try {
+			driver.navigate().to('http://localhost:8080/core') // TODO should read this url from grails settings
+		} catch(TimeoutException ex) {
+			sleep 30000
+		}
+		def width = 1366
+		def height = 768
+		driver.manage().window().setSize(new Dimension(width, height))
+		actualCapabilities = ((RemoteWebDriver) driver).getCapabilities();
+		assert actualCapabilities.javascriptEnabled
 	}
 	return driver
 }
