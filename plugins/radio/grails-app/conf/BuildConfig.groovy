1407ea05798e127a00fd291e1008440e1ffff11e
geoffreymuchai <geoffreymuchai@gmail.com>
2012-06-18 16:58:41 +0300

    Upgraded Radio to grails 2.0.3

--- plugins/radio/grails-app/conf/BuildConfig.groovy
+++ plugins/radio/grails-app/conf/BuildConfig.groovy
@@ -1,6 +1,7 @@
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
+grails.project.target.level = 1.6
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 grails.plugin.location.core = "../core"
@@ -10,25 +11,30 @@ grails.project.dependency.resolution = {
     inherits("global") {
         // uncomment to disable ehcache
         // excludes 'ehcache'
-		excludes 'xml-apis'
     }
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     repositories {
-		grailsHome()
+        grailsCentral()
         // uncomment the below to enable remote dependency resolution
         // from public Maven repositories
-        mavenLocal()
-        mavenCentral()
+        //mavenCentral()
+        //mavenLocal()
         //mavenRepo "http://snapshots.repository.codehaus.org"
         //mavenRepo "http://repository.codehaus.org"
         //mavenRepo "http://download.java.net/maven/2/"
         //mavenRepo "http://repository.jboss.com/maven2/"
-		grailsPlugins()
-		grailsCentral()
     }
-	dependencies {
-		// specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
+    dependencies {
+        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
-		// COMPILE
-	}
+        // runtime 'mysql:mysql-connector-java:5.1.5'
+    }
+
+    plugins {
+        build(":tomcat:$grailsVersion",
+              ":release:1.0.0") {
+            export = false
+        }
+
+    }
 }
