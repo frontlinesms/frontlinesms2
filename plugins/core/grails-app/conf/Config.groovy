abb5a33513d19db4df7544dcc8399de11b59901a
Alex Anderson <alex@frontlinesms.com>
2012-06-19 19:26:37 +0300

    Re-added config.

---
+++ plugins/core/grails-app/conf/Config.groovy
@@ -0,0 +1,135 @@
+import org.apache.log4j.ConsoleAppender
+import org.apache.log4j.RollingFileAppender
+
+
+// locations to search for config files that get merged into the main config
+// config files can either be Java properties files or ConfigSlurper scripts
+
+// grails.config.locations = [ "classpath:${appName}-config.properties",
+//                             "classpath:${appName}-config.groovy",
+//                             "file:${userHome}/.grails/${appName}-config.properties",
+//                             "file:${userHome}/.grails/${appName}-config.groovy"]
+
+// if (System.properties["${appName}.config.location"]) {
+//    grails.config.locations << "file:" + System.properties["${appName}.config.location"]
+// }
+
+
+grails.project.groupId = appName // change this to alter the default package name and Maven publishing destination
+grails.mime.file.extensions = true // enables the parsing of file extensions from URLs into the request format
+grails.mime.use.accept.header = false
+grails.mime.types = [ html: ['text/html','application/xhtml+xml'],
+                      xml: ['text/xml', 'application/xml'],
+                      text: 'text/plain',
+                      js: 'text/javascript',
+                      rss: 'application/rss+xml',
+                      atom: 'application/atom+xml',
+                      css: 'text/css',
+                      csv: 'text/csv',
+                      all: '*/*',
+                      json: ['application/json','text/json'],
+                      form: 'application/x-www-form-urlencoded',
+                      multipartForm: 'multipart/form-data'
+                    ]
+
+// URL Mapping Cache Max Size, defaults to 5000
+//grails.urlmapping.cache.maxsize = 1000
+
+// What URL patterns should be processed by the resources plugin
+grails.resources.adhoc.patterns = ['/images/*', '/css/*', '/js/*', '/plugins/*']
+
+
+// The default codec used to encode data with ${}
+grails.views.default.codec = "none" // none, html, base64
+grails.views.gsp.encoding = "UTF-8"
+grails.converters.encoding = "UTF-8"
+// enable Sitemesh preprocessing of GSP pages
+grails.views.gsp.sitemesh.preprocess = true
+grails.views.pagination.max = 50
+// scaffolding templates configuration
+grails.scaffolding.templates.domainSuffix = 'Instance'
+
+// Set to false to use the new Grails 1.2 JSONBuilder in the render method
+grails.json.legacy.builder = false
+// enabled native2ascii conversion of i18n properties files
+grails.enable.native2ascii = true
+// packages to include in Spring bean scanning
+grails.spring.bean.packages = []
+// whether to disable processing of multi part requests
+grails.web.disable.multipart=false
+
+// request parameters to mask when logging exceptions
+grails.exceptionresolver.params.exclude = ['password']
+
+// enable query caching by default
+grails.hibernate.cache.queries = true
+
+//Enable automatic database migrations
+grails.plugin.databasemigration.updateOnStart = true
+grails.plugin.databasemigration.updateOnStartFileNames = ['changelog.groovy']
+
+// set per-environment serverURL stem for creating absolute links
+environments {
+    development {
+        grails.logging.jul.usebridge = true
+    }
+    production {
+        grails.logging.jul.usebridge = false
+        // TODO: grails.serverURL = "http://www.changeme.com"
+    }
+}
+
+// log4j configuration
+// first check for external config
+
+grails.config.locations = []
+grails.config.locations << "file:${System.properties.'user.home'}/.frontlinesms2/log4j.groovy"
+log4j = {
+    // Example of changing the log pattern for the default console
+    // appender:
+    //
+    //appenders {
+    //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
+    //}
+
+    warn 'org.codehaus.groovy.grails.web.servlet',  //  controllers
+	environments {
+		def layout = pattern(conversionPattern:'%d %-5p [%c{2}] %m%n')
+		production {
+			def conf = "${System.properties.'user.home'}/.frontlinesms2"
+			appender new RollingFileAppender(name:"prod",
+					layout:layout, file:"$conf/standard.log",
+					threshold:org.apache.log4j.Level.INFO)
+			appender new RollingFileAppender(name:"prod-stacktrace",
+					layout:layout, file:"$conf/stacktrace.log",
+					threshold:org.apache.log4j.Level.ERROR)
+			info 'prod-stacktrace'
+			error 'prod'
+		}
+		development {
+			appender new ConsoleAppender(name:'console-logger',
+					layout:layout,
+					threshold:org.apache.log4j.Level.INFO)
+			info 'console-logger'
+		}
+	}
+
+	warn 'org.codehaus.groovy.grails.web.servlet',  //  controllers
+		'org.codehaus.groovy.grails.web.pages', //  GSP
+		'org.codehaus.groovy.grails.web.sitemesh', //  layouts
+		'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
+		'org.codehaus.groovy.grails.web.mapping', // URL mapping
+		'org.codehaus.groovy.grails.commons', // core / classloading
+		'org.codehaus.groovy.grails.plugins', // plugins
+		'org.codehaus.groovy.grails.orm.hibernate', // hibernate integration
+		'org.springframework',
+		'org.hibernate',
+		'net.sf.ehcache.hibernate'
+}
+
+
+// Added by the JQuery Validation plugin:
+jqueryValidation.packed = true
+jqueryValidation.cdn = false  // false or "microsoft"
+jqueryValidation.additionalMethods = false
+
