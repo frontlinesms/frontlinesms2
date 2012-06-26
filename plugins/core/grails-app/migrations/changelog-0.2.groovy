51fc80fc5030df1a4844426a12a6cee3ba2663c8
geoffreymuchai <geoffreymuchai@gmail.com>
2012-06-26 09:35:14 +0300

    Release 2.0.2 RC1

---
+++ plugins/core/grails-app/migrations/changelog-0.2.groovy
@@ -0,0 +1,6 @@
+databaseChangeLog = {
+
+	changeSet(author: "geoffrey (generated)", id: "1340692427901-1") {
+		dropIndex(indexName: "TEXT_UNIQUE_1337355800008", tableName: "SYSTEM_NOTIFICATION")
+	}
+}
