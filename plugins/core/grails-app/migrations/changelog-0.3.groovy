c1ea0ded25c8e95259a778c22f967de70d406bdc
vaneyck <vaneycksemeotoki@gmail.com>
2012-07-05 17:01:18 +0300

    changelog fix again

--- plugins/core/grails-app/migrations/changelog-0.3.groovy
+++ plugins/core/grails-app/migrations/changelog-0.3.groovy
@@ -7,17 +7,13 @@ databaseChangeLog = {
 
 	changeSet(author: "geoffrey (generated)", id: "1341237212656-1") {
 		addColumn(tableName: "smslib_fconnection") {
-			column(name: "receive", type: "boolean") {
-				constraints(nullable: "false")
-			}
-		}
+			column(name: "receive", type: "boolean")
+		} 
 	}
 
 	changeSet(author: "geoffrey (generated)", id: "1341237212656-2") {
 		addColumn(tableName: "smslib_fconnection") {
-			column(name: "send", type: "boolean") {
-				constraints(nullable: "false")
-			}
+			column(name: "send", type: "boolean")
 		}
 	}
 }
