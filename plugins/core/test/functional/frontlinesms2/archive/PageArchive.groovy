40b60b3907497d849f0f3f0f0bba26469dc82769
Joy Kendi <joy@frontlinesms.com>
2012-07-23 17:35:22 +0300

    Fixed functional tests for archive

--- plugins/core/test/functional/frontlinesms2/archive/PageArchive.groovy
+++ plugins/core/test/functional/frontlinesms2/archive/PageArchive.groovy
@@ -6,3 +6,11 @@ class PageArchive extends geb.Page {
 		title.endsWith('Inbox archive')
 	}
 }
+
+class PageArchiveActivity extends PageArchive {
+	static def url = "archive/activity"
+
+	static at = {
+		title.contains('Activity Archive')
+	}
+}
