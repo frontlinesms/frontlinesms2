fe054b5b58e6c9a47f90b424cd2b7896f1191553
vaneyck <vaneycksemeotoki@gmail.com>
2012-07-26 16:13:09 +0300

    the all new wordcloud view using jqcloud

--- plugins/radio/grails-app/conf/RadioResources.groovy
+++ plugins/radio/grails-app/conf/RadioResources.groovy
@@ -2,8 +2,9 @@ modules = {
 	common {
 		dependsOn 'jquery, jquery-ui' // need to redefine these to make sure they are loaded first
 		resource url:[dir:'css', file:'radio.css']
+		resource url:[dir:'css', file:'jqcloud.css']
 		resource url:[dir:'js', file:'on_air.js'], disposition:'head'
-		resource url:[dir:'js', file:'jquery.tagcloud.js'], disposition:'head'
+		resource url:[dir:'js', file:'jqcloud-1.0.0.js'], disposition:'head'
 		resource url:[dir:'js', file:'word_cloud.js'], disposition:'head'
 	}
 }
