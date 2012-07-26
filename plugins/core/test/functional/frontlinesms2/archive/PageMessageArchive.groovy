6832c559c660ec304c8da5639a327d5ca0043a98
Joy Kendi <joy@frontlinesms.com>
2012-07-26 10:55:49 +0300

    Fixed archive functional tests

--- plugins/core/test/functional/frontlinesms2/archive/PageMessageArchive.groovy
+++ plugins/core/test/functional/frontlinesms2/archive/PageMessageArchive.groovy
@@ -4,23 +4,20 @@ import frontlinesms2.*
 import frontlinesms2.archive.PageArchive
 
 class PageMessageArchive extends frontlinesms2.message.PageMessage {
-	static url = 'archive/inbox'
+	static url = 'archive'
 
 	String convertToPath(Object[] args) {
 		def restOfPath = ""
-	    if (args[0] instanceof Fmessage)
-	    	restOfPath += "/show/"+(args[0] as Fmessage).id
+		if (args[0] instanceof Fmessage)
+			restOfPath += "/inbox/show/"+(args[0] as Fmessage).id
 	    else if (args[0] instanceof Number)
-	    	restOfPath += "/show/"+args[0]
-	    else if (args[0] instanceof Activity)
-	    	restOfPath +="/show"+(args[0] as Activity).id
-	    if (args.length > 1)
-	    {
-	    	if (args[1] instanceof Fmessage)
-	    		restOfPath +="/show/"+(args[1] as Fmessage).id
-	    	else if (args[1] instanceof Number)
-	    		restOfPath +="/show/"+args[1]
-	    }    
-	    return restOfPath
+			restOfPath += "/inbox/show/"+args[0]
+		else
+			restOfPath += "/activity/"+Activity.findByName(args[0])?.id+"?messageSection=activity&viewingMessages=true"
+		return restOfPath
+	}
+
+	static content = {
+		archiveAll {$('a', text:'Archive all')}
 	}
 }
