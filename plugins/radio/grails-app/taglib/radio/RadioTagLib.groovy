62bd20320fa226877957627181fb06ffcca523da
Alex Anderson <alex@frontlinesms.com>
2012-08-01 12:44:16 +0300

    Fixed layout of show-reassignment popup.

--- plugins/radio/grails-app/taglib/radio/RadioTagLib.groovy
+++ plugins/radio/grails-app/taglib/radio/RadioTagLib.groovy
@@ -7,31 +7,35 @@ class RadioTagLib {
 
 	def selectShow = { att ->
 		out << "<div id='select-radio-show' class='input'>"
-		out << "<label>Assign Activity to Radio Show</label>"
+println "att.hideLabel?.toBoolean() = ${att.hideLabel?.toBoolean()}"
+		if(!(att.hideLabel?.toBoolean())) {
+			println "att.hideLabel = $att.hideLabel (${att.hideLabel.getClass()})"
+			out << "<label>Assign Activity to Radio Show</label>"
+		}
 		if(att.activityInstance) {
 			att.radioShowInstance = RadioShow.findByOwnedActivity(att.activityInstance).list()[0]
 		}
-		if(att.formtag) {
+		if(att.wrapInFormTag) {
 			out << g.form(controller:"radioShow", action:"addActivity") {
 				out << g.hiddenField(name:"activityId", value:att.activityInstance?.id)
-				out << select(att)
+				selectShow_selecter(att)
 			}
 		} else {
-			out << select(att)
+			selectShow_selecter(att)
 		}
 		out << "</div>"
 	}
 
-	def select = { att ->
+	private def selectShow_selecter = { att ->
 		att.class = "radio-show-select"
 		att.name = "radioShowId"
-		att.noSelection = ['':att.radioShowInstance?g.message(code:'activity.assigned.defaultoption', args:[att.radioShowInstance.name]):g.message(code:'activity.unassigned.defaultoption')]
+		att.noSelection = ['':att.radioShowInstance?
+				g.message(code:'activity.assigned.defaultoption', args:[att.radioShowInstance.name]):
+				g.message(code:'activity.unassigned.defaultoption')]
 		att.optionKey = "id"
 		att.optionValue = "name"
 		att.value = att.radioShowInstance?.id
-		att.remove("activityInstance")
-		att.remove("radioShowInstance")
-		att.remove("formtag")
+		["activityInstance", "radioShowInstance", 'showLabel', "wrapInFormTag"].each { att.remove(it) }
 		out << g.select(att)
 	}
 
