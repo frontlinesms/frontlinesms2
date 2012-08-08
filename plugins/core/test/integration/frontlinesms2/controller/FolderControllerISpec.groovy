c48371389a52cc0b61909ce796f9db1cdab08987
Joy Kendi <joy@frontlinesms.com>
2012-08-08 10:47:17 +0300

    Restoring folder restores its messages as well

--- plugins/core/test/integration/frontlinesms2/controller/FolderControllerISpec.groovy
+++ plugins/core/test/integration/frontlinesms2/controller/FolderControllerISpec.groovy
@@ -36,5 +36,24 @@ class FolderControllerISpec extends grails.plugin.spock.IntegrationSpec {
 			Folder.findAllByDeleted(false) == [folder]
 			Folder.findAllByDeleted(true) == []
 	}
+
+	def "Restoring a folder restores its messages as well"() {
+		setup:
+			def folder = new Folder(name: 'test')
+			def message = new Fmessage(src:'Bob', text:'I like manchester', inbound:true, date: new Date()).save(failOnError: true, flush:true)
+			folder.addToMessages(message)
+			folder.save(failOnError:true, flush:true)
+			folder.deleted = true
+			folder.save(failOnError:true, flush:true)
+		when:
+			assert Folder.findAllByDeleted(true) == [folder]
+			controller.params.id  = folder.id
+			controller.restore()
+		then:
+			Folder.findAllByDeleted(false) == [folder]
+			Folder.findAllByDeleted(true) == []
+			folder.getFolderMessages([:]).find {message}
+
+	}
 }
 
