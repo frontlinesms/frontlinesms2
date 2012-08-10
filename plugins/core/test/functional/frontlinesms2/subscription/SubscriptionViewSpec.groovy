454e0125aa8d354441e137fcb1e7021707cbc5a0
Sitati Kituyi <sitati.kituyi@googlemail.com>
2012-08-10 11:27:59 +0300

    moved delete group with subscription func. test to subscriptionviewspec

--- plugins/core/test/functional/frontlinesms2/subscription/SubscriptionViewSpec.groovy
+++ plugins/core/test/functional/frontlinesms2/subscription/SubscriptionViewSpec.groovy
@@ -1,11 +1,33 @@
 package frontlinesms2.subscription
 
 import frontlinesms2.*
+import frontlinesms2.contact.*
 
 class SubscriptionViewSpec {
 	def ""(){}
 
 	def "Clicking the Quick Message button brings up the Quick Message Dialog with the group prepopulated as recipients"() {
 
+	}
+
+	def 'Deleting a group that is used in a subscription should fail with an appropriate error'(){
+		given:
+			def friendsGroup = new Group(name: "Friends").save()
+			def subscription = new Subscription(group:friendsGroup, name:"sign-me-up") // TODO populate with appropriate args
+		when:
+			to PageContactShow, friendsGroup
+		then:
+			waitFor { header.groupHeaderSection.displayed }
+		when:
+			header.moreGroupActions.value("delete").click()
+		then:
+			waitFor{ at DeleteGroupPopup }
+		when:
+			warningMessage == 'Are you sure you want to delete Friends? WARNING: This cannot be undone'
+			ok.jquery.trigger("click")
+		then:
+			at PageContactShow
+			bodyMenu.groupSubmenuLinks.contains("Friends")
+			notifications.flashMessagesText.contains("Cannot delete group Friends: is used by sign-me-up Subscription")
 	}
 }
