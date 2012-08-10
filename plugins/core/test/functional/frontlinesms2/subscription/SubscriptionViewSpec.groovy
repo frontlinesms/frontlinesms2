9c0f4084b06c041e794440fd87728b281ec515c0
vaneyck <vaneycksemeotoki@gmail.com>
2012-08-10 11:26:24 +0300

    added more tests definitions to SubscritpionViewSpec

--- plugins/core/test/functional/frontlinesms2/subscription/SubscriptionViewSpec.groovy
+++ plugins/core/test/functional/frontlinesms2/subscription/SubscriptionViewSpec.groovy
@@ -3,5 +3,15 @@ package frontlinesms2.subscription
 import frontlinesms2.*
 
 class SubscriptionViewSpec {
-	def ""(){}
+	def "subscription page should show the details of the subscription in the header"(){
+		/*
+		link to group
+		join autoreply and keyword
+		leave autoreply and keyword
+		toggle enables
+		group members count
+
+		*/
+	}
+	def "clicking the group link shoud redirect to the group page"(){}
 }
