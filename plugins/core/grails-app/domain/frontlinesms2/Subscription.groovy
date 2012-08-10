2eaf970658b5ce632ff048e20755dc340774752e
vaneyck <vaneycksemeotoki@gmail.com>
2012-08-10 18:14:09 +0300

    integration tests for SubscriptionISpec

--- plugins/core/grails-app/domain/frontlinesms2/Subscription.groovy
+++ plugins/core/grails-app/domain/frontlinesms2/Subscription.groovy
@@ -11,7 +11,59 @@ class Subscription extends Activity{
 	String leaveAliases
 
 	def addToMessages(def message) {}
-	def processKeyword(Fmessage message, boolean exactMatch) {}
-	Action getAction(String messageText, boolean exactMatch) {}
+	def processKeyword(Fmessage message, boolean exactMatch) {
+		def action = getAction(message.text,exactMatch)
+		def foundContact = Contact.findByMobile(message.src)
+		if(action == Action.JOIN){
+			 if(!foundContact){
+			 	group.addToMembers(new Contact(name:"", mobile:message.src).save(failOnError:true));
+			 }else{
+			 	group.addToMembers(foundContact);
+			 }
+		}else if(action == Action.LEAVE){
+			foundContact?.removeFromGroup(group)
+		}else if(action == Action.TOGGLE){
+			if(foundContact){
+				if(foundContact.isMemberOf(group)){
+					foundContact.removeFromGroup(group)
+				}else{
+					group.addToMembers(foundContact);
+				}
+			}else{
+				group.addToMembers(new Contact(name:"", mobile:message.src).save(failOnError:true));			
+			}
+		}
+	}
+
+	Action getAction(String messageText, boolean exactMatch) {
+		def words =  messageText.trim().toUpperCase().split(/\s+/)
+		if(words.size() == 1){
+			if(words[0].contains(keyword.value)){
+				if(hasAtLeastOneAlias(joinAliases,words[0])){
+					return Action.JOIN
+				}else if(hasAtLeastOneAlias(leaveAliases,words[0])){
+					return Action.LEAVE
+				}else if (exactMatch){
+					return Action.TOGGLE
+				}else if(!exactMatch){
+					return null;
+				}
+			}
+		}
+		else if (words.size() > 1 && exactMatch){
+			if(joinAliases.toUpperCase().split(",").contains(words[1].trim())){
+				return Action.JOIN
+		 	}else if(leaveAliases.toUpperCase().split(",").contains(words[1].trim())){
+		 		return Action.LEAVE
+		 	}else{
+		 		return Action.TOGGLE
+		 	}
+		}
+	}
+
+	def hasAtLeastOneAlias(aliases,message){
+		aliases.toUpperCase().split(",").contains(message.substring(keyword.value.length()))	
+	}
 }
+
 
