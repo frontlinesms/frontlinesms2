38a87892a1640d50790fe77cbea6d1356e8508a2
Joy Kendi <joy@frontlinesms.com>
2012-08-10 14:10:14 +0300

    Subscription validation functional tests

--- plugins/core/test/functional/frontlinesms2/subscription/SubscriptionCedSpec.groovy
+++ plugins/core/test/functional/frontlinesms2/subscription/SubscriptionCedSpec.groovy
@@ -14,7 +14,7 @@ class SubscriptionCedSpec extends SubscriptionBaseSpec  {
 		when:
 			subscription.click()
 		then:
-			waitFor { at SubscriptionDialog }
+			waitFor { at SubscriptionCreateDialog }
 	}
 
 	def "Can create a new subscription" () {
@@ -26,7 +26,7 @@ class SubscriptionCedSpec extends SubscriptionBaseSpec  {
 		when:
 			subscription.click()
 		then:
-			waitFor {at SubscriptionDialog}
+			waitFor {at SubscriptionCreateDialog}
 		when:
 			group.addToGroup Group.findByName('Friends').id.toString()
 			keywordText = 'friend'
@@ -85,5 +85,39 @@ class SubscriptionCedSpec extends SubscriptionBaseSpec  {
 			submit.click()
 		then:
 			waitFor { summary.message.displayed }
+	}
+
+	def "Should not proceed if subscription not named"() {
+		when:
+			launchSubscriptionPopup()
+			group.addToGroup Group.findByName('Friends').id.toString()
+			keywordText = 'friend'
+			enableJoinKeyword.click()
+			joinAliases = 'join, start'
+			enableLeaveKeyword.click()
+			leaveAliases = 'leave, stop'
+			next.click()
+		then:
+			waitFor {autoreply.displayed}
+		when:
+			enableJoinAutoreply.click()
+			joinAutoreplyText = "You have been successfully subscribed to Friends group"
+			enableLeaveAutoreply.click()
+			leaveAutoreplyText = "You have been unsubscribed from Friends group"
+		then:
+			waitFor { confirm.subscriptionName.displayed }
+		when:
+			submit.click()
+		then:
+			waitFor { error }
+			at SubscriptionCreateDialog
+	}
+
+	def launchSubscriptionPopup() {
+		to PageMessageInbox
+		bodyMenu.newActivity.click()
+		waitFor('slow') { at CreateActivityDialog }
+		subscription.click()
+		waitFor('slow') { at SubscriptionCreateDialog }
 	}
 }
