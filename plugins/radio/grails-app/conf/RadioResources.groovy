168b0a492e263dc9be2c620be86a4e94afea1f03
Alex Anderson <alex@frontlinesms.com>
2012-08-16 16:41:33 +0300

    Removed references to on_air javascript.

--- plugins/radio/grails-app/conf/RadioResources.groovy
+++ plugins/radio/grails-app/conf/RadioResources.groovy
@@ -4,7 +4,6 @@ modules = {
 		dependsOn 'frontlinesms-core'
 		resource url:[dir:'css', file:'radio.css']
 		resource url:[dir:'css', file:'jqcloud.css']
-		resource url:[dir:'js', file:'on_air.js'], disposition:'head'
 		resource url:[dir:'js', file:'jqcloud-1.0.0.js'], disposition:'head'
 		resource url:[dir:'js', file:'word_cloud.js'], disposition:'head'
 	}
