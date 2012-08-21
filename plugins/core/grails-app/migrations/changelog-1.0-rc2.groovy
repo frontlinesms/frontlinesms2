699fd6642743c9c926be7250ed7eb5f61a44d5fc
geoffreymuchai <geoffreymuchai@gmail.com>
2012-08-21 11:26:55 +0300

    Removed changeSet for creating trash index

--- plugins/core/grails-app/migrations/changelog-1.0-rc2.groovy
+++ plugins/core/grails-app/migrations/changelog-1.0-rc2.groovy
@@ -7,12 +7,5 @@ databaseChangeLog = {
 		addNotNullConstraint(columnDataType: "boolean", columnName: "SEND", tableName: "SMSLIB_FCONNECTION")
 	}
 
-	changeSet(author: "frontlinesms (generated)", id: "1345215027861-3") {
-		createIndex(indexName: "unique-object_id", tableName: "trash") {
-			column(name: "object_class")
-
-			column(name: "object_id")
-		}
-	}
 }
 
