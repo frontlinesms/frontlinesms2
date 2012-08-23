ef7400dc49158515780d638aaf765bdbb0bfa5ae
Sitati Kituyi <sitati.kituyi@googlemail.com>
2012-08-23 11:51:52 +0300

    test DB name is randomised to avoid database clashes when testing builds in parallel on CI

--- plugins/core/scripts/_Events.groovy
+++ plugins/core/scripts/_Events.groovy
@@ -35,7 +35,7 @@ eventTestPhaseStart = { phaseName ->
 
 eventTestStart = { testName ->
 	if (currentTestPhase == 'functional' || currentTestPhase == 'integration') {
-		def sql = Sql.newInstance('jdbc:h2:mem:testDb', 'sa', '', 'org.h2.Driver')
+		def sql = Sql.newInstance("jdbc:h2:mem:testDb${frontlinesms2.StaticApplicationInstance.uniqueId}", 'sa', '', 'org.h2.Driver')
 		sql.execute "SET REFERENTIAL_INTEGRITY FALSE"
 		sql.eachRow("SHOW TABLES") { table -> sql.execute('DELETE FROM ' + table.TABLE_NAME) } 
 		sql.execute "SET REFERENTIAL_INTEGRITY TRUE"
