7c7978081812a5f19deb48a33b6c3b4bbdf3f160
Sitati Kituyi <sitati.kituyi@googlemail.com>
2012-08-27 14:20:41 +0300

    added substitution variables for external command messages

--- plugins/core/grails-app/domain/frontlinesms2/ExternalCommand.groovy
+++ plugins/core/grails-app/domain/frontlinesms2/ExternalCommand.groovy
@@ -1,9 +1,23 @@
 package frontlinesms2
 
 class ExternalCommand extends Activity {
+
+	// HTTP Request methods
+	enum RequestType { POST, GET }
+
+	// Substitution variables
+	public static final String MESSAGE_BODY = "\${MESSAGE_BODY}"
+	public static final String MESSAGE_SOURCE_NUMBER = "\${MESSAGE_SRC_NUMBER}"
+	public static final String MESSAGE_SOURCE_NAME = "\${MESSAGE_SRC_NAME}"
+	public static final String MESSAGE_TIMESTAMP = "\${MESSAGE_TIMESTAMP}"
+
+	/// Variables
 	String url
 	String sendMethod
 	static hasOne = [keyword: Keyword]
-
+	RequestType requestType
+	
 	static constraints = {}
+
 }
+	
