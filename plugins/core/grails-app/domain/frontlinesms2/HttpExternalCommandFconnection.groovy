c06166b7abb04b49dc1a2e7a1905676820863fcd
Sitati Kituyi <sitati.kituyi@googlemail.com>
2012-08-28 11:14:01 +0300

    fixed bootstrap data for subscription

--- plugins/core/grails-app/domain/frontlinesms2/HttpExternalCommandFconnection.groovy
+++ plugins/core/grails-app/domain/frontlinesms2/HttpExternalCommandFconnection.groovy
@@ -10,6 +10,12 @@ import frontlinesms2.camel.exception.*
 class HttpExternalCommandFconnection extends Fconnection {
 	enum HttpMethod { POST, GET }
 
+	// Substitution variables
+	public static final String MESSAGE_BODY = "\${MESSAGE_BODY}"
+	public static final String MESSAGE_SOURCE_NUMBER = "\${MESSAGE_SRC_NUMBER}"
+	public static final String MESSAGE_SOURCE_NAME = "\${MESSAGE_SRC_NAME}"
+	public static final String MESSAGE_TIMESTAMP = "\${MESSAGE_TIMESTAMP}"
+
 	String url
 	HttpMethod httpMethod
 	static Fconnection.ConnectionRole connectionRole = Fconnection.ConnectionRole.FSMS_EXT_COMMAND
