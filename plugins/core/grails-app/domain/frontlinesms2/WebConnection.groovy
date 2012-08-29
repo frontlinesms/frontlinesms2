9335f1fd9116a939aa65c27caa4c9b09d5f90106
vaneyck <vaneycksemeotoki@gmail.com>
2012-08-29 12:37:18 +0300

    WebConnection removed references to Fconnection

--- plugins/core/grails-app/domain/frontlinesms2/WebConnection.groovy
+++ plugins/core/grails-app/domain/frontlinesms2/WebConnection.groovy
@@ -1,11 +1,20 @@
 package frontlinesms2
 
 class WebConnection extends Activity {
-
+	enum HttpMethod { POST, GET }
 	static String getShortName() { 'webConnection' }
 
+	// Substitution variables
+	public static final String MESSAGE_BODY = "\${MESSAGE_BODY}"
+	public static final String MESSAGE_SOURCE_NUMBER = "\${MESSAGE_SRC_NUMBER}"
+	public static final String MESSAGE_SOURCE_NAME = "\${MESSAGE_SRC_NAME}"
+	public static final String MESSAGE_TIMESTAMP = "\${MESSAGE_TIMESTAMP}"
+	
 	/// Variables
-	static hasOne = [keyword: Keyword, connection: HttpWebConnectionFconnection]
+	String url
+	HttpMethod httpMethod
+	static hasMany = [requestParameters:RequestParameter]
+	static hasOne = [keyword: Keyword]
 	
 	static constraints = {}
 
