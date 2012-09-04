384c26d19a01a89f036980f4d16c3a3ebc25c559
vaneyck <vaneycksemeotoki@gmail.com>
2012-09-04 12:10:54 +0300

    database migration for subscriptions

--- plugins/core/grails-app/conf/DataSource.groovy
+++ plugins/core/grails-app/conf/DataSource.groovy
@@ -23,27 +23,27 @@ environments {
 			logSql = true
 		}
 	}
-    test {
-        dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE"
-            logSql = true
+  test {
+    dataSource {
+        dbCreate = "update"
+        url = "jdbc:h2:mem:testDb;MVCC=TRUE"
+        logSql = true
+    }
+  }
+  production {
+    dataSource {
+        url = "jdbc:h2:${frontlinesms2.ResourceUtils.resourcePath}/prodDb;MVCC=TRUE"
+        pooled = true
+        properties {
+           maxActive = -1
+           minEvictableIdleTimeMillis=1800000
+           timeBetweenEvictionRunsMillis=1800000
+           numTestsPerEvictionRun=3
+           testOnBorrow=true
+           testWhileIdle=true
+           testOnReturn=true
+           validationQuery="SELECT 1"
         }
     }
-    production {
-        dataSource {
-            url = "jdbc:h2:${frontlinesms2.ResourceUtils.resourcePath}/prodDb;MVCC=TRUE"
-            pooled = true
-            properties {
-               maxActive = -1
-               minEvictableIdleTimeMillis=1800000
-               timeBetweenEvictionRunsMillis=1800000
-               numTestsPerEvictionRun=3
-               testOnBorrow=true
-               testWhileIdle=true
-               testOnReturn=true
-               validationQuery="SELECT 1"
-            }
-        }
-    }
+  }
 }
