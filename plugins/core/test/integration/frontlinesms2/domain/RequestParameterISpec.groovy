726b27ae21da7f8388bee4ccc90105eee79143e6
vaneyck <vaneycksemeotoki@gmail.com>
2012-09-05 14:45:58 +0300

    requestparameters integration tests finally working

---
+++ plugins/core/test/integration/frontlinesms2/domain/RequestParameterISpec.groovy
@@ -0,0 +1,27 @@
+package frontlinesms2.domain
+
+import frontlinesms2.*
+
+class RequestParameterISpec extends grails.plugin.spock.IntegrationSpec {
+	@spock.lang.Unroll
+	def "parameter in a message should be replaced with actual values"(){
+		when:
+			def message = Fmessage.build(text:"Sample text to send out", src:'12345')
+			def syncKeyword = new Keyword(value:"SYNC")
+			def parameter = new RequestParameter(name:'message', value:"\${}")
+			def webconnection = new WebConnection(name:"Sync", keyword:syncKeyword, url:"http://www.frontlinesms.com/sync", httpMethod:WebConnection.HttpMethod.GET)
+			webconnection.addToRequestParameters(parameter)
+			webconnection.save(failOnError:true)
+			
+			def p = RequestParameter.findByName("message")
+			p.value = "\${"+paramText+"}"
+			p.save(failOnError:true)
+		then:
+			returnval == parameter.getProcessedValue(message)
+		where:
+			returnval					|paramText
+			'Sample text to send out'	|"message_body"
+			'12345'						|"message_src_number"
+			'12345'						|"message_src_name"
+	}
+}
