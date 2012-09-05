f433dc16192b4f9ddf225c151c6325ce2ac71aa8
Alex Anderson <alex@frontlinesms.com>
2012-09-05 15:07:19 +0300

    Removed legacy dependsOn from plugin groovy descriptor.

--- plugins/core/CoreGrailsPlugin.groovy
+++ plugins/core/CoreGrailsPlugin.groovy
@@ -1,7 +1,6 @@
 class CoreGrailsPlugin {
     def version = "2.0-SNAPSHOT"
     def grailsVersion = "2.0.3 > *"
-    def dependsOn = [csv:"0.3.1", jquery:"1.7.1", routing:"1.2.0"]
 	def pluginExcludes = ["grails-app/views/error.gsp",
 			"grails-app/conf/CoreBootStrap.groovy",
 			"grails-app/conf/CoreUrlMappings.groovy"]
