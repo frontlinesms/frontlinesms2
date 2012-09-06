9c736daf84afa5cdc15dc26a7fdc1a99343ad74f
vaneyck <vaneycksemeotoki@gmail.com>
2012-09-06 11:40:00 +0300

    added edit test for WebConnectioncontrollerISpec

--- plugins/core/test/integration/frontlinesms2/controller/WebConnectionControllerISpec.groovy
+++ plugins/core/test/integration/frontlinesms2/controller/WebConnectionControllerISpec.groovy
@@ -43,4 +43,25 @@ class WebConnectionControllerISpec extends grails.plugin.spock.IntegrationSpec {
 		then:
 			WebConnection.get(controller.params.ownerId).name == "New WebConnection name"
 	}
+
+	def 'edit should save all the details from the walkthrough'(){
+		setup:
+			def keyword = new Keyword(value:'TEST')
+			def webConnection = new WebConnection(name:"Old WebConnection name", keyword:keyword, url:"www.frontlinesms.com/sync",httpMethod:WebConnection.HttpMethod.POST)
+			webConnection.save(failOnError:true)
+
+			controller.params.ownerId = webConnection.id
+			controller.params.name = "New WebConnection name"
+			controller.params.url = "www.frontlinesms.com/syncing"
+			controller.params.httpMethod = "get"
+			controller.params.keyword = "keyword"
+		when:
+			controller.save()
+		then:
+			webConnection.refresh()
+			webConnection.name == "New WebConnection name"
+			webConnection.keyword.value == "KEYWORD"
+			webConnection.httpMethod == WebConnection.HttpMethod.GET
+			webConnection.nurl == "www.frontlinesms.com/syncing"
+	}
 }
