54a99bc2f03133a1c1a8970522be4427acc8ef24
vaneyck <vaneycksemeotoki@gmail.com>
2012-09-06 11:55:03 +0300

    integration tests for webconnectioncontrollerISec

--- plugins/core/test/integration/frontlinesms2/controller/WebConnectionControllerISpec.groovy
+++ plugins/core/test/integration/frontlinesms2/controller/WebConnectionControllerISpec.groovy
@@ -27,23 +27,6 @@ class WebConnectionControllerISpec extends grails.plugin.spock.IntegrationSpec {
 			WebConnection.findByName("Test WebConnection").name == controller.params.name
 	}
 
-	def 'save action should edit a webconnection and change the name'(){
-		setup:
-			def keyword = new Keyword(value:'TEST')
-			def webConnection = new WebConnection(name:"Old WebConnection name", keyword:keyword, url:"www.frontlinesms.com/sync",httpMethod:WebConnection.HttpMethod.GET)
-			webConnection.save(failOnError:true)
-
-			controller.params.ownerId = webConnection.id
-			controller.params.name = "New WebConnection name"
-			controller.params.url = "www.frontlinesms.com/sync"
-			controller.params.httpMethod = "get"
-			controller.params.keyword = "keyword"
-		when:
-			controller.save()
-		then:
-			WebConnection.get(controller.params.ownerId).name == "New WebConnection name"
-	}
-
 	def 'edit should save all the details from the walkthrough'(){
 		setup:
 			def keyword = new Keyword(value:'TEST')
@@ -55,6 +38,8 @@ class WebConnectionControllerISpec extends grails.plugin.spock.IntegrationSpec {
 			controller.params.url = "www.frontlinesms.com/syncing"
 			controller.params.httpMethod = "get"
 			controller.params.keyword = "keyword"
+			controller.params.'params-name' = "['username', 'password']"
+			controller.params.'params-value' = "['bob','secret']"
 		when:
 			controller.save()
 		then:
@@ -62,6 +47,8 @@ class WebConnectionControllerISpec extends grails.plugin.spock.IntegrationSpec {
 			webConnection.name == "New WebConnection name"
 			webConnection.keyword.value == "KEYWORD"
 			webConnection.httpMethod == WebConnection.HttpMethod.GET
-			webConnection.nurl == "www.frontlinesms.com/syncing"
+			webConnection.url == "www.frontlinesms.com/syncing"
+			webConnection.requestParameters*.name.containsAll(['username','password'])
+			webConnection.requestParameters*.value.containsAll(['bob','secret'])
 	}
 }
