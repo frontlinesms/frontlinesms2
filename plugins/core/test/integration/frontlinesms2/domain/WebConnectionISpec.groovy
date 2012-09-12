f187f7c1be3f84c4c29860273778f7f744972322
Sitati Kituyi <sitati.kituyi@googlemail.com>
2012-09-12 15:55:34 +0300

    WebConnectionServiceISpec fixes

--- plugins/core/test/integration/frontlinesms2/domain/WebConnectionISpec.groovy
+++ plugins/core/test/integration/frontlinesms2/domain/WebConnectionISpec.groovy
@@ -1,33 +1,37 @@
 package frontlinesms2.domain
 
 import frontlinesms2.*
-
 import spock.lang.*
+import grails.test.mixin.*
+
 
 class WebConnectionISpec extends grails.plugin.spock.IntegrationSpec {
-	def webConnectionService
+	def webCService = Mock(WebConnectionService)
 
 	def 'incoming message matching keyword should trigger http message sending'() {
 		given:
 			def k = new Keyword(value:'FORWARD')
 			def webConnection = new WebConnection(name:"Sync", keyword:k, url:"www.frontlinesms.com/sync",httpMethod:WebConnection.HttpMethod.GET).save(failOnError:true)
+			webConnection.webConnectionService = webCService
+			webConnection.save(failOnError:true)
 			def incomingMessage = Fmessage.build(text:"FORWARD ME", src:'123123')
-			webConnection.addToMessages(incomingMessage).save(failOnError:true)
 		when:
 			webConnection.processKeyword(incomingMessage, true)
 		then:
-			1 * webConnectionService.send(incomingMessage)
+			1 * webCService.send(incomingMessage)
 	}
 
 	def 'incoming message should match if keyword is blank and exactmatch == false'() {
 		given:
 			def k = new Keyword(value:'')
 			def webConnection = new WebConnection(name:"Sync", keyword:k, url:"www.frontlinesms.com/sync",httpMethod:WebConnection.HttpMethod.GET).save(failOnError:true)
+			webConnection.webConnectionService = webCService
+			webConnection.save(failOnError:true)
 			def incomingMessage = Fmessage.build(text:"FORWARD ME", src:'123123')
 			webConnection.addToMessages(incomingMessage).save(failOnError:true)
 		when:
 			webConnection.processKeyword(incomingMessage, false)
 		then:
-			1 * webConnectionService.send(incomingMessage)
+			1 * webCService.send(incomingMessage)
 	}
 }
