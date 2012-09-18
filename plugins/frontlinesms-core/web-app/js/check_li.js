5413ffc5ecf4144d4d71c13880a2444ac2adefa1
geoffreymuchai <geoffreymuchai@gmail.com>
2012-09-18 17:22:31 +0300

    Fixed select all contacts UI

--- plugins/frontlinesms-core/web-app/js/check_li.js
+++ plugins/frontlinesms-core/web-app/js/check_li.js
@@ -147,7 +147,7 @@ function updateCheckAllBox(count) {
 	// Check whether all messages are checked
 	if(count == $('#main-list tbody tr :checkbox').size() && !$('#main-list :checkbox')[0].checked) {
 		$('#main-list :checkbox')[0].checked = true;
-	} else if(count == $('#main-list li input:checked').size() && !$('#main-list li input:checkbox')[0].checked) {
+	} else if(count == $('#main-list li:not(:first-child) input:checkbox').size() && !$('#main-list li input:checkbox')[0].checked) {
 		$('#main-list :checkbox')[0].checked = true;
 	} else if($('#main-list :checkbox')[0].checked) {
 		$('#main-list :checkbox')[0].checked = false;
