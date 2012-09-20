3143435821611aa9ca3671545aeb90a453e4df7f
Alex Anderson <alex@frontlinesms.com>
2012-09-20 17:25:39 +0300

    Renamed search URL mapping spec to fit convention.

--- plugins/frontlinesms-core/test/unit/SearchUrlMappingSpec.groovy
+++ plugins/frontlinesms-core/test/unit/SearchUrlMappingSpec.groovy
@@ -5,8 +5,7 @@ import frontlinesms2.*
 
 @TestFor(CoreUrlMappings)
 @Mock(SearchController)
-class SearchUrlMappingITests extends Specification {
-
+class SearchUrlMappingSpec extends Specification {
 	def testSearchMapping() {
 		assertForwardUrlMapping('/search/result/show/123', controller:'search', action:'result') {
 			messageId = 123
