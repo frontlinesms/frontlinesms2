0a6e13ae69fc69538c5566f2c717920b2e9964cb
Sitati Kituyi <sitati.kituyi@googlemail.com>
2012-09-26 15:09:04 +0300

    UshahidiWebConnectionSpec: unit test for pre- and post-process

--- plugins/frontlinesms-core/test/unit/frontlinesms2/UshahidiWebConnectionSpec.groovy
+++ plugins/frontlinesms-core/test/unit/frontlinesms2/UshahidiWebConnectionSpec.groovy
@@ -30,5 +30,46 @@ class UshahidiWebConnectionSpec extends CamelUnitSpecification {
 			''		|true		|false | WebConnection.HttpMethod.POST
 			null	|true		|false | WebConnection.HttpMethod.POST
 	}
+
+	def "Test Ushahidi pre-processor"() {
+		when:
+			def message = Fmessage.build(text:"testing", src:"bob")
+			def connection = new UshahidiWebConnection(name:name, keyword:keyword, url:"www.ushahidi.com/frontlinesms2", httpMethod:method)
+			def s = Mock(RequestParameter)
+			s >> ['name':'s', 'value':'${message_src_name}']
+			def m = Mock(RequestParameter)
+			m >> ['name':'m', 'value':'${message_body}']
+			def key = Mock(RequestParameter)
+			key >> ['name':'key', 'value':'1234567']
+
+			[s, m, key].each { connection.addToRequestParameters(it) }
+
+			Exchange exchange = Mock(Exchange)
+			exchange.in >> mockExchangeMessage(['frontlinesms.fmessageId':message.id,'frontlinesms.webConnectionId':webconnection.id], null)
+			exchange.out >> mockExchangeMessage([:], null)
+			exchange.unitOfWork >> Mock(UnitOfWork)
+			connection.preProcess(exchange)
+		then:
+			1* exchange.out.setBody({ bodyContent ->
+				bodyContent.contains("m=testing") && 
+				bodyContent.contains("s=bob") && 
+				bodyContent.contains("key=1234567")
+			})
+	}
+
+	def "Test Ushahidi post-processor"() {
+		when:
+			def thisTest = "TODO"
+			// TODO: if/when we decide to handle different Ushahidi response codes with appropriate actions, the post processor
+			// tests need to be expanded to cover this. Currently it just logs the output after 3 attempts.
+		then:
+			true
+	}
 	
+	def mockExchangeMessage(headers, body) {
+		def m = Mock(Message)
+		m.body >> body
+		m.headers >> headers
+		return m
+	}
 }
