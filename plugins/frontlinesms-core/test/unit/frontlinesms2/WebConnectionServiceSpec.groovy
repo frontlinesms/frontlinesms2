d7f57d09e169356bfeba504a73e194a06ba8d05d
Alex Anderson <alex@frontlinesms.com>
2012-09-26 15:24:28 +0300

    Wrote tests for refactor of WebConnection service methods -> inside domain objects.

--- plugins/frontlinesms-core/test/unit/frontlinesms2/WebConnectionServiceSpec.groovy
+++ plugins/frontlinesms-core/test/unit/frontlinesms2/WebConnectionServiceSpec.groovy
@@ -1,26 +1,49 @@
 package frontlinesms2
 
-import static org.junit.Assert.*
+import spock.lang.*
 
-import grails.test.mixin.*
-import grails.test.mixin.support.*
-import org.junit.*
+import org.apache.camel.Exchange
+import org.apache.camel.Message
 
-/**
- * See the API for {@link grails.test.mixin.support.GrailsUnitTestMixin} for usage instructions
- */
-@TestMixin(GrailsUnitTestMixin)
-class WebConnectionServiceSpec {
+@TestFor(WebConnectionService)
+class WebConnectionServiceSpec extends Specification {
+	def requestedId
+	def mockConnection
 
-    void setUp() {
-        // Setup logic here
-    }
+	def setup() {
+		mockConnection = Mock(WebConnection)
+		WebConnection.metaClass.static.get = { id ->
+			requestedId = id
+			return mockConnection
+		}
+	}
 
-    void tearDown() {
-        // Tear down logic here
-    }
+	def 'preprocess call is handed back to the relevant domain object'() {
+		given:
+			def x = mockExchange(null, ['webconnection-id':'123'])
+		when:
+			service.preProcess(x)
+		then:
+			requestedId == '123'
+			1 * mockConnection.preProcess(x)
+	}
 
-    void testSomething() {
-        fail "Implement me"
-    }
+	def 'postprocess call is handed back to the relevant domain object'() {
+		given:
+			def x = mockExchange(null, ['webconnection-id':'123'])
+		when:
+			service.postProcess(x)
+		then:
+			requestedId == '123'
+			1 * mockConnection.postProcess(x)
+	}
+
+	Exchange mockExchange(body, Map headers) {
+		Exchange x = Mock()
+		def inMessage = Mock(Message)
+		if(body) inMessage.body >> body
+		inMessage.headers >> headers
+		x.in >> inMessage
+		return x
+	}
 }
