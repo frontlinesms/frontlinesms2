634f08e3e117f63413d4d4739a9a9e8243952212
ivermac <mark.ekisa@gmail.com>
2012-09-26 12:51:51 +0300

    Refactored functional tests for WebConnections.

---
+++ plugins/frontlinesms-core/test/functional/frontlinesms2/webconnection/UshahidiWebConnectionCedSpec.groovy
@@ -0,0 +1,9 @@
+package frontlinesms2.webconnection
+
+import frontlinesms2.*
+import frontlinesms2.message.*
+import frontlinesms2.popup.*
+
+class UshahidiWebConnectionCedSpec extends WebConnectionBaseSpec {
+}
+
