fe90d7c4d4a893a32596d494fb718e6856b7535f
mike <mike@frontlinesms.com>
2012-10-11 09:41:12 +0300

    Alex|Mike: implemented func. test for broken help link

---
+++ plugins/frontlinesms-core/test/functional/frontlinesms2/help/HelpFSpec.groovy
@@ -0,0 +1,16 @@
+package frontlinesms2.help
+
+import frontlinesms2.popup.*
+import frontlinesms2.*
+import frontlinesms2.message.PageMessageInbox
+
+class HelpFSpec extends grails.plugin.geb.GebSpec{
+	def 'help link should open the help dialog'() {
+		given: 'the inbox is open'
+			to PageMessageInbox
+		when: 'the help link is clicked'
+			systemMenu.help.click()
+		then: 'the help dialog is visible'
+			at PageHelp
+	}
+}
---
+++ plugins/frontlinesms-core/test/functional/frontlinesms2/help/PageHelp.groovy
@@ -0,0 +1,8 @@
+package frontlinesms2.help
+
+import frontlinesms2.*
+import frontlinesms2.popup.MediumPopup
+
+class PageHelp extends MediumPopup {
+	static at = { popupTitle.contains("help") }
+}
