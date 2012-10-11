73bd92ae0af7ec52e5a8a263c756a0604ae57e73
mike <mike@frontlinesms.com>
2012-10-11 10:06:32 +0300

    Alex|Mike: fixed help dialog display

--- plugins/frontlinesms-core/test/functional/frontlinesms2/help/PageHelp.groovy
+++ plugins/frontlinesms-core/test/functional/frontlinesms2/help/PageHelp.groovy
@@ -4,5 +4,5 @@ import frontlinesms2.*
 import frontlinesms2.popup.MediumPopup
 
 class PageHelp extends MediumPopup {
-	static at = { popupTitle.contains("help") }
+	static at = { waitFor { popupTitle.contains("help") } }
 }
