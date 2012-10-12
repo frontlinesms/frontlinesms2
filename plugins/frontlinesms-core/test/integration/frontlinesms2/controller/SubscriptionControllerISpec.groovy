75482d1da42fa0e5e1fd5d99bed6d7c95d544cd5
Vaneyck <vaneycksemeotoki@gmail.com>
2012-10-12 15:11:51 +0300

    fix subscriptioncontrollerISpec fix

--- plugins/frontlinesms-core/test/integration/frontlinesms2/controller/SubscriptionControllerISpec.groovy
+++ plugins/frontlinesms-core/test/integration/frontlinesms2/controller/SubscriptionControllerISpec.groovy
@@ -11,7 +11,7 @@ class SubscriptionControllerISpec extends grails.plugin.spock.IntegrationSpec {
 	def controller
 	def trashService
 	def setup() {
-		controller = new subscriptionController()
+		controller = new SubscriptionController()
 	}
 
 	//> CED tests
