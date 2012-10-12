9deaf6f521e7511b57ce0c6d1854032dc14b0f73
Vaneyck <vaneycksemeotoki@gmail.com>
2012-10-12 15:08:11 +0300

    refactor subscriptioniSpec

--- plugins/frontlinesms-core/grails-app/domain/frontlinesms2/Subscription.groovy
+++ plugins/frontlinesms-core/grails-app/domain/frontlinesms2/Subscription.groovy
@@ -115,6 +115,7 @@ class Subscription extends Activity{
 
 	Action getAction(Keyword k) {
 		def actionText = k.ownerDetail
+		println "### OwnerDetail ## ${k.ownerDetail}"
 		if(actionText == Action.JOIN.toString()){
 			return Action.JOIN
 		} else if(actionText == Action.LEAVE.toString()){
