c5ddafc42345a3e1f54db331b8e42b7ac9655187
Alex Anderson <alex@frontlinesms.com>
2012-10-16 11:37:56 +0300

    Added failing test for CORE-1541

--- plugins/frontlinesms-core/test/integration/frontlinesms2/controller/ImportControllerISpec.groovy
+++ plugins/frontlinesms-core/test/integration/frontlinesms2/controller/ImportControllerISpec.groovy
@@ -12,15 +12,12 @@ class ImportControllerISpec extends grails.plugin.spock.IntegrationSpec {
 	}
 
 	def 'Uploading a contacts CSV file should create new contacts and groups in the database'() {
-		given:
-			mockFileUpload('importCsvFile', '''"Name","Mobile Number","Other Mobile Number","E-mail Address","Current Status","Notes","Group(s)"
+		when:
+			importContacts('''"Name","Mobile Number","Other Mobile Number","E-mail Address","Current Status","Notes","Group(s)"
 "Alice Sihoho","+254728749000","","","true","","/ToDo/Work"
 "Amira Cheserem","+254715840801","","","true","","/ToDo/Work"
 "anyango Gitu","+254727689908","","","true","","/isIt\\/ToDo/Work/jobo"
 ''')
-		when:
-			// file is uploaded
-			controller.importContacts()
 		then:
 			// check that contacts and groups were created
 			Contact.list()*.name.sort() == ['Alice Sihoho', 'Amira Cheserem', 'anyango Gitu']
@@ -29,12 +26,11 @@ class ImportControllerISpec extends grails.plugin.spock.IntegrationSpec {
 	
 	def 'Uploading a contacts CSV file with failed contacts should create failed contacts in a file'() {
 		given:
-			mockFileUpload('importCsvFile', '''"Name","Mobile Number","Other Mobile Number","E-mail Address","Current Status","Notes","Group(s)"
+			importContacts('''"Name","Mobile Number","Other Mobile Number","E-mail Address","Current Status","Notes","Group(s)"
 "Alice Sihoho254728749000","","","true","","/ToDo/Work"
 "Amira Cheserem","+254715840801","","","true","","/ToDo/Work"
 "anyango Gitu","+254727689908","","","true","","/isIt\\/ToDo/Work/jobo"
 ''')
-			controller.importContacts()
 		when:
 			// failed contacts file is downloaded
 			controller.failedContacts()
@@ -46,18 +42,30 @@ class ImportControllerISpec extends grails.plugin.spock.IntegrationSpec {
 "Alice Sihoho254728749000","","","true","","/ToDo/Work"
 '''
 	}
+
+	def 'uploading contacts with backslash characters should not cause issues'() {
+		when:
+			importContacts('''"Name","Mobile Number","E-mail Address","Notes","Group(s)","lake","town"
+"Alex","0702597711",,,"\\o/ team",,
+"Enock","0711756950",,,"\\o/ team",,
+"Geoff","0725675317",,,"\\o/ team",,
+"Vaneyck","0723127992",,,"\\o/ team",,
+''')
+		then:
+			Group.list()*.name == ['\\o/']
+			Contact.count() == 4
+			def group = Group.findAll().first()
+			Contact.findAll().every { it.isMemberOf(group) }
+	}
 	
 	def 'Uploading a messages CSV file from version 1 should create new messages and folder in the database'() {
-		given:
-			mockFileUpload('importCsvFile', '''"Message Type","Message Status","Message Date","Message Content","Sender Number","Recipient Number"
+		when:
+			importMessages('''"Message Type","Message Status","Message Date","Message Content","Sender Number","Recipient Number"
 "Received","Received","2012-02-16 16:42:24","Message Received Msg1.","Safaricom","254704593656"
 "Received","Received","2012-02-24 17:22:59","Message Received Msg2.","254705693656","254704593656"
 "Sent","Failed","2012-03-09 12:48:42","Message Sent Msg1","* N/A *","0720330266"
 "Sent","Sent","2012-03-07 12:19:41","Message Sent Msg2","* N/A *","144"
 ''')
-		when:
-			// file is uploaded
-			controller.importMessages()
 		then:
 			// check that messages and folders were created
 			Fmessage.list()*.text.sort() == ['Message Received Msg1.', 'Message Received Msg2.', 'Message Sent Msg1', 'Message Sent Msg2']
@@ -66,14 +74,12 @@ class ImportControllerISpec extends grails.plugin.spock.IntegrationSpec {
 	}
 	
 	def 'Uploading a messages CSV file from version 2 should create new messages and folder in the database'() {
-		given:
-			mockFileUpload('importCsvFile', '''"DatabaseID","Source Name","Source Mobile","Destination Name","Destination Mobile","Text","Date Created"
+		when:
+			importMessages('''"DatabaseID","Source Name","Source Mobile","Destination Name","Destination Mobile","Text","Date Created"
 "302","Simon","+123987123","","[]","Message 1","2012-07-27 10:22:02.943"
 "302","Says","+123987123","","[]","Message 2","2012-07-27 10:22:02.943"
 "302","Import","+123987123","","[]","Message 3","2012-07-27 10:22:02.943"
 ''')
-		when:
-			controller.importMessages()
 		then:
 			// check that messages and folders were created
 			Fmessage.list()*.text.sort() == ['Message 1', 'Message 2', 'Message 3']
@@ -82,8 +88,8 @@ class ImportControllerISpec extends grails.plugin.spock.IntegrationSpec {
 	}
 
 	def 'Uploading a messages CSV file from version 2 should be able to handle line breaks in messages'() {
-		given:
-			mockFileUpload('importCsvFile', '''"DatabaseID","Source Name","Source Mobile","Destination Name","Destination Mobile","Text","Date Created"
+		when:
+			importMessages('''"DatabaseID","Source Name","Source Mobile","Destination Name","Destination Mobile","Text","Date Created"
 "27",,+12345678,"[Bobby Briggs]","[+2547123456]","Joyce
 Vancouver
 Siloi
@@ -92,8 +98,6 @@ Amelia
 Georgina
 Shantelle","2012-06-12 15:58:44.488"
 ''')
-		when:
-			controller.importMessages()
 		then:
 			// check that messages and folders were created
 			Fmessage.list()*.text.sort() == ['''Joyce
@@ -108,26 +112,32 @@ Shantelle''']
 	}
 
 	def 'Uploading a message with a very long content field results in the message content being...'() {
-		given:
-			mockFileUpload('importCsvFile', '''"Message Type","Message Status","Message Date","Message Content","Sender Number","Recipient Number"
+		when:
+			importMessages('''"Message Type","Message Status","Message Date","Message Content","Sender Number","Recipient Number"
 "Received","Received","2012-02-16 16:42:24","''' + ('0123456789ABCDEF' * 256)  + '''","Safaricom","254704593656"
 "Received","Received","2012-02-24 17:22:59","short message","254705693656","254704593656"
 ''')
-		when:
-			controller.importMessages()
 		then:
 			Fmessage.list()*.text == ['short message', ('0123456789ABCDEF' * 256)[0..1598] + '…']
 	}
 
 	def 'Uploading a CSV with a BOM should not cause issues'() {
-		given:
-			mockFileUpload('importCsvFile', '''"\uFEFFMessage Type","Message Status","Message Date","Message Content","Sender Number","Recipient Number"
+		when:
+			importMessages('''"\uFEFFMessage Type","Message Status","Message Date","Message Content","Sender Number","Recipient Number"
 "Received","Received","2012-02-16 16:42:24","+123456789","Safaricom","254704593656"
 ''')
-		when:
-			controller.importMessages()
 		then:
 			Fmessage.list()*.inbound == [true]
+	}
+
+	def importMessages(String fileContent) {
+		mockFileUpload('importCsvFile', fileContent)
+		controller.importMessages()
+	}
+
+	def importContacts(String fileContent) {
+		mockFileUpload('importCsvFile', fileContent)
+		controller.importContacts()
 	}
 
 	def mockFileUpload(filename, fileContent) {
