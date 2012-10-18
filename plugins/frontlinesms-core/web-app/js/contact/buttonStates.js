2651d59d293053ba3f2d623b86547d1057f0ae14
Alex Anderson <alex@frontlinesms.com>
2012-10-18 10:57:54 +0300

    Fixed some JS to remove jslint violations.

--- plugins/frontlinesms-core/web-app/js/contact/buttonStates.js
+++ plugins/frontlinesms-core/web-app/js/contact/buttonStates.js
@@ -1,10 +1,11 @@
 function enableSaveAndCancel() {
+	var element, selecters;
 	// Some of these elements are links, and some of them are
 	// <input> tags which are replaced with links.  Handling
 	// is slightly different for each.
-	var selecters = ["#update-single", "#update-all", ".buttons .cancel"];
+	selecters = ["#update-single", "#update-all", ".buttons .cancel"];
 	for(i=selecters.length-1; i>=0; --i) {
-		var element = $(selecters[i]);
+		element = $(selecters[i]);
 		element.removeAttr("disabled");
 		element.removeClass("disabled");
 		if(element.hasClass("fsms-button-replaced")) {
--- plugins/frontlinesms-core/web-app/js/graph/graph-utils.js
+++ plugins/frontlinesms-core/web-app/js/graph/graph-utils.js
@@ -1,21 +1,24 @@
 Array.prototype.sum = function() {
-	var sum = 0;
-	for (var i = 0; i < this.length; sum += this[i++]) {}
+	var i, sum;
+	sum = 0;
+	for (i=0; i<this.length; i++) {
+		sum+=this[i];
+	}
 	return sum;
 };
+
 Array.prototype.max = function() {
-	return Math.max.apply({},
-	this);
+	return Math.max.apply({}, this);
 };
+
 Array.prototype.min = function() {
-	return Math.min.apply({},
-	this);
+	return Math.min.apply({}, this);
 };
 
 Array.prototype.pack = function() {
-	var x = [],
+	var i, x = [],
 		length = this[0].length;
-	for (var i = 0; i < length; i++) {
+	for (i=0; i<length; i++) {
 		x[i] = 0;
 		$.each(this, function(index, value) {
 			x[i] += value[i];
