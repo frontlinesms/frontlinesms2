e7ce6a248d44291d66fbe142641fefeb12e5415a
geoffreymuchai <geoffreymuchai@gmail.com>
2012-11-02 16:46:07 +0300

    Fixed bug CORE-1626: Activity and Folder filters show new messages after a 10 secs

--- plugins/frontlinesms-core/grails-app/controllers/frontlinesms2/MessageController.groovy
+++ plugins/frontlinesms-core/grails-app/controllers/frontlinesms2/MessageController.groovy
@@ -39,9 +39,13 @@ class MessageController {
 				messageCount = Fmessage."$section"(params.starred).count()
 			}
 		} else if(section == 'activity') {
-			messageCount = Activity.get(params.ownerId)?.getActivityMessages(params.starred)?.count()
+			def getSent = null
+			if(params.inbound) getSent = Boolean.parseBoolean(params.inbound)
+			messageCount = Activity.get(params.ownerId)?.getActivityMessages(params.starred, getSent)?.count()
 		} else if(section == 'folder') {
-			messageCount = Folder.get(params.ownerId)?.getFolderMessages(params.starred)?.count()
+			def getSent = null
+			if(params.inbound) getSent = Boolean.parseBoolean(params.inbound)
+			messageCount = Folder.get(params.ownerId)?.getFolderMessages(params.starred, getSent)?.count()
 		} else messageCount = 0
 		render messageCount
 	}
--- plugins/frontlinesms-core/test/functional/frontlinesms2/message/PageMessage.groovy
+++ plugins/frontlinesms-core/test/functional/frontlinesms2/message/PageMessage.groovy
@@ -53,8 +53,8 @@ class ContentFooter extends geb.Module {
 	static content = {
 		showAll { $('a')[0] }
 		showStarred { $('a')[1] }
-		showSent { $('a')[3] }
-		showReceived { $('a')[2] }
+		showOutgoing { $('a')[3] }
+		showIncoming { $('a')[2] }
 		nextPage { $('a.nextLink') }
 		prevPage { $('a.prevLink') }
 	}
