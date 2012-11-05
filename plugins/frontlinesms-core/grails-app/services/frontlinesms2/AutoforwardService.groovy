d3db1405d2cc30c8098edfd6aec813ed2d692a7c
Vaneyck <vaneycksemeotoki@gmail.com>
2012-11-05 17:14:57 +0300

    Added Autoforward popup ui

---
+++ plugins/frontlinesms-core/grails-app/services/frontlinesms2/AutoforwardService.groovy
@@ -0,0 +1,44 @@
+package frontlinesms2
+
+import frontlinesms2.*
+
+class AutoforwardService {
+
+    def saveInstance(Autoforward autoforward, params) {
+		autoforward.name = params.name ?: autoforward.name
+		autoforward.autoforwardText = params.messageText ?: autoforward.autoforwardText
+		autoforward.keywords?.clear()
+
+		def newContacts = params.addresses.collect { return Contact.findByMobile(it)?:new Contact(mobile:it).save() }
+		(autoforward.contacts - newContacts).each { autoforward.removeFromContacts(it) }
+		(newContacts - autoforward.contacts).each { autoforward.addToContacts(it) }
+
+		def newGroups = params.groups.collect { return Group.get(it.substring(it.length()-2,it.length()-1)) }
+
+		((autoforward.groups + autoforward.smartGroups) - newGroups).each{
+			if(group instanceof SmartGroup){ autoforward.removeFromSmartGroups(group) }
+			else if (group instanceof Group){ autoforward.removeFromGroups(group) }
+		}
+
+		(newGroups - (autoforward.groups + autoforward.smartGroups)).each{
+			if(group instanceof SmartGroup){ autoforward.addToSmartGroups(group) }
+			else if (group instanceof Group){ autoforward.addToGroups(group) }
+		}
+		
+		autoforward.save(flush:true, failOnError:true)
+
+		if(params.sorting == 'global'){
+			autoforward.addToKeywords(new Keyword(value:''))
+		}else if(params.sorting == 'enabled'){
+			def keywordRawValues = params.keywords?.toUpperCase().replaceAll(/\s/, "").split(",")
+			for(keywordValue in keywordRawValues) {
+				def keyword = new Keyword(value: keywordValue.trim().toUpperCase())
+				autoforward.addToKeywords(keyword)
+			}
+		} else {
+			println "##### AutoforwardService.saveInstance() # removing keywords"
+		}
+		autoforward.save(failOnError:true,flush:true)
+		return autoforward
+	}
+}
---
+++ plugins/frontlinesms-core/grails-app/views/autoforward/_validate.gsp
@@ -0,0 +1,53 @@
+<r:script>
+	function initializePopup() {
+		<g:if test="${activityInstanceToEdit?.id}">
+			$("#messageText").val("${activityInstanceToEdit.autoforwardText}");
+			$("#messageText").trigger("keyup");
+		</g:if>
+		
+		aliasCustomValidation();
+		genericSortingValidation();
+
+		var validator = $("#create_autoforward").validate({
+			errorContainer: ".error-panel",
+			rules: {
+				messageText: { required:true },
+				name: { required:true }
+			}
+		});
+
+		var keyWordTabValidation = function() {
+			if(!isGroupChecked("blankKeyword")){
+				return validator.element('#keywords');
+			}
+			 else return true;
+		};
+		var messageTextTabValidation = function() {
+			return validator.element('#messageText');
+		};
+
+		var confirmTabValidation = function() {
+			return validator.element('input[name=name]');
+		};
+
+		mediumPopup.addValidation('activity-generic-sorting', keyWordTabValidation);
+		mediumPopup.addValidation('autoforward-create-message', messageTextTabValidation);
+		mediumPopup.addValidation('autoforward-confirm', confirmTabValidation);
+
+		$("#tabs").bind("tabsshow", function(event, ui) {
+			updateConfirmationMessage();
+		});
+	}
+	
+	function updateConfirmationMessage() {
+		var autoforwardText = $('#messageText').val().htmlEncode();
+		if(!(isGroupChecked("blankKeyword"))){
+			var keywords = $('#keywords').val().toUpperCase();
+			$("#keyword-confirm").html('<p>' + keywords  + '</p>');
+		} else {
+			$("#keyword-confirm").html('<p>' + i18n("autoforward.blank.keyword")  + '</p>');
+		}
+		$("#autoforward-confirm").html('<p>' + autoforwardText  + '</p>');
+	}
+
+</r:script>
