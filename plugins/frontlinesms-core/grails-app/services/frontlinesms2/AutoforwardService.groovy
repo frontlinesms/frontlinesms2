e8a1b9009635a127050a359a8d1a458093d589fd
Vaneyck <vaneycksemeotoki@gmail.com>
2012-11-06 10:47:38 +0300

    fix attempt on saving an autoforward

--- plugins/frontlinesms-core/grails-app/services/frontlinesms2/AutoforwardService.groovy
+++ plugins/frontlinesms-core/grails-app/services/frontlinesms2/AutoforwardService.groovy
@@ -5,13 +5,15 @@ import frontlinesms2.*
 class AutoforwardService {
 
     def saveInstance(Autoforward autoforward, params) {
+    	println "##### Saving Autoforward in Service"
 		autoforward.name = params.name ?: autoforward.name
 		autoforward.autoforwardText = params.messageText ?: autoforward.autoforwardText
 		autoforward.keywords?.clear()
-
-		def newContacts = params.addresses.collect { return Contact.findByMobile(it)?:new Contact(mobile:it).save() }
-		(autoforward.contacts - newContacts).each { autoforward.removeFromContacts(it) }
-		(newContacts - autoforward.contacts).each { autoforward.addToContacts(it) }
+		def newContacts = params.addresses.collect { return Contact.findByMobile(it)?:new Contact(mobile:it).save(failOnError:true) }
+		println "#### Contacts from the form : ${newContacts}"
+		autoforward.removeFromContacts(autoforward.contacts - newContacts)
+		autoforward.addToContacts(newContacts - autoforward.contacts)
+		println "##### All of the contacts : ${autoforward.contacts}"
 
 		def newGroups = params.groups.collect { return Group.get(it.substring(it.length()-2,it.length()-1)) }
 
@@ -19,12 +21,12 @@ class AutoforwardService {
 			if(group instanceof SmartGroup){ autoforward.removeFromSmartGroups(group) }
 			else if (group instanceof Group){ autoforward.removeFromGroups(group) }
 		}
-
+		println "Changed smart groups"
 		(newGroups - (autoforward.groups + autoforward.smartGroups)).each{
 			if(group instanceof SmartGroup){ autoforward.addToSmartGroups(group) }
 			else if (group instanceof Group){ autoforward.addToGroups(group) }
 		}
-
+		println "# 1 ######### $autoforward.errors.allErrors"
 		autoforward.save(flush:true, failOnError:true)
 
 		if(params.sorting == 'global'){
@@ -38,6 +40,7 @@ class AutoforwardService {
 		} else {
 			println "##### AutoforwardService.saveInstance() # removing keywords"
 		}
+		println "# 2 ######### $autoforward.errors.allErrors"
 		autoforward.save(failOnError:true,flush:true)
 		return autoforward
 	}
