4e111ed447f10ef8fa697a7722a3540a30353d2c
geoffreymuchai <geoffreymuchai@gmail.com>
2012-11-13 11:57:59 +0300

    Deleting messages when emptying the trash nolonger throws a referential integrity contraints violation message

--- plugins/frontlinesms-core/grails-app/services/frontlinesms2/TrashService.groovy
+++ plugins/frontlinesms-core/grails-app/services/frontlinesms2/TrashService.groovy
@@ -2,6 +2,13 @@ package frontlinesms2
 
 class TrashService {
     	def emptyTrash() {
+    	Fmessage.findAllByIsDeleted(true).each {
+    		def conn = it.receivedOn
+    		if(conn) {
+    			conn.removeFromMessages(it)
+    			conn.save()
+    		}
+    	}
 		Fmessage.findAllByIsDeleted(true)*.delete()
 		MessageOwner.findAllByDeleted(true)*.delete()
 		Trash.findAll()*.delete()
