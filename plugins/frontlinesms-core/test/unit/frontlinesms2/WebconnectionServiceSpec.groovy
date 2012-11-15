3182023ecf4f51c59baa8f5f9a9dc95b08532dc1
ivermac <mark.ekisa@gmail.com>
2012-11-15 16:23:38 +0300

    changes

---
+++ plugins/frontlinesms-core/test/unit/frontlinesms2/WebconnectionServiceSpec.groovy
@@ -0,0 +1,52 @@
+package frontlinesms2
+
+import spock.lang.*
+
+import org.apache.camel.Exchange
+import org.apache.camel.Message
+
+@TestFor(WebconnectionService)
+class WebconnectionServiceSpec extends Specification {
+	def requestedId
+	def mockConnection
+
+	def setup() {
+		mockConnection = Mock(Webconnection)
+		Webconnection.metaClass.static.get = { Serializable id ->
+			requestedId = id
+			return mockConnection
+		}
+		Fmessage.metaClass.static.get = { Serializable id ->
+			return Mock(Fmessage)
+		}
+	}
+
+	def 'preprocess call is handed back to the relevant domain object'() {
+		given:
+			def x = mockExchange(null, ['webconnection-id':'123'])
+		when:
+			service.preProcess(x)
+		then:
+			requestedId == '123'
+			1 * mockConnection.preProcess(x)
+	}
+
+	def 'postprocess call is handed back to the relevant domain object'() {
+		given:
+			def x = mockExchange(null, ['webconnection-id':'123','fmessage-id':'1'])
+		when:
+			service.postProcess(x)
+		then:
+			requestedId == '123'
+			1 * mockConnection.postProcess(x)
+	}
+
+	Exchange mockExchange(body, Map headers) {
+		Exchange x = Mock()
+		def inMessage = Mock(Message)
+		if(body) inMessage.body >> body
+		inMessage.headers >> headers
+		x.in >> inMessage
+		return x
+	}
+}
