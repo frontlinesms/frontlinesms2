4a232b04541ff8f82ff712a123ad2a4d315973c0
ivermac <mark.ekisa@gmail.com>
2012-11-22 14:25:11 +0300

    added open on mediumPopup

--- plugins/frontlinesms-core/web-app/js/mediumPopup.js
+++ plugins/frontlinesms-core/web-app/js/mediumPopup.js
@@ -41,7 +41,8 @@ var mediumPopup = (function() {
 				title: title,
 				buttons: [{ text:i18n("action.cancel"), click:cancel, id:"cancel" }, { text:i18n("action.back"), disabled:"true" },
 						{ text:btnFinishedText, click:submitAction, id:"submit" }],
-				close: function() { $(this).remove(); }
+				close: function() { $(this).remove(); },
+				open: function(){ console.log("opened"); }
 		});
 		addChangeHandlersForRadiosAndCheckboxes();
 		initializePopup(modalBox);
