45c9c2c1ce90a53057bc6c428a57af6a23fa2b90
Sitati Kituyi <sitati.kituyi@googlemail.com>
2012-11-26 11:28:30 +0300

    fixed duplicate code in KeywordProcessorServiceISpec helper method

--- plugins/frontlinesms-core/test/integration/frontlinesms2/service/KeywordProcessorServiceISpec.groovy
+++ plugins/frontlinesms-core/test/integration/frontlinesms2/service/KeywordProcessorServiceISpec.groovy
@@ -96,13 +96,6 @@ class KeywordProcessorServiceISpec extends grails.plugin.spock.IntegrationSpec {
 			k.ownerDetail = "PROCESSED"
 			k.save(failOnError:true, flush:true)
 		}
-		Poll p = new Poll(name:'test poll', archived:archived, deleted:deleted)
-		p.addToKeywords(new Keyword(value:"TOP", isTopLevel:true))
-		(1..5).each {
-			p.addToResponses(new PollResponse(value:"poll response ${it}"))
-			p.addToKeywords(new Keyword(value:"BOTTOM${it}", isTopLevel:false, ownerDetail:"${it}"))
-		}
-		p.addToResponses(PollResponse.createUnknown())
 		p.save(failOnError:true, flush:true)
 		return p
 	}
