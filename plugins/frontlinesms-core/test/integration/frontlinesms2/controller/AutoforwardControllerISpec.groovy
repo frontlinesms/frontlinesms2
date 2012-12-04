d10a1af0332060106a1b43df6ac902f762148fea
Sitati Kituyi <sitati.kituyi@googlemail.com>
2012-12-04 18:00:55 +0300

    CORE-1760: fixed - Autoforward edit saves new instance instead of updating

--- plugins/frontlinesms-core/test/integration/frontlinesms2/controller/AutoforwardControllerISpec.groovy
+++ plugins/frontlinesms-core/test/integration/frontlinesms2/controller/AutoforwardControllerISpec.groovy
@@ -24,4 +24,23 @@ class AutoforwardControllerISpec extends grails.plugin.spock.IntegrationSpec {
 			Autoforward.findByName('Forward')
 			controller.flash.message == i18nUtilService.getMessage([code:"autoforward.save.success", args:[Autoforward.findByName('Forward').name]])
 	}
+
+	def 'Editing a autoforward should persist changes'(){
+		setup:
+			def contact = new Contact(name:"Contact", mobile:"+123321").save()
+			def autoforward = new Autoforward(name: "test", sentMessageText: "Someone said something")
+				.addToContacts(contact)
+				.save(failOnError:true)
+			controller.params.messageText = "Forward this message"
+			controller.params.name = "Forward"
+			controller.params.addresses = ['123123','6867896789']
+			controller.params.keywords = 'try,again'
+			controller.params.ownerId = autoforward.id
+		when:
+			controller.save()
+		then:
+			Autoforward.count() == 1
+			Autoforward.findByName('Forward')
+			controller.flash.message == i18nUtilService.getMessage([code:"autoforward.save.success", args:[Autoforward.findByName('Forward').name]])
+	}
 }
