fb9c03f6f2a0d673e6aff034d0226327e850e19d
Sitati Kituyi <sitati.kituyi@googlemail.com>
2013-01-15 14:09:11 +0300

    Dropped mandatory config field contraint; added helper method for retrieving lists of domains

--- plugins/frontlinesms-core/grails-app/domain/frontlinesms2/Step.groovy
+++ plugins/frontlinesms-core/grails-app/domain/frontlinesms2/Step.groovy
@@ -6,11 +6,7 @@ abstract class Step {
 	static configFields = [:]
 
 	static constraints = {
-		// the following assumes all configFields are mandatory
-		stepProperties(nullable: true, validator: { val, obj ->
-			if (!val) return false
-			val*.key?.containsAll(obj.configFields?.collect { name, type -> name })
-		})
+		
 	}
 	
 	def process(Fmessage message) {
@@ -19,5 +15,12 @@ abstract class Step {
 
 	String getPropertyValue(key) {
 		stepProperties?.find { it.key == key }?.value
+	}
+
+	// helper method to retrieve list of entities saved as StepProperties
+	def getEntityList(entityType, propertyName) {
+		println "ALL STEP PROPERTIES::: ${stepProperties.findAll { true }.collect { it.key + ' ' + it.value }}"
+		println "entityType getall: ${entityType.getAll([1,2])}"
+		entityType.getAll(stepProperties.findAll { it.key == propertyName }*.value) - null
 	}
 }
