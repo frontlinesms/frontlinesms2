f9ed19827b1af53db8ec3e53c14e2ddce754dd78
Vaneyck <vaneycksemeotoki@gmail.com>
2013-01-16 12:45:30 +0300

    Merge branch 'CORE-1104' of github.com:frontlinesms/frontlinesms2 into CORE-1104
    Conflicts:
    
    	plugins/frontlinesms-core/test/unit/frontlinesms2/services/SubscriptionServiceSpec.groovy

--- plugins/frontlinesms-core/test/unit/frontlinesms2/services/SubscriptionServiceSpec.groovy
+++ plugins/frontlinesms-core/test/unit/frontlinesms2/services/SubscriptionServiceSpec.groovy
@@ -7,83 +7,65 @@ import grails.buildtestdata.mixin.Build
 @TestFor(SubscriptionService)
 @Build([Group, SmartGroup, Contact, Fmessage, Subscription, GroupMembership])
 class SubscriptionServiceSpec extends Specification {
-	def s, c, g, service
+	def s, c, g, service, sendService, replyMessage
 
 	private static final String TEST_CONTACT = '+1111111111'
 	private static final String TEST_NON_CONTACT = '+2222222222'
 
 	def setup() {
 		service = new SubscriptionService()
-		createTestSubscriptionAndGroup()
 		createTestContact()
 		Subscription.metaClass.addToMessages  = { m -> m }
 	}
 
 	def 'join autoreply message should be sent when doJoin is triggered'() {
 		given:
-			def sendService = Mock(MessageSendService)
-			s.messageSendService = sendService
-
-			def replyMessage = mockFmessage("woteva")
-			sendService.createOutgoingMessage({ params ->
-				params.addresses==TEST_CONTACT && params.messageText=='you have joined'
-			}) >> replyMessage
+			createTestSubscription(true)
 		when:
-			service.doJoin(s, mockFmessage("doesntmatter"))
+			service.doJoin(s, mockFmessage("doesntmatter", TEST_CONTACT))
 		then:
 			1 * sendService.send(replyMessage)
 	}
 
 	def 'leave autoreply message should be sent when doLeave is triggered'() {
 		given:
+			createTestSubscription(false)
+		when:
 			service.doLeave(s, mockFmessage("doesntmatter", TEST_CONTACT))
-			def sendService = Mock(MessageSendService)
-			s.messageSendService = sendService
-
-			def replyMessage = mockFmessage("woteva")
-			sendService.createOutgoingMessage({ params ->
-				params.addresses==TEST_CONTACT && params.messageText=='you have left'
-			}) >> replyMessage
-		when:
-			service.doLeave(s, mockFmessage("doesntmatter"))
 		then:
 			1 * sendService.send(replyMessage)
 	}
 
-	def 'correct autoreply message for join should be sent when toggle action is triggered'() {
+	def 'correct autoreply message for join should be sent when toggle action is triggered with a non-group member'() {
 		given:
-			def sendService = Mock(MessageSendService)
-			s.messageSendService = sendService
-
-			def replyMessage = mockFmessage("woteva")
-			sendService.createOutgoingMessage({ params ->
-				params.addresses==TEST_CONTACT && params.messageText=='you have joined'
-			}) >> replyMessage
+			createTestSubscription(true)
 		when:
-			service.doToggle(s, mockFmessage("doesntmatter"))
+			service.doToggle(s, mockFmessage("doesntmatter", TEST_CONTACT))
 		then:
 			1 * sendService.send(replyMessage)
 	}
 
-	def 'correct autoreply message for leave should be sent when toggle action is triggered'() {
+	def 'correct autoreply message for leave should be sent when toggle action is triggered with existing group member'() {
 		given:
-			def sendService = Mock(MessageSendService)
-			s.messageSendService = sendService
-			c.addToGroups(g)
-
-			def replyMessage = mockFmessage("woteva")
-			sendService.createOutgoingMessage({ params ->
-				params.addresses==TEST_CONTACT && params.messageText=='you have left'
-			}) >> replyMessage
+			createTestSubscription(false)
+			c.addToGroup(g)
 		when:
-			service.doToggle(s, mockFmessage("doesntmatter"))
+			service.doToggle(s, mockFmessage("doesntmatter", TEST_CONTACT))
 		then:
 			1 * sendService.send(replyMessage)
 	}
 
-	private def createTestSubscriptionAndGroup() {
+	private def createTestSubscription(joinExpected=true) {
+		sendService = Mock(MessageSendService)
+		replyMessage = mockFmessage("woteva")
+		sendService.createOutgoingMessage({ params ->
+			println "CREATE OUTGOING MESSAGE INVOKED WITH PARAMS ::: $params"
+			params.addresses==TEST_CONTACT && params.messageText == "you have ${joinExpected ? 'joined' : 'left'}"
+		}) >> replyMessage
 		g = new Group(name:"Subscription Group").save()
-		Subscription.build(name:"test subscription", group:g, joinAliases:"join", joinAutoreplyText:"you have joined", leaveAutoreplyText:"you have left", leaveAliases:"leave")
+		s = Subscription.build(name:"test subscription", group:g, joinAutoreplyText:"you have joined", leaveAutoreplyText:"you have left")
+		service.messageSendService = sendService
+		s.save(failOnError:true)
 	}
 
 	//> HELPERS
