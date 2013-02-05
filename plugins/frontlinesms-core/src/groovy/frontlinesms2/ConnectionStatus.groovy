9f324db442a7bcc1a2059b190a2cb01cd49dd964
Okal Otieno <okal@justokal.com>
2013-02-05 14:44:52 +0300

    ekisa|okal Add FAILED to ConnectionStatus enum

--- plugins/frontlinesms-core/src/groovy/frontlinesms2/ConnectionStatus.groovy
+++ plugins/frontlinesms-core/src/groovy/frontlinesms2/ConnectionStatus.groovy
@@ -4,7 +4,8 @@ enum ConnectionStatus {
 	NOT_CONNECTED,
 	CONNECTING,
 	CONNECTED,
-	DISABLED
+	DISABLED,
+	FAILED
 
 	String getI18n() {
 		getClass().simpleName.toLowerCase() + "." + name().toLowerCase()
