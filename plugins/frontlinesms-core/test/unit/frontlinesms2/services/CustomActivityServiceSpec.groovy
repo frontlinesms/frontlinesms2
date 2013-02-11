9cea7b8ac780a997d28cfb4e670c568c9b2c7ac9
geoffreymuchai <geoffreymuchai@gmail.com>
2013-02-11 12:19:51 +0300

    Fixed failing tests in CustomActivityService

--- plugins/frontlinesms-core/test/unit/frontlinesms2/services/CustomActivityServiceSpec.groovy
+++ plugins/frontlinesms-core/test/unit/frontlinesms2/services/CustomActivityServiceSpec.groovy
@@ -6,33 +6,21 @@ import grails.test.mixin.*
 import grails.buildtestdata.mixin.Build
 
 @TestFor(CustomActivityService)
-@Mock(CustomActivity)
-@Build(CustomActivity)
+@Mock([CustomActivity])
 class CustomActivityServiceSpec extends Specification {
-	def joinStep, leaveStep, replyStep, service
-	def setup() {
-		service = new CustomActivityService()
-	}
 	
 	def "triggerSteps invokes doAction on each step"() {
 		given:
 			def m = Mock(Fmessage)
-			def c = createTestCustomActivity()
+			def joinStep = Mock(JoinActionStep)
+			def leaveStep = Mock(LeaveActionStep)
+			def replyStep = Mock(ReplyActionStep)
+			def c = new CustomActivity(name:"test", steps:[joinStep, leaveStep, replyStep])
 		when:
 			service.triggerSteps(c, m)
 		then:
 			1 * joinStep.process(_)
 			1 * leaveStep.process(_)
 			1 * replyStep.process(_)
-	}
-
-	private def createTestCustomActivity() {
-		def c = CustomActivity.build()
-		joinStep = Mock(JoinActionStep)
-		leaveStep = Mock(LeaveActionStep)
-		replyStep = Mock(ReplyActionStep)
-		[joinStep, leaveStep, replyStep].each { c.addToSteps(it) }
-		println "c is $c and its steps are ${c.steps}"
-		c
 	}
 }
