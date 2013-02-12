1babfa4dac9ee669f071646081f63f6e5918efcc
Okal Otieno <okal@justokal.com>
2013-02-12 16:03:26 +0300

    Add messageComposer to taglib

--- plugins/frontlinesms-core/grails-app/taglib/frontlinesms2/FsmsTagLib.groovy
+++ plugins/frontlinesms-core/grails-app/taglib/frontlinesms2/FsmsTagLib.groovy
@@ -443,6 +443,16 @@ class FsmsTagLib {
 		out << g.select(att, body)
 	}
 
+	def messageComposer = { att, body ->
+		out << '<div class="message-composer">'
+			//<fsms:messageComposer name="myMessage" placeholder="message.compose.prompt"/>
+			def placeholder = g.message(code:att.placeholder)
+			out << g.textArea(name:att.name, value:att.value, placeholder:placeholder, rows:att.rows?:"3")
+			def magicWandAttributes = [controller:att.controller, target:att.target, fields:att.fields, hidden:false]
+			out << magicWand(magicWandAttributes)
+		out << '</div>'
+	}
+
 	def step = { att, body ->
 		out << render(template:'/customactivity/step', model:[stepId:att.stepId, type:att.type, body:body])
 	}
