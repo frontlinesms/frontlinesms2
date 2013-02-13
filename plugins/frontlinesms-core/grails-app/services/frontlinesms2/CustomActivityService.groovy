963fcf7b0813c1754112aff63a9aff4aa22ac7a3
Vaneyck <vaneycksemeotoki@gmail.com>
2013-02-13 12:30:03 +0300

    fixed saving of stepProperties in CustomActivityService

--- plugins/frontlinesms-core/grails-app/services/frontlinesms2/CustomActivityService.groovy
+++ plugins/frontlinesms-core/grails-app/services/frontlinesms2/CustomActivityService.groovy
@@ -54,11 +54,14 @@ class CustomActivityService {
 			
 			def stepInstance = Step.implementations.find {it.shortName == step.stepType}.newInstance(step)
 			stepInstance.save()
-			step.stepProperties.each { stepProperty ->
-				def stepProp = new StepProperty(stepProperty)
-				println "$stepProp::: ${stepProp.key}: ${stepProp.value}"
-				stepInstance.addToStepProperties(stepProp)
-				println "$stepInstance::: ${stepInstance.stepProperties}"
+			step.each { k,v ->
+				def stepProp
+				if(!(k in ["stepType", "stepId"])) {
+					stepProp = new StepProperty(key:k, value:v)
+					println "$stepProp::: ${stepProp.key}: ${stepProp.value}"
+					stepInstance.addToStepProperties(stepProp)
+					println "$stepInstance::: ${stepInstance.stepProperties}"
+				}
 
 			}
 			stepInstanceList << stepInstance
