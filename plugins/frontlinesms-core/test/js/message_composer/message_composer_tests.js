ed39f1de0a21b2e9e3308822b84702b8421857b0
Okal Otieno <okal@justokal.com>
2013-02-15 14:55:39 +0300

    Add test for getCharacterCount

--- plugins/frontlinesms-core/test/js/message_composer/message_composer_tests.js
+++ plugins/frontlinesms-core/test/js/message_composer/message_composer_tests.js
@@ -1,1 +1,12 @@
 QUnit.module("message_composer");
+
+function setUp() {
+	dom_trix.initDomFromFile("test/js/message_composer/message_composer_test.html");
+	window.messageComposerUtils = messageComposerUtils;
+}
+
+test("messageComposerUtils.getCharacterCount should return the number of characters in the textarea of a messageComposer", function() {
+	var messageComposerInstance = $('.message-composer')[0],
+	charCount = messageComposerUtils.getCharacterCount($(messageComposerInstance));
+	equal(charCount, 17);
+});
