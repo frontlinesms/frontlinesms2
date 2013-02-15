530621e9a77162edf6181136efd6719b6d62d6be
Okal Otieno <okal@justokal.com>
2013-02-15 15:43:59 +0300

    Fix message composer test JS

--- plugins/frontlinesms-core/test/js/message_composer_tests.js
+++ plugins/frontlinesms-core/test/js/message_composer_tests.js
@@ -1,11 +1,12 @@
 QUnit.module("message_composer");
 
-function setUp() {
-	dom_trix.initDomFromFile("test/js/message_composer/message_composer_test.html");
+function setup() {
+	dom_trix.initDomFromFile("test/js/message_composer/standard.html");
 	window.messageComposerUtils = messageComposerUtils;
 }
 
 test("messageComposerUtils.getCharacterCount should return the number of characters in the textarea of a messageComposer", function() {
+	setup();
 	var messageComposerInstance = $('.message-composer')[0],
 	charCount = messageComposerUtils.getCharacterCount($(messageComposerInstance));
 	equal(charCount, 17);
