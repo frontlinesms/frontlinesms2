7e9e6edd8d45b1dafec7d700b35030737198c6ee
Okal Otieno <okal@justokal.com>
2013-02-15 16:51:47 +0300

    Refactor messageComposerUtils into an anonymous function

--- plugins/frontlinesms-core/web-app/js/message_composer.js
+++ plugins/frontlinesms-core/web-app/js/message_composer.js
@@ -1,36 +1,47 @@
-messageComposerUtils = {
-	getCharacterCount: function(messageComposerInstance) {
+messageComposerUtils = (function() {
+	var init, getCharacterCount, getText, updateCharacterCount;
+
+	init = function() {
+		$('.message-composer').live('focus', function() {
+			$(this).addClass('focus');
+			$(this).children('textarea').autosize();
+		});
+
+		$('.message-composer').live('blur', function() {
+			$(this).removeClass('focus');
+		});
+
+		$('.message-composer textarea').live('keyup', function() {
+			messageComposerUtils.updateCharacterCount($($(this).parent()[0]));
+		});
+	};
+
+	getCharacterCount = function(messageComposerInstance) {
 		return $($(messageComposerInstance).children('textarea')[0]).val().length;
-	},
+	};
 
-	getText: function(messageComposerInstance) {
-		return $($(messageComposerInstance).children('textarea')[0]).val()
-	},
+	getText = function(messageComposerInstance) {
+		return $($(messageComposerInstance).children('textarea')[0]).val();
+	};
 
-	updateCharacterCount: function(messageComposerInstance) {
+	updateCharacterCount = function(messageComposerInstance) {
 		var charCount, charCountDisplay, textArea, text;
-		textArea = $(messageComposerInstance).children('textarea')
+		textArea = $(messageComposerInstance).children('textarea');
 		charCountDisplay = $($(messageComposerInstance).children('.controls')[0]).children('.character-count-display');
-		charCount = this.getCharacterCount($(messageComposerInstance))
-		text = this.getText($(messageComposerInstance))
+		charCount = this.getCharacterCount($(messageComposerInstance));
+		text = this.getText($(messageComposerInstance));
 		$(charCountDisplay).html(charCount);
 		if (text.indexOf('${') !== -1) {
 			$(charCountDisplay).addClass('warning').attr('title', 'May be longer after substitution');
 		} else {
 			$(charCountDisplay).removeClass('warning').attr('title', 'OK');
 		}
-	}
-}
+	};
 
-$('.message-composer').live('focus', function() {
-	$(this).addClass('focus');
-	$(this).children('textarea').autosize();
-});
-
-$('.message-composer').live('blur', function() {
-	$(this).removeClass('focus');
-});
-
-$('.message-composer textarea').live('keyup', function() {
-	messageComposerUtils.updateCharacterCount($($(this).parent()[0]));
-});
+	return {
+		init: init,
+		getCharacterCount: getCharacterCount,
+		getText: getText,
+		updateCharacterCount: updateCharacterCount
+	};
+})();
