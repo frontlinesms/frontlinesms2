854084773026779616df35d2896c3ff17d339b61
Alex Anderson <alex@frontlinesms.com>
2013-02-26 17:51:24 +0300

    Minor tweak to GroupBaseSpec for efficiency.

--- plugins/frontlinesms-core/test/functional/frontlinesms2/contact/GroupBaseSpec.groovy
+++ plugins/frontlinesms-core/test/functional/frontlinesms2/contact/GroupBaseSpec.groovy
@@ -13,15 +13,16 @@ abstract class GroupBaseSpec extends grails.plugin.geb.GebSpec {
 		def bobby = new Contact(name:'Bobby').save(failOnError:true, flush:true)
 		def duchamps = new Contact(name:'Duchamps').save(failOnError:true, flush:true)
 		[bobby, duchamps].each() { friendsGroup.addToMembers(it) }
-		friendsGroup.save(failOnError:true, flush:true)
 	}
 	
 	def createManyContactsAddToGroups() {
+		def lastGroupMembership
 		(11..90).each {
 			def c = new Contact(name: "Contact${it}", mobile: "987654321${it}", notes: 'notes').save(failOnError:true, flush:true)
-			c.addToGroups(Group.findByName('Friends')).save(failOnError:true, flush:true)
+			lastGroupMembership = c.addToGroup(Group.findByName('Friends'))
 		}
+		// TODO this could be changed to `sessionFactory.flush()`, but would need to get hold of the session factory
+		lastGroupMembership.save(failOnError:true, flush:true)
 	}
-	
 }
 
