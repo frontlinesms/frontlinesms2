92a96f0082f4ff48835c655b893d788c6d458a96
Sitati Kituyi <sitati.kituyi@googlemail.com>
2013-02-28 10:39:16 +0300

    Url mappings no longer excluded in plugin build

--- plugins/frontlinesms-core/FrontlinesmsCoreGrailsPlugin.groovy
+++ plugins/frontlinesms-core/FrontlinesmsCoreGrailsPlugin.groovy
@@ -2,8 +2,7 @@ class FrontlinesmsCoreGrailsPlugin {
 	def version = "3.0-SNAPSHOT"
 	def grailsVersion = "2.0.3"
 	def pluginExcludes = ["grails-app/views/error.gsp",
-			"grails-app/conf/CoreBootStrap.groovy",
-			"grails-app/conf/CoreUrlMappings.groovy"]
+			"grails-app/conf/CoreBootStrap.groovy"]
 	def author = "FrontlineSMS team"
 	def authorEmail = "dev@frontlinesms.com"
 	def title = "FrontlineSMS Core"
