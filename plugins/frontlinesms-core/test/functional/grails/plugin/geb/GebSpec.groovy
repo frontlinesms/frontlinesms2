cfe460c710b2c87451102e02e71c87f6a1c37507
Alex Anderson <alex@frontlinesms.com>
2013-02-28 13:08:09 +0300

    Modified GebSpec to clear hibernate cache after each spec is run.

--- plugins/frontlinesms-core/test/functional/grails/plugin/geb/GebSpec.groovy
+++ plugins/frontlinesms-core/test/functional/grails/plugin/geb/GebSpec.groovy
@@ -3,4 +3,11 @@ package grails.plugin.geb
 /** TODO you should directly extend geb.spock.GebReportingSpec instead of this class */
 @Deprecated
 class GebSpec extends geb.spock.GebReportingSpec {
+	def cleanupSpec() {
+		// CLearing the hibernate session should improve performance of tests over time
+		frontlinesms2.Contact.withSession { s ->
+			s.clear()
+		}
+	}
 }
+
