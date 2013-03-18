8d080e4e57ace3904810e1c965c688c437b3361b
Okal Otieno <okal@justokal.com>
2013-03-18 14:43:43 +0300

    TOOLS-299 Remove event listeners in log service

--- plugins/frontlinesms-core/grails-app/services/frontlinesms2/LogService.groovy
+++ plugins/frontlinesms-core/grails-app/services/frontlinesms2/LogService.groovy
@@ -1,15 +1,11 @@
 package frontlinesms2
 
-import grails.events.Listener
-
 class LogService {
-	@Listener(topic="routeCreated", namespace="fconnection")
 	def handleRouteCreated(connection) {
 		def routes = connection.routeDefinitions
 		LogEntry.log("Created routes: ${routes*.id}")	
 	}
 
-	@Listener(topic="routeCreationFailed", namespace="fconnection")
 	def handleRouteCreationFailed(connection) {
 		LogEntry.log("Error creating routes to fconnection with name ${connection?.name?: connection?.id}")
 	}
