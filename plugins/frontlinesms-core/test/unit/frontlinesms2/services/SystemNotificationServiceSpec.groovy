62262e5f6b1d31cc94e19ccf626108da91b2d560
Okal Otieno <okal@justokal.com>
2013-03-22 17:19:40 +0300

    TOOLS-299 Add test for SystemNotificationService

---
+++ plugins/frontlinesms-core/test/unit/frontlinesms2/services/SystemNotificationServiceSpec.groovy
@@ -0,0 +1,25 @@
+package frontlinesms2.services
+
+import spock.lang.*
+import frontlinesms2.SystemNotification
+import frontlinesms2.SystemNotificationService
+import frontlinesms2.I18nUtilService
+
+@TestFor(SystemNotificationService)
+@Mock([SystemNotification])
+
+class SystemNotificationServiceSpec extends Specification {
+	def setup() {
+		def i18nUtilService = Mock(I18nUtilService)
+		i18nUtilService.getMessage(_) >> { args -> args.code }
+		service.i18nUtilService = i18nUtilService	
+	}
+	def "createSystemNotification creates a SystemNotification"() {
+		given:
+			def code = "a.bit.of.i18n.code" 
+		when:
+			service.createSystemNotification(code, [], [:])
+		then:
+			SystemNotification.count()
+	}
+}
