4a1e17498886e9cf9dd6e001e55d411bb4fcaaaf
Okal Otieno <okal@justokal.com>
2013-03-26 15:33:14 +0300

    Fix bug to enable SystemNotificationService to access the exception in kwargs

--- plugins/frontlinesms-core/grails-app/services/frontlinesms2/SystemNotificationService.groovy
+++ plugins/frontlinesms-core/grails-app/services/frontlinesms2/SystemNotificationService.groovy
@@ -3,7 +3,7 @@ package frontlinesms2
 class SystemNotificationService {
 	def i18nUtilService
 	def createSystemNotification(code, args, kwargs=[:]) {
-		if(kwargs.exception) args += [i18nUtilService.getMessage(code:'connection.error.'+exception.class.name.toLowerCase(), args:[exception.message])]
+		if(kwargs.exception) args += [i18nUtilService.getMessage(code:'connection.error.'+kwargs.exception.class.name.toLowerCase(), args:[kwargs.exception.message])]
 		def text = i18nUtilService.getMessage(code:code, args:args)
 		getOrCreate(text)
 	}
