17368437c608aa1ceb4c566a26af2a6c8e765950
Vaneyck <vaneycksemeotoki@gmail.com>
2013-04-16 16:32:03 +0300

    TOOLS-410 WIP serverUrl value is returned by ConnectionController.list action

--- plugins/frontlinesms-core/grails-app/controllers/frontlinesms2/ConnectionController.groovy
+++ plugins/frontlinesms-core/grails-app/controllers/frontlinesms2/ConnectionController.groovy
@@ -7,17 +7,20 @@ class ConnectionController extends ControllerUtils {
 	def fconnectionService
 	def messageSendService
 	def smssyncService
+	def grailsApplication
 
 	def index() {
 		redirect action:'list'
 	}
 	
 	def list() {
+		def url = grailsApplication.config.server.url
 		def fconnectionInstanceList = Fconnection.list(params)
 		def fconnectionInstanceTotal = Fconnection.count()
 
 		def model = [connectionInstanceList:fconnectionInstanceList,
-				fconnectionInstanceTotal:fconnectionInstanceTotal]
+				fconnectionInstanceTotal:fconnectionInstanceTotal
+				serverUrl:"${url}"]
 		if(params?.id) {
 			model << show()
 			render view:'show', model:model
