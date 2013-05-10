a439a1f3e0d08b56e97e5b7e55436fd57c70d36c
Okal Otieno <okal@justokal.com>
2013-05-10 11:37:02 +0300

    TOOLS-247 Add remote closure to GebSpec class.

--- plugins/frontlinesms-core/test/functional/grails/plugin/geb/GebSpec.groovy
+++ plugins/frontlinesms-core/test/functional/grails/plugin/geb/GebSpec.groovy
@@ -1,8 +1,18 @@
 package grails.plugin.geb
 
+import grails.plugin.remotecontrol.RemoteControl
+
 /** TODO you should directly extend geb.spock.GebReportingSpec instead of this class */
 @Deprecated
 class GebSpec extends geb.spock.GebReportingSpec {
+	static final remote
+	static final vanillaRemote
+	static {
+		remote = new RemoteControl()
+		remote.useNullIfResultWasUnserializable = true
+		vanillaRemote = remote
+	}
+
 	def cleanupSpec() {
 		// CLearing the hibernate session should improve performance of tests over time
 		frontlinesms2.Contact.withSession { s ->
