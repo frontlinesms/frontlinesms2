1a3815980d14efe788af82c8d67bcf0fa5f4c827
ivermac <mark.ekisa@gmail.com>
2013-05-10 16:51:14 +0300

    FIX TOOLS-438 formatted and removed spaces in code

--- plugins/frontlinesms-core/grails-app/taglib/frontlinesms2/FsmsTagLib.groovy
+++ plugins/frontlinesms-core/grails-app/taglib/frontlinesms2/FsmsTagLib.groovy
@@ -400,13 +400,10 @@ class FsmsTagLib {
 	}
 
 	def quickMessage = { att ->
-		att << [ controller:"quickMessage",
-				 action:"create",
-				 id:"quick_message",
-				 popupCall:"mediumPopup.launchMediumWizard(i18n('wizard.quickmessage.title'), data, i18n('wizard.send'), true); mediumPopup.selectSubscriptionGroup(${att.groupId});" ]
+		def popupCall = "mediumPopup.launchMediumWizard(i18n('wizard.quickmessage.title'), data, i18n('wizard.send'), true); mediumPopup.selectSubscriptionGroup(${att.groupId});"
+		att << [controller:"quickMessage", action:"create", id:"quick_message", popupCall: popupCall]
 		def body = "<span class='quick-message'>${g.message(code:'fmessage.quickmessage')}</span>"
 		out << fsms.popup(att,body)
-
 	}
 
 	def popup = { att, body ->
