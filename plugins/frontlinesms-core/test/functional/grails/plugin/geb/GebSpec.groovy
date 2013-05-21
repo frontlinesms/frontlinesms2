cbb1329ad9e67aab7f671c4da7e37ba618646cfd
Alex Anderson <alex@frontlinesms.com>
2013-05-21 10:07:22 +0300

    Added remote control injection to Page Objects.

--- plugins/frontlinesms-core/test/functional/grails/plugin/geb/GebSpec.groovy
+++ plugins/frontlinesms-core/test/functional/grails/plugin/geb/GebSpec.groovy
@@ -3,12 +3,11 @@ package grails.plugin.geb
 import grails.plugin.remotecontrol.RemoteControl
 import groovy.sql.Sql
 
-/** TODO you should directly extend geb.spock.GebReportingSpec instead of this class */
-@Deprecated
 class GebSpec extends geb.spock.GebReportingSpec {
 	static final remoteControl
 	static {
 		remoteControl = new RemoteControl()
+		geb.Page.metaClass.static.remote = { Closure c -> remoteControl.exec(c) }
 	}
 	static remote(Closure c) { remoteControl.exec(c) }
 
