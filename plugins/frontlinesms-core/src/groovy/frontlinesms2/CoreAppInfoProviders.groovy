4c0917b36956952e32c17b537bf3e846147ede2a
Okal Otieno <okal@justokal.com>
2013-06-04 14:57:02 +0300

    TOOLS-548 Refactor FsmsTaglib and CoreAppInfoProviders to use StatusIndicatorService.getColour

--- plugins/frontlinesms-core/src/groovy/frontlinesms2/CoreAppInfoProviders.groovy
+++ plugins/frontlinesms-core/src/groovy/frontlinesms2/CoreAppInfoProviders.groovy
@@ -1,18 +1,9 @@
 package frontlinesms2
 
-import frontlinesms2.ConnectionStatus as CS
 
 class CoreAppInfoProviders {
 	static def statusIndicatorProvider = { app, controller, data ->
-		def status = Fconnection.list()*.status
-		if(CS.FAILED in status) {
-			return 'red'
-		} else if(CS.CONNECTING in status) {
-			return 'orange'
-		} else if(CS.CONNECTED in status) {
-			return 'green'
-		}
-		return 'grey'
+		app.mainContext.statusIndicatorService.color
 	}
 
 	static def contactMessageStats =  { app, controller, data ->
