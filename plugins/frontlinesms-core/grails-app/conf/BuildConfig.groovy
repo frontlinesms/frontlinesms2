4af410f1552c474ead13cab7b9bafa5633d779e9
Sitati Kituyi <sitati.kituyi@googlemail.com>
2013-06-14 11:53:10 +0300

    Prepare for 3.4 plugin release

--- plugins/frontlinesms-core/grails-app/conf/BuildConfig.groovy
+++ plugins/frontlinesms-core/grails-app/conf/BuildConfig.groovy
@@ -105,7 +105,7 @@ grails.project.dependency.resolution = {
 			exclude 'hibernate'
 		}
 
-		compile ":flashier-messages:1.0-SNAPSHOT"
+		compile ":flashier-messages:1.0"
 		// Uncomment these (or add new ones) to enable additional resources capabilities
 		//runtime ":zipped-resources:1.0"
 		//runtime ":cached-resources:1.0"
