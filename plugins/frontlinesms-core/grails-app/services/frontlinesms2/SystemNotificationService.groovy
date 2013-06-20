1afa5e69f07d3ddac9b50b9a6861e1fd7a547755
geoffreymuchai <geoffreymuchai@gmail.com>
2013-06-20 12:19:20 +0300

    TOOLS-630 Added nullable topic field to SystemNotification domain

--- plugins/frontlinesms-core/grails-app/services/frontlinesms2/SystemNotificationService.groovy
+++ plugins/frontlinesms-core/grails-app/services/frontlinesms2/SystemNotificationService.groovy
@@ -5,12 +5,13 @@ class SystemNotificationService {
 	def create(Map params) {
 		if(params?.kwargs?.exception) params?.args += [i18nUtilService.getMessage(code:'connection.error.'+params.kwargs.exception.class.name.toLowerCase(), args:[params.kwargs.exception.message])]
 		def text = i18nUtilService.getMessage(code:params?.code, args:params?.args)
-		getOrCreate(text)
+		getOrCreate(text, params?.topic)
 	}
 
-	private def getOrCreate(text) {
+	private def getOrCreate(text, topic=null) {
 		def notification = SystemNotification.findOrCreateByText(text)
 		notification.read = false
+		notification.topic = topic
 		notification.save(flush:true)
 	}
 }
