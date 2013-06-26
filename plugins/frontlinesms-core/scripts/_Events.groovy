37064b18e36ccc9d4bae8889d017ef83639ab3bf
Alex Anderson <alex@frontlinesms.com>
2013-06-26 16:58:01 +0300

    Updated to latest version of test plugin to remove duplicated screenshot wall generation code from Events.

--- plugins/frontlinesms-core/scripts/_Events.groovy
+++ plugins/frontlinesms-core/scripts/_Events.groovy
@@ -54,17 +54,6 @@ eventTestStart = { testName ->
 	}
 }
 
-eventTestPhaseEnd = { phaseName ->
-	if (phaseName == 'functional' && new File('target/test-reports/geb').exists()) {
-		def report = new File('target/test-reports/html/screenshots.html')
-		new File('target/test-reports/geb').eachFileRecurse { f ->
-			if(!f.name.endsWith('.png')) return
-			report.append '<img height="120" src="..' +
-			f.path.substring('target/test-reports'.size()) + '"/>\n'
-		}
-	}
-}
-
 def getApplicationProperty(key) {
 	def MATCHER = key.replace('.', /\./) + /=(.*)/
 	def match
