660441a5092a7d3d10a8e3090a86d44d133cf12c
Sitati Kituyi <sitati.kituyi@googlemail.com>
2013-09-30 14:39:49 +0300

    TOOLS-882 Dropped redundant url building from ConnectionController

--- plugins/frontlinesms-core/grails-app/controllers/frontlinesms2/ConnectionController.groovy
+++ plugins/frontlinesms-core/grails-app/controllers/frontlinesms2/ConnectionController.groovy
@@ -16,15 +16,6 @@ class ConnectionController extends ControllerUtils {
 	}
 
 	def list() {
-		def scheme = request.scheme.toLowerCase()
-		def port = request.serverPort
-		if(request.getHeader('x-forwarded-proto') == 'https') {
-			scheme = 'https'
-			port = 443
-		}
-		def explicitPort = (scheme == 'http' && port != 80) || (scheme == 'https' && port != 443)
-		def domain = (request.serverName == 'localhost' ? '&lt;your-ip-address&gt;' : request.serverName)
-		def url = "${scheme}://${domain}${explicitPort ? (':' + port ):''}"
 		def fconnectionInstanceList = Fconnection.list(params)
 		def fconnectionInstanceTotal = Fconnection.count()
 		def appSettings = [:]
@@ -39,8 +30,7 @@ class ConnectionController extends ControllerUtils {
 		[connectionInstanceList:fconnectionInstanceList,
 				fconnectionInstanceTotal:fconnectionInstanceTotal,
 				fconnectionRoutingMap:fconnectionRoutingMap,
-				appSettings:appSettings,
-				serverUrl:"${url}"]
+				appSettings:appSettings]
 	}
 
 	def wizard() {
