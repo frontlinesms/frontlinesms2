f060bdf8cfd7d194d7e928e0caa9f01e40ba0d56
Sitati Kituyi <sitati.kituyi@googlemail.com>
2013-10-09 12:10:28 +0300

    updated to snapshot test plugin

--- plugins/frontlinesms-core/grails-app/conf/BuildConfig.groovy
+++ plugins/frontlinesms-core/grails-app/conf/BuildConfig.groovy
@@ -70,7 +70,7 @@ grails.project.dependency.resolution = {
 	}
 
 	plugins {
-		test ':frontlinesms-grails-test:0.13'
+		test ':frontlinesms-grails-test:0.14-SNAPSHOT'
 
 		compile ":hibernate:$grailsVersion"
 		compile ":database-migration:1.0"
