5f190a8c405a06c5aa2497330799a7b65e1cee07
Sitati Kituyi <sitati.kituyi@googlemail.com>
2013-10-10 14:27:54 +0300

    Autoreply-related FSpec fixes

--- plugins/frontlinesms-core/test/functional/frontlinesms2/popup/PageSmallPopup.groovy
+++ plugins/frontlinesms-core/test/functional/frontlinesms2/popup/PageSmallPopup.groovy
@@ -94,7 +94,7 @@ class RenameFolderDialog extends SmallPopup {
 
 class RenameActivityDialog extends SmallPopup {
 	static at = {
-		popupTitle.startsWith('smallpopup.activity.rename.title')
+		popupTitle.startsWith('smallpopup.fmessage.rename.title')
 	}
 	static content = {
 		errorPanel { $('#modalBox #smallpopup-error-panel') }
