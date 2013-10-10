afbc2be188a55ca1455ec7dcac8a713ff7e4548a
Alex Anderson <alex@frontlinesms.com>
2013-10-10 13:05:10 +0300

    Simplified URL generation of PageMessageInbox

--- plugins/frontlinesms-core/test/functional/frontlinesms2/message/PageMessageInbox.groovy
+++ plugins/frontlinesms-core/test/functional/frontlinesms2/message/PageMessageInbox.groovy
@@ -6,16 +6,15 @@ class PageMessageInbox extends PageMessage {
 	static url = 'message/inbox'
 
 	String convertToPath(Object[] args) {
-		def restOfPath = ""
-		if (!args)
-			return ""
-		if (args.length == 0)
-			return ""
+		if (!args) {
+			return ''
+		}
 
-		if (args[0] instanceof Fmessage)
-			restOfPath += "/show/"+(args[0] as Fmessage).id
-		else if (args[0] instanceof Number)
-			restOfPath += "/show/"+args[0]
-		return restOfPath
+		if (args[0] instanceof Number) {
+			return "/show/${args[0]}"
+		}
+
+		return ''
 	}
 }
+
