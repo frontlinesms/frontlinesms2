f80fe7d64b6ecabd8005d12a3ed08ce495144a3c
Alex Anderson <alex@frontlinesms.com>
2014-01-30 17:18:58 +0300

    Restored MessageStorageService's processorness

--- plugins/frontlinesms-core/grails-app/services/frontlinesms2/MessageStorageService.groovy
+++ plugins/frontlinesms-core/grails-app/services/frontlinesms2/MessageStorageService.groovy
@@ -3,14 +3,13 @@ package frontlinesms2
 import org.apache.camel.Exchange
 import org.apache.camel.Processor
 
-class MessageStorageService {
-	public void process(ex) {
-		def log = { println "MessageStorageService.process() : $it" }
-		log "headers # $ex.in.headers"
-		def message = ex.in.body
+class MessageStorageService implements Processor {
+	public void process(Exchange x) {
+		def message = x.in.body
 		assert message instanceof TextMessage
 		message = message.id ? TextMessage.findById(message.id) : message
-		message.connectionId = ex.in.headers[Fconnection.HEADER_FCONNECTION_ID]
+		message.connectionId = x.in.headers[Fconnection.HEADER_FCONNECTION_ID]
 		message.save(flush:true)
 	}
 }
+
