2560cb32dcf3fb444b44010c7a8ad434278a6588
Sitati Kituyi <sitati.kituyi@googlemail.com>
2014-02-10 14:54:31 +0300

    Added debug to MessageStorageService to investigate failing IncomingSmslibRouteSpec

--- plugins/frontlinesms-core/grails-app/services/frontlinesms2/MessageStorageService.groovy
+++ plugins/frontlinesms-core/grails-app/services/frontlinesms2/MessageStorageService.groovy
@@ -6,6 +6,7 @@ import org.apache.camel.Processor
 class MessageStorageService implements Processor {
 	public void process(Exchange x) {
 		def message = x.in.body
+		println "*#*#*#*#*#* message instanceof TextMessage is :: ${message instanceof TextMessage}"
 		assert message instanceof TextMessage
 		message = message.id ? TextMessage.findById(message.id) : message
 		message.connectionId = x.in.headers[Fconnection.HEADER_FCONNECTION_ID]
