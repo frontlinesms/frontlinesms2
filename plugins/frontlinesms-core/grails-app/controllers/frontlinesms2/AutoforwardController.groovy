ae4b815ba3cac401d91018eeffeffcc5520bb138
Okal Otieno <okal@justokal.com>
2014-02-13 11:53:03 +0300

    TOOLS-1153 Remove contactList from Autoforward controller

--- plugins/frontlinesms-core/grails-app/controllers/frontlinesms2/AutoforwardController.groovy
+++ plugins/frontlinesms-core/grails-app/controllers/frontlinesms2/AutoforwardController.groovy
@@ -11,8 +11,11 @@ class AutoforwardController extends ActivityController {
 
 	def create() {
 		def groupList = Group.getGroupDetails() + SmartGroup.getGroupDetails()
-		[contactList: Contact.list(), messageText: '${message_text}',
-				groupList:groupList, activityType: params.controller]
+		[
+			messageText: '${message_text}',
+			groupList:groupList,
+			activityType: params.controller
+		]
 	}
 
 	private def withAutoforward = withDomainObject Autoforward, { params.ownerId }
