2ebc40c2b4b6faabe3cd08d54dfff6ee17b061c3
Sitati Kituyi <sitati.kituyi@googlemail.com>
2014-02-13 15:28:27 +0300

    CORE-2042 Set maximum upload size to 15mb

--- plugins/frontlinesms-core/grails-app/conf/spring/resources.groovy
+++ plugins/frontlinesms-core/grails-app/conf/spring/resources.groovy
@@ -1,3 +1,7 @@
 // Place your Spring DSL code here
 beans = {
+	multipartResolver(org.springframework.web.multipart.commons.CommonsMultipartResolver) {
+		maxInMemorySize = 15728640
+		maxUploadSize = 15728640
+	}
 }
