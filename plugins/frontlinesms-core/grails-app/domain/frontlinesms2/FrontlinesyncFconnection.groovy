fbe5f8ccf604dbab94f7c46a8c0c6384d486c43a
Vaneyck <vaneycksemeotoki@gmail.com>
2014-03-07 13:53:17 +0300

    A-24 made sendEnabled configurable for FrontlineSync

--- plugins/frontlinesms-core/grails-app/domain/frontlinesms2/FrontlinesyncFconnection.groovy
+++ plugins/frontlinesms-core/grails-app/domain/frontlinesms2/FrontlinesyncFconnection.groovy
@@ -9,9 +9,9 @@ import frontlinesms2.api.*
 @FrontlineApiAnnotations(apiUrl="frontlinesync")
 class FrontlinesyncFconnection extends Fconnection implements FrontlineApi {
 	static String getShortName() { 'frontlinesync' }
-	static final configFields = ['info-setup': ['secret'], 'info-name':['name']]
+	static final configFields = ['info-setup': ['secret'], 'info-name':['name'], 'info-sendEnabled':['sendEnabled']]
 	static final passwords = []
-	static final defaultValues = [timeout:60]
+	static final defaultValues = [timeout:60, sendEnabled:true]
 
 	def frontlinesyncService
 	def appSettingsService
