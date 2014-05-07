05b9a62b9d42c206e6631a0f17dfe259d8b630d5
Sitati Kituyi <sitati.kituyi@googlemail.com>
2014-05-07 16:21:20 +0300

    Exclude SecurityFilters from Multitenant plugin

--- plugins/frontlinesms-core/FrontlinesmsCoreGrailsPlugin.groovy
+++ plugins/frontlinesms-core/FrontlinesmsCoreGrailsPlugin.groovy
@@ -2,7 +2,8 @@ class FrontlinesmsCoreGrailsPlugin {
 	def version = '3.45-SNAPSHOT'
 	def grailsVersion = "2.0.3"
 	def pluginExcludes = ["grails-app/views/error.gsp",
-			"grails-app/conf/CoreBootStrap.groovy"]
+			"grails-app/conf/CoreBootStrap.groovy",
+			"grails-app/conf/frontlinesms2/SecurityFilters.groovy"]
 	def author = "FrontlineSMS team"
 	def authorEmail = "dev@frontlinesms.com"
 	def title = "FrontlineSMS Core"
