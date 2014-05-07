6127fa4fbfce7a1611b65e255b9e86f5fba590e6
Sitati Kituyi <sitati.kituyi@googlemail.com>
2014-05-07 16:35:34 +0300

    Manually rolled up plugin version - should have happened in last releaser

--- plugins/frontlinesms-core/FrontlinesmsCoreGrailsPlugin.groovy
+++ plugins/frontlinesms-core/FrontlinesmsCoreGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 class FrontlinesmsCoreGrailsPlugin {
-	def version = '3.45-SNAPSHOT'
+	def version = '3.46-SNAPSHOT'
 	def grailsVersion = "2.0.3"
 	def pluginExcludes = ["grails-app/views/error.gsp",
 			"grails-app/conf/CoreBootStrap.groovy",
